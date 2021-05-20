961f77af51acb9a4db31b74e23335c954630da39
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-05-20 16:00:30 +0300

    UHF-1313: Fixed phpcs.

--- helfi_platform_config.deploy.php
+++ helfi_platform_config.deploy.php
@@ -18,14 +18,14 @@ function helfi_platform_config_deploy_9001_permissions() {
     'edit own article content',
     'revert article revisions',
     'translate article node',
-    'view article revisions'
+    'view article revisions',
   ]);
   user_role_revoke_permissions('authenticated', [
     'create article content',
     'delete own article content',
     'edit own article content',
     'revert article revisions',
-    'view article revisions'
+    'view article revisions',
   ]);
   return t('Successfully revoked Article permissions.');
 }
