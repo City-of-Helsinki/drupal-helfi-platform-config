b3fafc20f0e7362659135e2103fbcf882c48d9c2
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-09-23 09:33:07 +0300

    code fixes

--- src/ConfigHelper.php
+++ src/ConfigHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\helfi_platform_config;
 
 use Symfony\Component\Yaml\Yaml;
@@ -8,24 +9,23 @@ use Symfony\Component\Yaml\Yaml;
  *
  * @package Drupal\helfi_platform_config
  */
-class ConfigHelper
-{
-    /**
-     * Install new configuration.
-     *
-     * @param string $config_location
-     *   Absolute path to the configuration to be created.
-     *
-     * @param string $config_name
-     *   Name of the configuration to install.
-     */
-    public static function installNewConfig(string $config_location, string $config_name): void
-    {
-        $config_factory = \Drupal::configFactory();
-        $filepath = "{$config_location}{$config_name}.yml";
-        $data = Yaml::parse(file_get_contents($filepath));
-        if (is_array($data)) {
-            $config_factory->getEditable($config_name)->setData($data)->save(TRUE);
-        }
+class ConfigHelper {
+
+  /**
+   * Install new configuration.
+   *
+   * @param string $config_location
+   *   Absolute path to the configuration to be created.
+   * @param string $config_name
+   *   Name of the configuration to install.
+   */
+  public static function installNewConfig(string $config_location, string $config_name): void {
+    $config_factory = \Drupal::configFactory();
+    $filepath = "{$config_location}{$config_name}.yml";
+    $data = Yaml::parse(file_get_contents($filepath));
+    if (is_array($data)) {
+      $config_factory->getEditable($config_name)->setData($data)->save(TRUE);
     }
+  }
+  
 }
