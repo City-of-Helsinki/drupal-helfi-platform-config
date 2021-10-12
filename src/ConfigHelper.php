faebbd4e09886ee699b8e97c9e4c6f51e527e25b
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-10-12 10:13:01 +0300

    Added field configuration installer and fixed update hook for helfi_content feature.

--- src/ConfigHelper.php
+++ src/ConfigHelper.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\helfi_platform_config;
 
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
 use Symfony\Component\Yaml\Yaml;
 
 /**
@@ -28,4 +30,30 @@ class ConfigHelper {
     }
   }
 
+  /**
+   * Install new field.
+   *
+   * @param string $config_location
+   *   Absolute path to the configuration to be created.
+   * @param string $field_storage
+   *   Name of the field storage configuration to install.
+   * @param string $field_config
+   *   Name of the field configuration to install.
+   */
+  public static function installNewField(string $config_location, string $field_storage, string $field_config): void {
+    // Install field storage configurations.
+    if (isset($field_storage)) {
+      $storage_data = Yaml::parse(file_get_contents("{$config_location}{$field_storage}.yml"));
+      if (!FieldStorageConfig::loadByName($storage_data['entity_type'], $storage_data['field_name'])) {
+        FieldStorageConfig::create($storage_data)->save();
+      }
+    }
+    // Install field configurations.
+    if (isset($field_config)) {
+      $field_data = Yaml::parse(file_get_contents("{$config_location}{$field_config}.yml"));
+      if (!FieldConfig::loadByName($field_data['entity_type'], $field_data['bundle'], $field_data['field_name'])) {
+        FieldConfig::create($field_data)->save();
+      }
+    }
+  }
 }
