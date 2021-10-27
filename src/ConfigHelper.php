19d86a2687609200487d32d8141a9184f27690c1
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-10-27 14:08:17 +0300

    Added file exists check on new configuration installation functions.

--- src/ConfigHelper.php
+++ src/ConfigHelper.php
@@ -24,9 +24,11 @@ class ConfigHelper {
   public static function installNewConfig(string $config_location, string $config_name): void {
     $config_factory = \Drupal::configFactory();
     $filepath = "{$config_location}{$config_name}.yml";
-    $data = Yaml::parse(file_get_contents($filepath));
-    if (is_array($data)) {
-      $config_factory->getEditable($config_name)->setData($data)->save(TRUE);
+    if (file_exists($filepath)) {
+      $data = Yaml::parse(file_get_contents($filepath));
+      if (is_array($data)) {
+        $config_factory->getEditable($config_name)->setData($data)->save(TRUE);
+      }
     }
   }
 
@@ -44,7 +46,7 @@ class ConfigHelper {
     foreach ($language_manager->getLanguages() as $language) {
       if ($language->getId() !== $language_manager->getDefaultLanguage()->getId()) {
         $filepath = "{$config_location}{$language->getId()}/{$config_name}.yml";
-        if ($yaml = file_get_contents($filepath)) {
+        if (file_exists($filepath) && $yaml = file_get_contents($filepath)) {
           $data = Yaml::parse($yaml);
           $language_manager->getLanguageConfigOverride($language->getId(), $config_name)
             ->setData($data)
@@ -65,9 +67,11 @@ class ConfigHelper {
    *   Name of the field configuration to install.
    */
   public static function installNewField(string $config_location, string $field_storage, string $field_config): void {
+    $field_storage_path = "{$config_location}{$field_storage}.yml";
+
     // Install field storage configurations.
-    if (isset($field_storage)) {
-      $storage_data = Yaml::parse(file_get_contents("{$config_location}{$field_storage}.yml"));
+    if (isset($field_storage) && file_exists($field_storage_path)) {
+      $storage_data = Yaml::parse(file_get_contents($field_storage_path));
       if (!FieldStorageConfig::loadByName($storage_data['entity_type'], $storage_data['field_name'])) {
         $allowed_values = FALSE;
 
@@ -92,9 +96,11 @@ class ConfigHelper {
       }
     }
 
+    $field_config_path = "{$config_location}{$field_config}.yml";
+
     // Install field configurations.
-    if (isset($field_config)) {
-      $field_data = Yaml::parse(file_get_contents("{$config_location}{$field_config}.yml"));
+    if (isset($field_config) && file_exists($field_config_path)) {
+      $field_data = Yaml::parse(file_get_contents($field_config_path));
       if (!FieldConfig::loadByName($field_data['entity_type'], $field_data['bundle'], $field_data['field_name'])) {
         FieldConfig::create($field_data)->save();
       }
