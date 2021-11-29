6cc749f6a2f5f21f61059a9a14240dca5d377cd3
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2021-11-29 15:11:46 +0200

    UHF-3550: Add anchors to all heading levels, not just h2 headings that are visibile in ToC

--- helfi_features/helfi_toc/assets/js/tableOfContents.js
+++ helfi_features/helfi_toc/assets/js/tableOfContents.js
@@ -21,7 +21,6 @@
       }
 
       var anchors = [];
-      var anchorLinks = [];
       var tableOfContents = $('#helfi-toc-table-of-contents');
       var tableOfContentsList = $('#helfi-toc-table-of-contents-list > ul');
       var mainContent = $('main.layout-main-wrapper');
@@ -32,13 +31,20 @@
       });
 
       // Do not include sidebar H2, Table of contents H2 or cookie compliance warnings.
+      var exclusions = '' +
+        ':not(aside *)' +
+        ':not(.unit__sidebar *)' +
+        ':not(.service__sidebar *)' +
+        ':not(#helfi-toc-table-of-contents *)' +
+        ':not(.embedded-content-cookie-compliance *)' +
+        ':not(.handorgel__header)'; // Accordion headings get their id's overridden by handorgel script
+
       var titleComponents = [
-        'h2:not(aside *)' +
-          ':not(.unit__sidebar *)' +
-          ':not(.service__sidebar *)' +
-          ':not(#helfi-toc-table-of-contents *)' +
-          ':not(.embedded-content-cookie-compliance *)' +
-          ':not(.handorgel__header)', // Accordion headings get their id's overridden by handorgel script
+        'h2'+exclusions,
+        'h3'+exclusions,
+        'h4'+exclusions,
+        'h5'+exclusions,
+        'h6'+exclusions,
         '.handorgel__header > button', // Instead of targeting accordion headings, lets target the button inside them.
       ];
 
@@ -55,6 +61,11 @@
             .replace(/\W/g, '-')
             .replace(/-(\d+)$/g, '_$1');
 
+          let nodeName = this.nodeName.toLowerCase();
+          if (nodeName === 'button') {
+            nodeName = this.parentElement.nodeName.toLowerCase();
+          }
+
           let anchorName;
           if (this.id) {
             anchorName = this.id;
@@ -64,8 +75,7 @@
           anchors.push(anchorName);
 
           // Create table of contents if component is enabled.
-          anchorLinks.push(this.textContent.trim());
-          if (tableOfContentsList.length > 0) {
+          if (tableOfContentsList.length > 0 && nodeName === "h2") {
             tableOfContentsList.append(
               '<li class="table-of-contents__item"><a class="table-of-contents__link' +
               '" href="#' +
