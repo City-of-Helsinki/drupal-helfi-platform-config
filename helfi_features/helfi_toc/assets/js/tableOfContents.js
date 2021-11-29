e7d0bd4f3a19cb05ed3624be853fd9f7c42d4927
Mikko Tapionlinna <mikko.tapionlinna@siili.com>
2021-11-29 10:29:50 +0200

    UHF-3550: Fix ToC script to avoid generating duplicate id's already on page and fix targeting accordion headings with ToC

--- helfi_features/helfi_toc/assets/js/tableOfContents.js
+++ helfi_features/helfi_toc/assets/js/tableOfContents.js
@@ -3,19 +3,43 @@
 (function ($, Drupal) {
   Drupal.behaviors.table_of_contents = {
     attach: function attach() {
-      var anchorMap = {};
+
+      function findAvailableId(name, reserved, anchors, count) {
+        var newName = name;
+        if (count > 0) { // Only when headings are not unique on page we want to add counter
+          newName += '-' + count;
+        }
+        if (reserved.includes(newName)) {
+          return findAvailableId(name, reserved, anchors, ++count);
+        } else if (anchors.includes(newName)) {
+          if (count === 0) {
+            count++; // When reserved heading is visible on page, lets start counting from 2 instead of 1
+          }
+          return findAvailableId(name, reserved, anchors, ++count);
+        }
+        return newName;
+      }
+
+      var anchors = [];
       var anchorLinks = [];
       var tableOfContents = $('#helfi-toc-table-of-contents');
       var tableOfContentsList = $('#helfi-toc-table-of-contents-list > ul');
       var mainContent = $('main.layout-main-wrapper');
+      var reservedElems = document.querySelectorAll('[id]');
+      var reserved = []; // Lets list current id's here to avoid creating duplicates
+      reservedElems.forEach(function (elem) {
+        reserved.push(elem.id);
+      });
 
       // Do not include sidebar H2, Table of contents H2 or cookie compliance warnings.
       var titleComponents = [
         'h2:not(aside *)' +
-        ':not(.unit__sidebar *)' +
-        ':not(.service__sidebar *)' +
-        ':not(#helfi-toc-table-of-contents *)' +
-        ':not(.embedded-content-cookie-compliance *)'
+          ':not(.unit__sidebar *)' +
+          ':not(.service__sidebar *)' +
+          ':not(#helfi-toc-table-of-contents *)' +
+          ':not(.embedded-content-cookie-compliance *)' +
+          ':not(.handorgel__header)', // Accordion headings get their id's overridden by handorgel script
+        '.handorgel__header > button', // Instead of targeting accordion headings, lets target the button inside them.
       ];
 
       // Craft table of contents.
@@ -32,13 +56,12 @@
             .replace(/-(\d+)$/g, '_$1');
 
           let anchorName;
-          if (!anchorMap[name]) {
-            anchorName = name;
-            anchorMap[name] = 2;
+          if (this.id) {
+            anchorName = this.id;
           } else {
-            anchorName = name + '-' + anchorMap[name];
-            anchorMap[name]++;
+            anchorName = findAvailableId(name, reserved, anchors, 0);
           }
+          anchors.push(anchorName);
 
           // Create table of contents if component is enabled.
           anchorLinks.push(this.textContent.trim());
