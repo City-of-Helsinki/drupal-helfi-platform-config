3fc03032147dcfe4cd1a79ff78565ff4ba22b45f
tuutti <tuutti@iki.fi>
2022-04-08 15:04:27 +0300

    UHF-4444: Test getters

--- helfi_features/helfi_news_feed/tests/src/Kernel/NewsFeedParagraphTest.php
+++ helfi_features/helfi_news_feed/tests/src/Kernel/NewsFeedParagraphTest.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace Drupal\Tests\helfi_news_feed\Kernel;
 
 use Drupal\helfi_news_feed\Entity\NewsFeedParagraph;
-use Drupal\KernelTests\KernelTestBase;
 use Drupal\paragraphs\Entity\Paragraph;
 
 /**
@@ -16,47 +15,18 @@ use Drupal\paragraphs\Entity\Paragraph;
 class NewsFeedParagraphTest extends KernelTestBase {
 
   /**
-   * {@inheritdoc}
-   */
-  protected static $modules = [
-    'system',
-    'field',
-    'link',
-    'image',
-    'file',
-    'user',
-    'paragraphs',
-    'external_entities',
-    'responsive_image',
-    'helfi_news_feed',
-  ];
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function setUp() : void {
-    parent::setUp();
-    $this->installEntitySchema('file');
-    $this->installEntitySchema('user');
-    $this->installEntitySchema('paragraph');
-    $this->installEntitySchema('responsive_image_style');
-    $this->installEntitySchema('paragraphs_type');
-    $this->installConfig('image');
-    $this->installConfig('paragraphs');
-    $this->installConfig('helfi_news_feed');
-    $this->installEntitySchema('helfi_news');
-  }
-
-  /**
    * Tests that paragraph uses proper bundle class.
    */
   public function testBundleClass() : void {
     $paragraph = Paragraph::create([
       'type' => 'news_list',
+      'field_tags' => ['first', 'second'],
+      'field_limit' => 22,
     ]);
     $paragraph->save();
-
     $this->assertInstanceOf(NewsFeedParagraph::class, $paragraph);
+    $this->assertEquals(['first', 'second'], $paragraph->getTags());
+    $this->assertEquals(22, $paragraph->getLimit());
   }
 
 }
