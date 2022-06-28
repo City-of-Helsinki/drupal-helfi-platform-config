b6668bdf61534c8dcca25f82d82b44adc72fd5e7
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2022-06-28 15:18:29 +0300

    UHF-5894: Fixed phpcs errors.

--- helfi_features/helfi_news_feed/tests/src/Kernel/NewsFeedParagraphTest.php
+++ helfi_features/helfi_news_feed/tests/src/Kernel/NewsFeedParagraphTest.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\helfi_news_feed\Kernel;
 
-use Drupal\external_entities\Entity\ExternalEntity;
 use Drupal\helfi_news_feed\Entity\NewsFeedParagraph;
 use Drupal\paragraphs\Entity\Paragraph;
 
@@ -21,15 +20,14 @@ class NewsFeedParagraphTest extends KernelTestBase {
   public function testBundleClass() : void {
     $paragraph = Paragraph::create([
       'type' => 'news_list',
-      // TODO: Fix tests for the external entities.
-      // 'field_helfi_news_tags' => [],
+      // 'field_helfi_news_tags' => [], @todo Fix tests for the external entities.
       'field_limit' => 22,
       'field_news_list_title' => 'test title',
       'field_news_list_description' => 'test description',
     ]);
     $paragraph->save();
     $this->assertInstanceOf(NewsFeedParagraph::class, $paragraph);
-    // TODO: Fix tests for the external entities.
+    // @todo Fix tests for the external entities.
     $this->assertEquals([], $paragraph->getTags());
     $this->assertEquals(22, $paragraph->getLimit());
     $this->assertEquals('test title', $paragraph->getTitle());
