05b8b16f0240f754e653bd84c8d69d59acba4624
Jussiles <juss.leskinen@gmail.com>
2022-08-12 14:19:04 +0300

    UHF-6277: Initial commit.

---
+++ assets/js/chat_leijuke.js
@@ -0,0 +1,5 @@
+// piirretään leijuke samantien
+// checkataan cookie(t)
+// jos clikataan leijukkeesta niin implisiittisesti asetetaan chat cookiet
+// ladataan ja triggeröidään oikea chat/chatbot
+// Drupal.removeChatIcon() tms
---
+++ src/Plugin/Block/ChatLeijuke.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\helfi_platform_config\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a Chat Leijuke block.
+ *
+ * @Block(
+ *  id = "chat_leijuke",
+ *  admin_label = @Translation("Chat Leijuke"),
+ * )
+ */
+class ChatLeijuke extends BlockBase {
+
+
+  // TODO: block configiin käyttäjälle valinta mikä chat kilke, jonka perusteella ladataan ja triggeröidään sopiva js
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['chat_selection'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Chat/bot provider'),
+      '#description' => $this->t('Choose the approriate chat/bot provider?'),
+      '#default_value' => $config['chat_selection'] ?? '',
+      '#options' => [
+        'kuura_health_chat' => 'kuura_health_chat',
+        'smartti_chatbot' => 'smartti_chatbot',
+        'genesys_chat' => 'genesys_chat',
+        'genesys_suunte' => 'genesys_suunte',
+        'genesys_neuvonta' => 'genesys_neuvonta',
+        'watson_chatbot' => 'watson_chatbot',
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $library = ['helfi_platform_config/chat_leijuke'];
+    $config = $this->getConfiguration();
+    $build = [];
+
+    $build['leijuke'] = [
+      '#title' => $this->t('Chat Leijuke'),
+      '#attached' => [
+        'library' => $library,
+        'drupalSettings' => ['chat_selection' => $config['chat_selection'] ?? ''],
+      ]
+    ];
+
+    return $build;
+  }
+
+}
