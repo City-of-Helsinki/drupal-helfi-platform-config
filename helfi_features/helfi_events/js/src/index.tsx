f8bcfd0bdee87d0e45fa2d00b4b775fd525d169b
Jere Ljungberg <jere@mysteerio.fi>
2022-09-27 08:09:35 +0300

    UHF-6666: Add search filters for React version of LinkedEvents

--- helfi_features/helfi_events/js/src/index.tsx
+++ helfi_features/helfi_events/js/src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchContainer from './containers/SearchContainer';
 
+import QueryBuilder from './utils/QueryBuilder';
+
 const rootSelector: string = 'helfi-events-search';
 const rootElement: HTMLElement | null = document.getElementById(rootSelector);
+const queryBuilder = QueryBuilder();
 
 if (rootElement) {
   ReactDOM.render(
     <React.StrictMode>
-      <SearchContainer />
+      <SearchContainer queryBuilder={queryBuilder}/>
     </React.StrictMode>,
     rootElement
   );
