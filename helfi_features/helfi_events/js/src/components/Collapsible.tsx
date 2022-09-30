68216dfc99390f1695977f9e8059b6d3134f6575
Jere Ljungberg <jere@mysteerio.fi>
2022-09-30 18:43:56 +0300

    Styling fixes

--- helfi_features/helfi_events/js/src/components/Collapsible.tsx
+++ helfi_features/helfi_events/js/src/components/Collapsible.tsx
@@ -14,18 +14,11 @@ type Props = {
   title: string|React.ReactNode;
 }
 
-<<<<<<< HEAD
-=======
-// Match HDS styles for helper text
-const helperStyles = {
-  color:' var(--helper-color-default)',
-  marginTop: 'var(--spacing-3-xs)'
-};
-
->>>>>>> fa3170f (UHF-6666: Move filter to own components)
 const Collapsible = ({active, ariaControls, helper, id, label, title, children, showHandle}: Props) => {
   const [isActive, setActive] = useState<boolean>(active||false);
   const ref = useRef<HTMLDivElement|null>(null);
+  const controlRef = useRef<HTMLButtonElement|null>(null);
+  const labelRef = useRef<HTMLLabelElement|null>(null);
 
   const getHandle = () => {
     if(showHandle !== false) {
@@ -35,34 +28,41 @@ const Collapsible = ({active, ariaControls, helper, id, label, title, children, 
     }
   }
 
+  const getChildrenStyle = () => {
+    let topMargin = 0;
+
+    if (controlRef && controlRef.current) {
+      console.log(controlRef.current);
+    }
+
+    return {marginTop: topMargin + 'px'}
+  }
+
   useOutsideClick(ref, () => {
     setActive(false);
   });
 
   return (
     <div className='collapsible-wrapper' ref={ref}>
-      <label className='collapsible__label' htmlFor={id}>{label}</label>
+      <label className='collapsible__label' htmlFor={id} ref={labelRef}>{label}</label>
       <button
         id={id}
         className='collapsible__element collapsible__control'
         aria-controls={ariaControls}
         aria-expanded={isActive}
         onClick={() => setActive(!isActive)}
+        ref={controlRef}
       >
         <span className='collapsible__title'>{ title }</span>
         {getHandle()}
       </button>
       {isActive &&
-        <div className='collapsible__element collapsible__children'>
+        <div style={getChildrenStyle()} className='collapsible__element collapsible__children'>
           {children}
         </div>
       }
       {helper &&
-<<<<<<< HEAD
         <div aria-hidden={true} className='collapsible__helper'>{helper}</div>
-=======
-        <div aria-hidden={true} style={helperStyles} className='collapsible__helper'>{helper}</div>
->>>>>>> fa3170f (UHF-6666: Move filter to own components)
       }
     </div>
   );
