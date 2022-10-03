25c72d4d12e10790bd3bf754c4eaa285925d57f3
Jere Ljungberg <jere@mysteerio.fi>
2022-10-03 11:50:42 +0300

    Add conditional rendering based on props

--- helfi_features/helfi_events/js/src/index.tsx
+++ helfi_features/helfi_events/js/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchContainer from './containers/SearchContainer';
 
+import type FilterSettings from './types/FilterSettings';
 import QueryBuilder from './utils/QueryBuilder';
 
 const rootSelector: string = 'helfi-events-search';
@@ -10,10 +11,16 @@ const eventsUrl = rootElement?.dataset?.eventsUrl;
 
 if (eventsUrl) {
   const queryBuilder = QueryBuilder(eventsUrl);
+  const filterSettings: FilterSettings = {
+    showLocation: rootElement?.dataset?.showLocationFilter === '1',
+    showTimeFilter: rootElement?.dataset?.showTimeFilter === '1',
+    showFreeFilter: rootElement?.dataset?.showFreeEventsFilter === '1',
+    showRemoteFilter: rootElement?.dataset?.showRemoteEventsFilter === '1'
+  };
 
   ReactDOM.render(
     <React.StrictMode>
-      <SearchContainer queryBuilder={queryBuilder}/>
+      <SearchContainer queryBuilder={queryBuilder} filterSettings={filterSettings} />
     </React.StrictMode>,
     rootElement
   );
