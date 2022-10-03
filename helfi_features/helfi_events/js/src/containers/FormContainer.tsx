fb8986a74c5ab099e31bfb402e7f9437dc00c7b3
Jere Ljungberg <jere@mysteerio.fi>
2022-10-03 12:30:43 +0300

    Fix end date parameter when using the same date for both dates

--- helfi_features/helfi_events/js/src/containers/FormContainer.tsx
+++ helfi_features/helfi_events/js/src/containers/FormContainer.tsx
@@ -123,6 +123,34 @@ const FormContainer = ({ filterSettings, queryBuilder, onSubmit, loading, locati
 
   }, [startDate, endDate, endDisabled, queryBuilder])
 
+  useEffect(() => {
+    const setDate = (key: string, value: string|undefined) => {
+      if (!value || value === '') {
+        queryBuilder.resetParam(key);
+        return;
+      }
+  
+      let parsedDate = null;
+      try {
+        parsedDate = parse(value, 'd.M.y', new Date());
+      }
+      catch (e) {
+      }
+  
+      if (parsedDate) {
+        queryBuilder.setParams({[key]: format(parsedDate, 'y-MM-dd')});
+      }
+    }
+
+    setDate(ApiKeys.START, startDate);
+    if (endDisabled) {
+      setDate(ApiKeys.END, startDate);
+    }
+    if (!endDisabled) {
+      setDate(ApiKeys.END, endDate);
+    }
+  }, [startDate, endDate, endDisabled, queryBuilder])
+
   const toggleFreeEvents = (event: React.ChangeEvent<HTMLInputElement>) => {
     const checked = event?.target?.checked;
 
