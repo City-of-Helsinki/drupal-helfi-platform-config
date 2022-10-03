4323d3eeee2d2c172e23f2b68d48f6aea1f9eb14
Jere Ljungberg <jere@mysteerio.fi>
2022-10-03 09:28:44 +0300

    Style changes

--- helfi_features/helfi_events/js/src/components/CheckboxFilter.tsx
+++ helfi_features/helfi_events/js/src/components/CheckboxFilter.tsx
@@ -2,17 +2,16 @@ import { Checkbox } from 'hds-react';
 
 type CheckboxFilterProps = {
   checked: boolean;
-  className: string;
   id: string;
   label: string;
   onChange: Function
 }
 
-const CheckboxFilter = ({ checked, className, id, label, onChange }: CheckboxFilterProps) => {
+const CheckboxFilter = ({ checked, id, label, onChange }: CheckboxFilterProps) => {
   return (
     <Checkbox
       checked={checked}
-      className={className}
+      className={'hdbt-search__filter hdbt-search__checkbox'}
       id={id}
       label={label}
       onChange={(event) => onChange(event)}
