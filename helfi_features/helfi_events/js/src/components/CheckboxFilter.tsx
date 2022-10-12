e89c24d5ed112a1364b96ce3d5f1260ff1eee715
Sakari Laaksonen <sakari.laaksonen@siili.com>
2022-10-12 15:38:44 +0300

    fix rebase cache save fumble. Add new build

--- helfi_features/helfi_events/js/src/components/CheckboxFilter.tsx
+++ helfi_features/helfi_events/js/src/components/CheckboxFilter.tsx
@@ -2,39 +2,16 @@ import { Checkbox } from 'hds-react';
 
 type CheckboxFilterProps = {
   checked: boolean;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  className: string;
->>>>>>> fa3170f (UHF-6666: Move filter to own components)
-=======
->>>>>>> dbca7bc (Style changes)
   id: string;
   label: string;
-  onChange: Function
+  onChange: Function;
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 const CheckboxFilter = ({ checked, id, label, onChange }: CheckboxFilterProps) => {
   return (
     <Checkbox
       checked={checked}
       className={'hdbt-search__filter hdbt-search__checkbox'}
-=======
-const CheckboxFilter = ({ checked, className, id, label, onChange }: CheckboxFilterProps) => {
-  return (
-    <Checkbox
-      checked={checked}
-      className={className}
->>>>>>> fa3170f (UHF-6666: Move filter to own components)
-=======
-const CheckboxFilter = ({ checked, id, label, onChange }: CheckboxFilterProps) => {
-  return (
-    <Checkbox
-      checked={checked}
-      className={'hdbt-search__filter hdbt-search__checkbox'}
->>>>>>> dbca7bc (Style changes)
       id={id}
       label={label}
       onChange={(event) => onChange(event)}
