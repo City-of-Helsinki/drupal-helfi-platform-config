bfc458bebbea1427ea9671eee0797048182d1f9b
tuutti <tuutti@iki.fi>
2022-12-08 10:25:42 +0200

    UHF-7219: Stash

---
+++ modules/helfi_media/tests/src/Functional/MediaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\helfi_base_content\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests helfi_media module.
+ *
+ * @group helfi_platform_config
+ */
+class MediaTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'helfi_media',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Make sure front page loads.
+   */
+  public function testFrontPage() : void {
+    $this->drupalGet('<front>');
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
