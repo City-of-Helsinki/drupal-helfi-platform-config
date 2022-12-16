2ab1a971ead2aa0813c3614261548f9acd09e88e
Aarne Wallén <aarne.wallen@siili.com>
2022-12-16 12:22:26 +0200

    UHF-7766: User consent functions abstraction exposed to js.

---
+++ assets/js/user_consent_functions.js
@@ -0,0 +1,12 @@
+(function (Drupal) {
+  'use strict';
+
+  window.chat_user_consent = {
+    retrieveUserConsent: () => (!Drupal.eu_cookie_compliance.hasAgreedWithCategory('chat')),
+    confirmUserConsent: () => {
+      if (Drupal.eu_cookie_compliance.hasAgreedWithCategory('chat')) return;
+      Drupal.eu_cookie_compliance.setAcceptedCategories([ ...Drupal.eu_cookie_compliance.getAcceptedCategories(), 'chat' ]);
+    }
+  };
+
+})(Drupal);
