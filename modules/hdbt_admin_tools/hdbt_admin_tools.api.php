843be50f68f433025156ca2dee691afc895ec231
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-02-14 13:43:26 +0200

    Backported changes from hdbt 4.4.9 and hdbt_admin 1.8.6.

--- modules/hdbt_admin_tools/hdbt_admin_tools.api.php
+++ modules/hdbt_admin_tools/hdbt_admin_tools.api.php
@@ -10,6 +10,8 @@
  * Hooks provided by the Helfi admin tools module.
  */
 
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Modify the image list items for the field preview images.
  *
@@ -19,4 +21,15 @@
  *   Field name which is uses the preview images.
  */
 function hook_design_selection_images_alter(array &$images, string $field_name) {
+}
+
+/**
+ * Alter the CKEditor link dialog form validation.
+ *
+ * @param array $form
+ *   The form.
+ * @param Drupal\Core\Form\FormStateInterface $form_state
+ *   The form state.
+ */
+function hook_helfi_form_editor_link_dialog_alter(array &$form, FormStateInterface &$form_state) {
 }
