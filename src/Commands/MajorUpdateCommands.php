aad7d5b107ff124c6bdb7676c537e5fa4395f574
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-02-22 13:36:56 +0200

    Remove helfi_news_feed settings if the file exists.

--- src/Commands/MajorUpdateCommands.php
+++ src/Commands/MajorUpdateCommands.php
@@ -219,9 +219,15 @@ final class MajorUpdateCommands extends DrushCommands {
     $this->forceEnableModules($modules);
 
     $configExportFolder = \Drupal::root() . '/../conf/cmi';
+    $obsoleteFiles = [
+      'select2_icon.settings.yml',
+      'helfi_news_feed.settings.yml',
+    ];
 
-    if (file_exists($configExportFolder . '/select2_icon.settings.yml')) {
-      unlink($configExportFolder . '/select2_icon.settings.yml');
+    foreach ($obsoleteFiles as $file) {
+      if (file_exists("$configExportFolder/$file")) {
+        unlink("$configExportFolder/$file");
+      }
     }
     $extensions = Yaml::decode(file_get_contents($configExportFolder . '/core.extension.yml'));
     $extensionsConfig = \Drupal::configFactory()->getEditable('core.extension');
