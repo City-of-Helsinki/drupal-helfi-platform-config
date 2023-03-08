a51a55e6b250c38a28eba9259852693148d2ae5d
Jere Ljungberg <jere@mysteerio.fi>
2023-03-08 16:01:54 +0200

    UHF-6848: Add event subscriber to strip boost parameter from index mappings

---
+++ helfi_features/helfi_react_search/src/EventSubscriber/BoostStripper.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\helfi_react_search\EventSubscriber;
+
+use Drupal\elasticsearch_connector\Event\PrepareIndexMappingEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Subscribe for stripping boost parameter from Elasticsearch indices.
+ *
+ * Search API wants to index fields with 'boost' parameter'.
+ * Index-time boosting has been deprecated in newer Elasticsearch versions.
+ * This subscriber strips boost from mapping to prevent errors.
+ */
+class BoostStripper implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      PrepareIndexMappingEvent::PREPARE_INDEX_MAPPING => 'stripBoost',
+    ];
+  }
+
+  /**
+   * Iterate over fields and remove any boosts.
+   *
+   * @param Drupal\elasticsearch_connector\Event\PrepareIndexMappingEvent $event
+   *   Event emitted by elasticsearch_connector.
+   */
+  public function stripBoost(PrepareIndexMappingEvent $event): void {
+    $params = $event->getIndexMappingParams();
+
+    // Bail if nothing to check against.
+    if (!isset($params['body']['properties'])) {
+      return;
+    }
+
+    foreach ($params['body']['properties'] as $key => $property) {
+      if (isset($property['boost'])) {
+        unset($params['body']['properties'][$key]['boost']);
+      }
+    }
+
+    $event->setIndexMappingParams($params);
+  }
+
+}
