3241cc000d86d8c73d6e9d803008763bdcae7e44
rpnykanen <risto.nykanen@druid.fi>
2023-03-30 12:29:09 +0300

    UHF-8090: added and enabled language negotiator

---
+++ src/Plugin/LanguageNegotiation/LanguageNegotiationAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\helfi_platform_config\Plugin\LanguageNegotiation;
+
+use Drupal\language\LanguageNegotiationMethodBase;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class for identifying language from the user preferences.
+ *
+ * @LanguageNegotiation(
+ *   id = \Drupal\helfi_platform_config\Plugin\LanguageNegotiation\LanguageNegotiationAdmin::METHOD_ID,
+ *   weight = -4,
+ *   name = @Translation("Admin"),
+ *   description = @Translation("Follow the user's administration language preference.")
+ * )
+ */
+class LanguageNegotiationAdmin extends LanguageNegotiationMethodBase {
+
+  /**
+   * The language negotiation method id.
+   */
+  const METHOD_ID = 'language-admin-user';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLangcode(Request $request = NULL) {
+    $langcode = NULL;
+
+    if ($this->languageManager && $this->currentUser->isAuthenticated()) {
+      $preferred_langcode = $this->currentUser->getPreferredAdminLangcode();
+      $languages = $this->languageManager->getLanguages();
+      if (!empty($preferred_langcode) && isset($languages[$preferred_langcode])) {
+        $langcode = $preferred_langcode;
+      }
+    }
+
+    return $langcode;
+  }
+
+}
