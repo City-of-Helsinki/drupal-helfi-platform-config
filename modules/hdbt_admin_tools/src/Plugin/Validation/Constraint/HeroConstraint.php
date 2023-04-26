629de3403de23118f1fcdd7ba2af3239bbf10dbc
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-04-26 13:33:12 +0300

    UHF-8128: Added constraint for missing hero block entities in entity forms and enchanced the checks for the hero block to prevent missing H1 elements.

---
+++ modules/hdbt_admin_tools/src/Plugin/Validation/Constraint/HeroConstraint.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\hdbt_admin_tools\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Checks that the hero is present if the boolean value has been enabled.
+ *
+ * @Constraint(
+ *   id = "Hero",
+ *   label = @Translation("Hero is missing.", context = "Validation"),
+ * )
+ */
+class HeroConstraint extends Constraint {
+
+  /**
+   * Message shown for the Hero paragraph.
+   *
+   * @var string
+   */
+  public string $heroRequired = 'Hero paragraph is mandatory if the Hero checkbox has been selected. Either unselect the checkbox or create the hero paragraph by clicking the Add Hero button.';
+
+}
