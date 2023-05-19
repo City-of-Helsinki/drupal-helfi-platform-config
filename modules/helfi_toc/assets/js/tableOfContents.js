14888d7d1deca9802870957293a0f9c3de601279
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-05-19 12:13:51 +0300

    UHF-8389: Converted the table of contents to Vanilla JS.

--- modules/helfi_toc/assets/js/tableOfContents.js
+++ modules/helfi_toc/assets/js/tableOfContents.js
@@ -1,11 +1,11 @@
 'use strict';
 
-(function ($, Drupal) {
+(function (Drupal, once) {
   Drupal.behaviors.table_of_contents = {
     attach: function attach() {
 
       function findAvailableId(name, reserved, anchors, count) {
-        var newName = name;
+        let newName = name;
         if (count > 0) { // Only when headings are not unique on page we want to add counter
           newName += '-' + count;
         }
@@ -20,18 +20,18 @@
         return newName;
       }
 
-      var anchors = [];
-      var tableOfContents = $('#helfi-toc-table-of-contents');
-      var tableOfContentsList = $('#helfi-toc-table-of-contents-list > ul');
-      var mainContent = $('main.layout-main-wrapper');
-      var reservedElems = document.querySelectorAll('[id]');
-      var reserved = []; // Lets list current id's here to avoid creating duplicates
+      const anchors = [];
+      const tableOfContents = document.getElementById('helfi-toc-table-of-contents');
+      const tableOfContentsList = document.querySelector('#helfi-toc-table-of-contents-list > ul');
+      const mainContent = document.querySelector('main.layout-main-wrapper');
+      const reservedElems = document.querySelectorAll('[id]');
+      const reserved = []; // Let's list current id's here to avoid creating duplicates
       reservedElems.forEach(function (elem) {
         reserved.push(elem.id);
       });
 
       // Do not include sidebar H2, Table of contents H2 or cookie compliance warnings.
-      var exclusions = '' +
+      const exclusions = '' +
         ':not(.layout-sidebar-first *)' +
         ':not(.layout-sidebar-second *)' +
         ':not(.tools__container *)' +
@@ -39,22 +39,21 @@
         ':not(#helfi-toc-table-of-contents *)' +
         ':not(.embedded-content-cookie-compliance *)' +
         ':not(.react-and-share-cookie-compliance *)' +
-        ':not(.handorgel__header)'; // Accordion headings get their id's overridden by handorgel script
+        ':not(.handorgel__header)';
 
-      var titleComponents = [
+      const titleComponents = [
         'h2'+exclusions,
         'h3'+exclusions,
         'h4'+exclusions,
         'h5'+exclusions,
         'h6'+exclusions,
-        '.handorgel__header > button', // Instead of targeting accordion headings, lets target the button inside them.
+        '.handorgel__header > button',
       ];
 
       // Craft table of contents.
-      $(titleComponents.join(','), mainContent)
-        .once()
-        .each(function (index) {
-          const name = this.textContent
+      once('table-of-contents', titleComponents.join(','), mainContent)
+        .forEach(function (content) {
+          const name = content.textContent
             .toLowerCase()
             .trim()
             .replace(/ä/gi, 'a')
@@ -63,36 +62,39 @@
             .replace(/\W/g, '-')
             .replace(/-(\d+)$/g, '_$1');
 
-          let nodeName = this.nodeName.toLowerCase();
+          let nodeName = content.nodeName.toLowerCase();
           if (nodeName === 'button') {
-            nodeName = this.parentElement.nodeName.toLowerCase();
+            nodeName = content.parentElement.nodeName.toLowerCase();
           }
 
-          let anchorName;
-          if (this.id) {
-            anchorName = this.id;
-          } else {
-            anchorName = findAvailableId(name, reserved, anchors, 0);
-          }
+          const anchorName = content.id
+            ? content.id
+            : findAvailableId(name, reserved, anchors, 0);
+
           anchors.push(anchorName);
 
           // Create table of contents if component is enabled.
-          if (tableOfContentsList.length > 0 && nodeName === "h2") {
-            tableOfContentsList.append(
-              '<li class="table-of-contents__item"><a class="table-of-contents__link' +
-              '" href="#' +
-              anchorName +
-              '">' +
-              this.textContent.trim() +
-              '</a></li>'
-            );
+          if (tableOfContentsList && nodeName === "h2") {
+            let listItem = document.createElement('li');
+            listItem.classList.add('table-of-contents__item');
+
+            let link = document.createElement('a');
+            link.classList.add('table-of-contents__link');
+            link.href = '#' + anchorName;
+            link.textContent = content.textContent.trim();
+
+            listItem.appendChild(link);
+            tableOfContentsList.appendChild(listItem);
           }
           // Create anchor links.
-          $(this).attr('id', anchorName);
+          content.setAttribute('id', anchorName);
         });
 
       // Remove loading text.
-      $('.js-remove', tableOfContents).remove();
+      const removeElements = tableOfContents.querySelectorAll('.js-remove');
+      removeElements.forEach(function (element) {
+        element.remove();
+      });
     },
   };
-})(jQuery, Drupal);
+})(Drupal, once);
