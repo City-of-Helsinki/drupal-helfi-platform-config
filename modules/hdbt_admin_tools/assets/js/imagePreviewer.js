ed8c38987595c3c72aa788b63d273e389c319538
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-06-06 23:32:43 +0300

    UHF-8508: Handle image preview event listeners in a separate function.

--- modules/hdbt_admin_tools/assets/js/imagePreviewer.js
+++ modules/hdbt_admin_tools/assets/js/imagePreviewer.js
@@ -35,68 +35,75 @@
     imageXOffset: 32,
   };
 
-  function imagePreviewer(selector, configuration) {
+  // Handle image preview event listeners.
+  function imagePreviewHandler(selector, imageID, config) {
+    return function(event) {
+      if (!event.target.matches(selector)) return;
+      // Rest of the code for handling the mouseenter event
+
+      const image = event.target.dataset.hoverImage;
+      const title = event.target.dataset.hoverTitle || '';
+      const description = event.target.dataset.hoverDescription || '';
+
+      let previewWrapper = document.createElement('p');
+      previewWrapper.id = imageID;
+      previewWrapper.className = 'image-previewer__image-wrapper';
+
+      let img = document.createElement('img');
+      img.className = 'image-previewer__image';
+      img.width = 723;
+      img.height = 407;
+      img.src = image;
+      img.alt = title;
+
+      let titleSpan = document.createElement('span');
+      titleSpan.className = 'image-previewer__title';
+      titleSpan.textContent = title;
+
+      let descriptionSpan = document.createElement('span');
+      descriptionSpan.className = 'image-previewer__description';
+      descriptionSpan.textContent = description;
+
+      previewWrapper.appendChild(img);
+      previewWrapper.appendChild(titleSpan);
+      previewWrapper.appendChild(descriptionSpan);
+
+      document.body.appendChild(previewWrapper);
+
+      const imageTemplate = document.getElementById(imageID);
+      imageTemplate.style.top = (event.pageY - config.imageYOffset) + 'px';
+      imageTemplate.style.left = (event.pageX + config.imageXOffset) + 'px';
+
+      function handleMouseMove(event) {
+        event.stopImmediatePropagation();
+        let dp = document.getElementById(imageID);
+        let height = dp.offsetHeight;
+        dp.style.top = (event.pageY - config.imageYOffset - height) + 'px';
+        dp.style.left = (event.pageX + config.imageXOffset) + 'px';
+      }
+
+      function handleMouseLeave() {
+        let element = document.getElementById(imageID);
+        if (element) {
+          element.style.display = 'none';
+          element.parentNode.removeChild(element);
+        }
+      }
+
+      event.target.addEventListener('mousemove', handleMouseMove);
+      event.target.addEventListener('mouseleave', handleMouseLeave);
+    }
+  }
+
+  function imagePreviewer(selector, configuration = {},  action = 'open') {
     let config = Object.assign({}, _config, configuration);
     const imageID = randomID('image-previewer', 10);
 
     const elements = document.querySelectorAll(selector);
     elements.forEach(function (element) {
-      element.addEventListener('mouseenter', function (event) {
-        if (!event.target.matches(selector)) return;
-        // Rest of the code for handling the mouseenter event
-
-        const image = event.target.dataset.hoverImage;
-        const title = event.target.dataset.hoverTitle || '';
-        const description = event.target.dataset.hoverDescription || '';
-
-        let previewWrapper = document.createElement('p');
-        previewWrapper.id = imageID;
-        previewWrapper.className = 'image-previewer__image-wrapper';
-
-        let img = document.createElement('img');
-        img.className = 'image-previewer__image';
-        img.width = '723';
-        img.height = '407';
-        img.src = image;
-        img.alt = title;
-
-        let titleSpan = document.createElement('span');
-        titleSpan.className = 'image-previewer__title';
-        titleSpan.textContent = title;
-
-        let descriptionSpan = document.createElement('span');
-        descriptionSpan.className = 'image-previewer__description';
-        descriptionSpan.textContent = description;
-
-        previewWrapper.appendChild(img);
-        previewWrapper.appendChild(titleSpan);
-        previewWrapper.appendChild(descriptionSpan);
-
-        document.body.appendChild(previewWrapper);
-
-        const imageTemplate = document.getElementById(imageID);
-        imageTemplate.style.top = (event.pageY - config.imageYOffset) + 'px';
-        imageTemplate.style.left = (event.pageX + config.imageXOffset) + 'px';
-
-        function handleMouseMove(event) {
-          event.stopImmediatePropagation();
-          let dp = document.getElementById(imageID);
-          let height = dp.offsetHeight;
-          dp.style.top = (event.pageY - config.imageYOffset - height) + 'px';
-          dp.style.left = (event.pageX + config.imageXOffset) + 'px';
-        }
-
-        function handleMouseLeave() {
-          let element = document.getElementById(imageID);
-          if (element) {
-            element.style.display = 'none';
-            element.parentNode.removeChild(element);
-          }
-        }
-
-        event.target.addEventListener('mousemove', handleMouseMove);
-        event.target.addEventListener('mouseleave', handleMouseLeave);
-      });
+      return action === 'open'
+        ? element.addEventListener('mouseenter', imagePreviewHandler(selector, imageID, config))
+        : element.removeEventListener('mouseenter', imagePreviewHandler(selector, imageID, config));
     });
 
     document.addEventListener('ajaxComplete', function () {
@@ -105,13 +112,6 @@
         element.style.display = 'none';
         element.parentNode.removeChild(element);
       }
-    });
-
-    window.addEventListener('select2:closing', function () {
-      let elements = document.querySelectorAll('[id^="image-previewer-"]');
-      elements.forEach(function (element) {
-        element.parentNode.removeChild(element);
-      });
     });
   }
 
