72c92fe5c360af7b54cd6a4ff90a8cc03500f8bb
Tero Elonen <tero.elonen@druid.fi>
2023-07-03 12:05:29 +0300

    UHF-1781: Change hero paragraphs field_hero_design to use function for the values

---
+++ modules/helfi_paragraphs_hero/helfi_paragraphs_hero.api.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * helfi_paragraphs_hero hooks.
+ */
+
+/**
+ * @file
+ * Hooks provided by the helfi_paragraphs_hero module.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Alter the Hero design selection array.
+ *
+ * @param array $designs
+ *   The designs for the hero.
+ */
+function hook_helfi_hero_design_alter(array &$designs) {
+}
