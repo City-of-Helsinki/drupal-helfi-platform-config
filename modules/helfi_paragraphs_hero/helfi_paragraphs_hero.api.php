5a384a79aa7619cfee2d13c9611ad254e02b326c
Tero Elonen <tero.elonen@druid.fi>
2023-07-03 12:41:55 +0300

    UHF-1781: Fix PHP coding standard issues

--- modules/helfi_paragraphs_hero/helfi_paragraphs_hero.api.php
+++ modules/helfi_paragraphs_hero/helfi_paragraphs_hero.api.php
@@ -2,15 +2,13 @@
 
 /**
  * @file
- * helfi_paragraphs_hero hooks.
+ * Module helfi_paragraphs_hero hooks.
  */
 
 /**
  * @file
  * Hooks provided by the helfi_paragraphs_hero module.
  */
-
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Alter the Hero design selection array.
