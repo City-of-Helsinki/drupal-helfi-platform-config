e478e43dd0696643ad4895e65a025bdb3be874b8
rpnykanen <risto.nykanen@druid.fi>
2023-07-10 12:01:31 +0300

    UHF-8441: tweaked migrate yml, added migration event subscriber

---
+++ modules/helfi_paragraphs_hearings/src/EventSubscriber/HearingMigrationSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\helfi_paragraph_hearings\EventSubscriber;
+
+use Drupal\migrate\Event\MigrateEvents;
+use Drupal\migrate\Event\MigratePostRowSaveEvent;
+use Drupal\node\Entity\Node;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class HearingMigrationSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      MigrateEvents::POST_ROW_SAVE => 'handleTranslations',
+    ];
+  }
+
+  /**
+   * Handle hearing translations.
+   *
+   * @param MigratePostRowSaveEvent $event
+   * @return void
+   */
+  public function handleTranslations(MigratePostRowSaveEvent $event): void {
+    $row = $event->getRow();
+    $data = $event->getDestinationIdValues();
+    $node = Node::create([]);
+
+    foreach (['en', 'sv'] as $langcode) {
+
+      if (!in_array("title_$langcode", $data)) {
+        continue;
+      }
+
+      if ($node->hasTranslation($langcode)) {
+        $translation = $node->getTranslation($langcode);
+      }
+    }
+  }
+
+}
