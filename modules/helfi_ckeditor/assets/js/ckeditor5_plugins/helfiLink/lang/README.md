58d164bc90f237fb1ffdd97792ec2eafd0859478
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-07-26 16:17:05 +0300

    UHF-8386: Added Quote CKEditor5 plugin.

--- modules/helfi_ckeditor/assets/js/ckeditor5_plugins/helfiLink/lang/README.md
+++
@@ -1,2 +0,0 @@
-Do not delete the translations directory and .po files
-as the ckeditor5-dev-translations will assume these translation files exist.
---
+++ modules/helfi_ckeditor/assets/js/ckeditor5_plugins/helfiQuote/src/ui/helfiQuoteCommand.js
@@ -0,0 +1,104 @@
+/**
+ * @file defines helfiQuoteCommand, which is executed when the quote toolbar
+ * button is pressed.
+ */
+import { Command } from 'ckeditor5/src/core';
+
+export default class HelfiQuoteCommand extends Command {
+
+  /**
+   * Executes the command.
+   * Insert <helfiQuote>*</helfiQuote> at the current selection position
+   * in a way that will result in creating a valid model structure.
+   *
+   * @param {string} quoteText The quote text.
+   * @param {string} author The Source / author.
+   */
+  execute({ quoteText, author}) {
+    const { model } = this.editor;
+
+    model.change( writer => {
+      if ( !quoteText ) { return; }
+      model.insertContent(this._createQuote(writer, quoteText, author));
+    } );
+  }
+
+  /**
+   * @inheritDoc
+   */
+  refresh() {
+    const { model } = this.editor;
+    const { selection } = model.document;
+
+    // Determine if the cursor (selection) is in a position where adding a
+    // helfiQuote is permitted. This is based on the schema of the model(s)
+    // currently containing the cursor.
+    const allowedIn = model.schema.findAllowedParent(
+      selection.getFirstPosition(),
+      'helfiQuote',
+    );
+
+    // If the cursor is not in a location where a helfiQuote can be added,
+    // return null so the quote toolbar button cannot be clicked.
+    this.isEnabled = allowedIn !== null;
+
+    // Set value based on selection to set it as default value for the
+    // new quote.
+    // this.value = this._getValueFromFirstAllowedNode();
+  }
+
+  /**
+   * Create the helfiQuote element for the editor.
+   *
+   * @param {object} writer The model writer.
+   * @param {string} quoteText The quote text.
+   * @param {string} author The Source / author.
+   * @return {*} Returns the element to be added to the editor.
+   */
+  _createQuote(writer, quoteText, author) {
+    const helfiQuote = writer.createElement('helfiQuote');
+    const helfiQuoteText = writer.createElement('helfiQuoteText');
+    const helfiQuoteFooter = writer.createElement('helfiQuoteFooter');
+    const helfiQuoteFooterCite = writer.createElement('helfiQuoteFooterCite');
+
+    // Append the quote text and author elements to the helfiQuote.
+    writer.append(helfiQuoteText, helfiQuote);
+    writer.insertText( quoteText, helfiQuoteText );
+
+    // Do not add the author if it's not available.
+    if (author) {
+      writer.append(helfiQuoteFooter, helfiQuote);
+      writer.append(helfiQuoteFooterCite, helfiQuoteFooter);
+      writer.insertText( author, helfiQuoteFooterCite );
+    }
+
+    // Return the element to be added to the editor.
+    return helfiQuote;
+  }
+
+  /**
+   * Returns the attribute value of the first node in the selection that allows the attribute.
+   * For a collapsed selection it returns the selection attribute.
+   *
+   * @return {string|false} The attribute value or false.
+   */
+  _getValueFromFirstAllowedNode() {
+    const model = this.editor.model;
+    const schema = model.schema;
+    const selection = model.document.selection;
+
+    if ( selection.isCollapsed ) {
+      return selection.getAttribute( 'helfiLanguageSelector' ) || false;
+    }
+
+    for ( const range of selection.getRanges() ) {
+      for ( const item of range.getItems() ) {
+        if ( schema.checkAttribute( item, 'helfiLanguageSelector' ) ) {
+          return item.getAttribute( 'helfiLanguageSelector' ) || false;
+        }
+      }
+    }
+
+    return false;
+  }
+}
