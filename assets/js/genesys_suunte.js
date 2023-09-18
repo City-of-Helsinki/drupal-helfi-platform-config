8f6df7dd5ad0cf789cd931e8e289fd668046176f
mmyllynen <4696381+dire@users.noreply.github.com>
2023-09-18 10:57:46 +0300

    UHF-X: updated suunte chat domains.

--- assets/js/genesys_suunte.js
+++ assets/js/genesys_suunte.js
@@ -128,8 +128,8 @@ var gcReturnSessionId = '';
       /* CHAT START BUTTON ICONS */
       var helFiChat_button = "";
       var helFiChat_localization =
-        "https://www.hel.fi/gms/sote/testpages/chat-suunte-fi.json";
-      var helFiChat_service = "SUUNTE"; //SUUNTE
+        "https://chat-proxy.hel.fi/gms/sote/testpages/chat-fi.json";
+      var helFiChat_service = "TESTISIVU_TESTI"; //SUUNTE
       var helFiChat_language = "fi";
       var helfiChat_GUI_lang = helFiChat_language;
       var helFiChat_title = "Hammashoidon chat";
@@ -145,10 +145,10 @@ var gcReturnSessionId = '';
         );
         var currentPage = window.location;
         var shibbolethString =
-          "https://www.hel.fi/chat/tunnistus/Shibboleth.sso/KAPALogin?";
+          "https://chat-proxy.hel.fi/chat/tunnistus/Shibboleth.sso/KAPALogin?";
         shibbolethString += "target=";
         shibbolethString +=
-          "https://www.hel.fi/chat/tunnistus/MagicPage/ReturnProcessor";
+          "https://chat-proxy.hel.fi/chat/tunnistus/MagicPage/ReturnProcessor";
         /*
               shibbolethString += "%3ForigPage%3D" + "https://www.hel.fi/helsinki/fi/sosiaali-ja-terveyspalvelut/terveyspalvelut/hammashoito/transfer?dir%3Din%26gcLoginButtonState%3D1%26errcode%3d0";
               */
@@ -253,7 +253,7 @@ var gcReturnSessionId = '';
           preload: ["webchat"],
         },
         webchat: {
-          dataURL: "https://asiointi.hel.fi/gms/sote/genesys/2/chat/prod",
+          dataURL: "https://chat-proxy.hel.fi/gms/sote/genesys/2/chat/prod",
           confirmFormCloseEnabled: false,
           userData: {
             service: helFiChat_service,
