12f46401f8ac9fa17d36d672202aa02b2b43e003
rpnykanen <risto.nykanen@druid.fi>
2023-09-27 11:00:22 +0300

    extra space

--- modules/helfi_paragraphs_hearings/src/Plugin/ExternalEntities/StorageClient/Hearings.php
+++ modules/helfi_paragraphs_hearings/src/Plugin/ExternalEntities/StorageClient/Hearings.php
@@ -137,11 +137,11 @@ final class Hearings extends ExternalEntityStorageClientBase {
         'langcode' => $selectedLangcode,
         'existing_translations' => implode(',', $existingTranslations),
       ];
-      
+
       $item['title'] = $hearing['title'][$selectedLangcode] ?? $hearing['title']['fi'] ?? '';
       $item['abstract'] = $hearing['abstract'][$selectedLangcode] ?? $hearing['abstract']['fi'] ?? '';
       $item['main_image_caption'] = $hearing['main_image']['caption'][$selectedLangcode]
-        ??  $hearing['main_image']['caption']['fi']
+        ?? $hearing['main_image']['caption']['fi']
         ?? '';
 
       $data[] = $item;
