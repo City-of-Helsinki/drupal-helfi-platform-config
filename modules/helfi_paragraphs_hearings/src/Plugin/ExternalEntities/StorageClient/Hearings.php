80509c493eb44942bd8f0ae5e32e16fe34e2575a
rpnykanen <risto.nykanen@druid.fi>
2023-09-27 11:00:09 +0300

    add extra checks in case api data is not as good as it should

--- modules/helfi_paragraphs_hearings/src/Plugin/ExternalEntities/StorageClient/Hearings.php
+++ modules/helfi_paragraphs_hearings/src/Plugin/ExternalEntities/StorageClient/Hearings.php
@@ -137,15 +137,12 @@ final class Hearings extends ExternalEntityStorageClientBase {
         'langcode' => $selectedLangcode,
         'existing_translations' => implode(',', $existingTranslations),
       ];
-
-      $caption = '';
-      if (!isset($hearing['main_image']['caption'][$selectedLangcode])) {
-        $caption = $hearing['main_image']['caption']['fi'] ?? '';
-      }
-
-      $item['title'] = $hearing['title'][$selectedLangcode] ?? $hearing['title']['fi'];
-      $item['abstract'] = $hearing['abstract'][$selectedLangcode] ?? $hearing['abstract']['fi'];
-      $item['main_image_caption'] = $caption;
+      
+      $item['title'] = $hearing['title'][$selectedLangcode] ?? $hearing['title']['fi'] ?? '';
+      $item['abstract'] = $hearing['abstract'][$selectedLangcode] ?? $hearing['abstract']['fi'] ?? '';
+      $item['main_image_caption'] = $hearing['main_image']['caption'][$selectedLangcode]
+        ??  $hearing['main_image']['caption']['fi']
+        ?? '';
 
       $data[] = $item;
     }
