d40a2645194ecc735bde9cd121e19725e2b46155
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2023-10-20 13:28:16 +0300

    UHF-8509: Decoupled dependency to SelectIcon().

--- modules/helfi_ckeditor/src/Plugin/CKEditor5Plugin/HelfiLink.php
+++ modules/helfi_ckeditor/src/Plugin/CKEditor5Plugin/HelfiLink.php
@@ -124,11 +124,49 @@ class HelfiLink extends CKEditor5PluginDefault implements CKEditor5PluginElement
     $config = $static_plugin_config;
     $config += [
       'link' => [
-        'loadedIcons' => SelectIcon::loadIcons() ?? [],
+        'loadedIcons' => $this->loadIcons(),
         'whiteListedDomains' => $this->internalDomainResolver->getDomains(),
       ],
     ];
     return $config;
+  }
+
+  /**
+   * Load icons array.
+   *
+   * Load icons either from cache or load them based on the data received from
+   * json-file which is saved in configuration.
+   *
+   * TODO: This is duplicated to SelectIcon::loadIcons(), needs refactoring.
+   *
+   * @return array
+   *   Returns an array of icons or empty array.
+   */
+  private function loadIcons(): array {
+    if ($icons = \Drupal::cache()->get('select_icon_cache')) {
+      return $icons->data;
+    }
+    else {
+      $icons = [];
+      $config = \Drupal::getContainer()->get('config.factory')->getEditable('hdbt_admin_tools.site_settings');
+      $json_path = \Drupal::root() . $config->get('path_to_json');
+
+      if (!$data = file_get_contents($json_path)) {
+        \Drupal::messenger()
+          ->addWarning('Failed to load icons due to missing icons data. Verify that the "path_to_json" key contains correct information in the hdbt_admin_tools.site_settings configuration. Current value: @current_value', [
+            '@current_value' => $json_path,
+          ]);
+
+        return [];
+      }
+      $json = json_decode($data, TRUE);
+
+      if (is_array($json) && !empty($json)) {
+        $icons = array_combine($json, $json);
+        \Drupal::cache()->set('select_icon_cache', $icons);
+      }
+    }
+    return $icons;
   }
 
 }
