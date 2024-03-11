84d220858785541e35bb2bb41d6071096095dc6c
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-03-11 06:39:48 +0200

    UHF-9530: Add field_filter_keywords to paragraph event_list

---
+++ modules/helfi_react_search/src/Entity/EventList.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_react_search\Entity;
+
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\ParagraphInterface;
+
+/**
+ * Bundle class for Hero -paragraph.
+ */
+class EventList extends Paragraph implements ParagraphInterface {
+
+  /**
+   * Get list of enabled filter keywords.
+   *
+   * @return \Drupal\taxonomy\TermInterface[]
+   *   Enabled keyword.
+   */
+  public function getFilterKeywords() : array {
+    $keywords = [];
+
+    /** @var \Drupal\Core\Field\EntityReferenceFieldItemListInterface $field_keywords */
+    $field_keywords = $this->get('field_filter_keywords');
+
+    /** @var \Drupal\taxonomy\TermInterface $term */
+    foreach ($field_keywords->referencedEntities() as $term) {
+      $keywords[] = $term;
+    }
+
+    return $keywords;
+  }
+
+}
