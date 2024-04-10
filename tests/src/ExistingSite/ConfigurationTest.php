e4f96c7334e784eccb1d96d22f846d10854ff9f2
tuutti <tuutti@iki.fi>
2024-04-10 09:04:06 +0300

    UHF-9935: Test configuration against actual schema

---
+++ tests/src/ExistingSite/ConfigurationTest.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_platform_config\ExistingSite;
+
+use Drupal\Component\Serialization\Yaml;
+use Drupal\Core\Config\TypedConfigManagerInterface;
+use Drupal\Core\Extension\ModuleExtensionList;
+use Drupal\Tests\helfi_api_base\Functional\ExistingSiteTestBase;
+
+/**
+ * Scans bundled configuration.
+ *
+ * @group helfi_platform_config
+ */
+class ConfigurationTest extends ExistingSiteTestBase {
+
+  /**
+   * The extension list.
+   *
+   * @var \Drupal\Core\Extension\ModuleExtensionList
+   */
+  private ModuleExtensionList $extensionList;
+
+  /**
+   * The typed config manager.
+   *
+   * @var \Drupal\Core\Config\TypedConfigManagerInterface
+   */
+  private TypedConfigManagerInterface $typedConfigManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() : void {
+    parent::setUp();
+
+    $this->extensionList = $this->container->get('extension.list.module');
+    $this->typedConfigManager = $this->container->get('config.typed');
+
+    /** @var \Drupal\Core\Extension\ModuleInstallerInterface $moduleHandler */
+    $moduleInstaller = $this->container->get('module_installer');
+    // Make sure all submodules are enabled.
+    $moduleInstaller->install($this->getSubModules());
+  }
+
+  /**
+   * Gets the submodules.
+   *
+   * @return array
+   *   The submodules list.
+   */
+  private function getSubModules() : array {
+    $path = $this->extensionList->getPath('helfi_platform_config');
+
+    $modules = [];
+    /** @var \DirectoryIterator $item */
+    foreach (new \DirectoryIterator($path . '/modules') as $item) {
+      if (!$item->isDir() || $item->isDot()) {
+        continue;
+      }
+      $modules[] = $item->getBasename();
+    }
+    return $modules;
+  }
+
+  /**
+   * Asserts that all module configuration contains 'uuid' key.
+   *
+   * @param string $module
+   *   The module to check.
+   */
+  private function assertModuleUuids(string $module) : void {
+    $path = $this->extensionList->getPath($module);
+
+    foreach (['install', 'optional'] as $type) {
+      $configPath = sprintf('%s/config/%s', $path, $type);
+
+      if (!is_dir($configPath)) {
+        continue;
+      }
+
+      /** @var \DirectoryIterator $file */
+      foreach (new \DirectoryIterator($configPath) as $file) {
+        if ($file->getExtension() !== 'yml') {
+          continue;
+        }
+        $configFileName = str_replace('.yml', '', $file->getFilename());
+        $definition = $this->typedConfigManager->get($configFileName)
+          ->getDataDefinition();
+
+        // Skip configuration that doesn't require UUID.
+        if (!isset($definition['mapping']['uuid'])) {
+          continue;
+        }
+        $yaml = Yaml::decode(file_get_contents($file->getPathname()));
+        $this->assertArrayHasKey('uuid', $yaml, "[{$file->getPathname()}] is missing UUID");
+      }
+    }
+  }
+
+  /**
+   * Asserts module configuration.
+   */
+  public function testModuleConfiguration() : void {
+    foreach ($this->getSubModules() as $module) {
+      $this->assertModuleUuids($module);
+    }
+  }
+
+}
--- tests/src/Kernel/ConfigurationTest.php
+++ tests/src/Kernel/ConfigurationTest.php
@@ -32,100 +32,6 @@ class ConfigurationTest extends KernelTestBase {
   }
 
   /**
-   * Asserts the matching file has a UUID.
-   *
-   * Finds the matching configuration file for given translation file.
-   *
-   * For example, '/config/install/language/fi/field.yml' file should have
-   * a matching configuration file with the same name under '/config/install'
-   * or '/config/optional' folder.
-   *
-   * @param string $module
-   *   The module this file belongs to.
-   * @param string $filename
-   *   The language file.
-   */
-  private function assertMatchingFileHasUuid(string $module, string $filename) : void {
-    $path = $this->extensionList->getPath($module);
-
-    // The matching file can be under /optional or /install folder, make sure
-    // to check both.
-    $matchingFile = NULL;
-    /** @var \DirectoryIterator $item */
-    foreach (['optional', 'install'] as $type) {
-      $file = sprintf('%s/config/%s/%s', $path, $type, $filename);
-
-      if (!file_exists($file)) {
-        continue;
-      }
-      $matchingFile = $file;
-    }
-
-    if (!$matchingFile) {
-      throw new \InvalidArgumentException(
-        sprintf('Failed to find a pair for: %s', $filename)
-      );
-    }
-
-    $yaml = Yaml::decode(file_get_contents($matchingFile));
-    $this->assertArrayHasKey('uuid', $yaml, "[{$matchingFile}] is missing UUID");
-  }
-
-  /**
-   * Asserts that all module configuration contains 'uuid' key.
-   *
-   * @param string $module
-   *   The module to check.
-   */
-  private function assertModuleUuids(string $module) : void {
-    $path = $this->extensionList->getPath($module);
-
-    foreach (['install', 'optional'] as $type) {
-      $configPath = sprintf('%s/config/%s', $path, $type);
-
-      if (!is_dir($configPath . '/language')) {
-        continue;
-      }
-
-      foreach (['fi', 'sv'] as $language) {
-        $languagePath = sprintf('%s/language/%s', $configPath, $language);
-
-        if (!is_dir($languagePath)) {
-          continue;
-        }
-
-        /** @var \DirectoryIterator $file */
-        foreach (new \DirectoryIterator($languagePath) as $file) {
-          if ($file->getExtension() !== 'yml') {
-            continue;
-          }
-          $this->assertMatchingFileHasUuid($module, $file->getFilename());
-        }
-      }
-    }
-  }
-
-  /**
-   * Gets the submodules.
-   *
-   * @return array
-   *   The submodules list.
-   */
-  private function getSubModules() : array {
-    $path = $this->extensionList->getPath('helfi_platform_config');
-
-    $modules = [];
-    /** @var \DirectoryIterator $item */
-    foreach (new \DirectoryIterator($path . '/modules') as $item) {
-      if (!$item->isDir() || $item->isDot()) {
-        continue;
-      }
-      $modules[] = $item->getBasename();
-    }
-    return $modules;
-  }
-
-  /**
    * Makes sure configuration does not contain '_core' key.
    */
   public function testModuleCoreKey() : void {
@@ -146,15 +52,6 @@ class ConfigurationTest extends KernelTestBase {
       }
       $content = Yaml::decode(file_get_contents($file));
       $this->assertArrayNotHasKey('_core', $content, "[{$file}] has _core key");
-    }
-  }
-
-  /**
-   * Makes sure necessary configuration has a UUID.
-   */
-  public function testModuleUuid() : void {
-    foreach ($this->getSubModules() as $module) {
-      $this->assertModuleUuids($module);
     }
   }
 
