6792baccc1a5549113d831bd8f6e5f213725801c
Juho Lehmonen <juho.lehmonen@siili.com>
2024-04-24 11:08:09 +0300

    UHF-9551: Always use load_ace.js script, remove URL field from block config.

--- src/Plugin/Block/TeliaAceWidget.php
+++ src/Plugin/Block/TeliaAceWidget.php
@@ -22,14 +22,6 @@ class TeliaAceWidget extends BlockBase {
     $form = parent::blockForm($form, $form_state);
     $config = $this->getConfiguration();
 
-    $form['script_url'] = [
-      '#type' => 'textfield',
-      '#required' => TRUE,
-      '#title' => $this->t('Script URL'),
-      '#description' => $this->t('URL to the chat JS library without the domain, for example: /wds/instances/J5XKjqJt/load_ace.js'),
-      '#default_value' => $config['script_url'] ?? '/wds/instances/J5XKjqJt/load_ace.js',
-    ];
-
     $form['chat_id'] = [
       '#type' => 'textfield',
       '#required' => TRUE,
@@ -45,7 +37,6 @@ class TeliaAceWidget extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $formState) {
-    $this->configuration['script_url'] = $formState->getValue('script_url');
     $this->configuration['chat_id'] = $formState->getValue('chat_id');
   }
 
@@ -57,32 +48,10 @@ class TeliaAceWidget extends BlockBase {
     $build = [];
 
     $config = $this->getConfiguration();
-    $base_url = 'https://wds.ace.teliacompany.com';
-    $script_url = $base_url . $config['script_url'];
     $chat_id = 'humany_' . $config['chat_id'];
-
-    if ($script_url === 'https://wds.ace.teliacompany.com/wds/instances/J5XKjqJt/load_ace.js') {
-      $attached = [
-        'library' => ['helfi_platform_config/telia_ace_widget_loadjs'],
-      ];
-    }
-    else {
-      $attached = [
-        'library' => ['helfi_platform_config/telia_ace_widget'],
-        'html_head' => [
-          [
-            [
-              '#tag' => 'script',
-              '#attributes' => [
-                'defer' => TRUE,
-                'type' => 'text/javascript',
-                'src' => $script_url,
-              ],
-            ], 'telia_ace_script',
-          ],
-        ],
-      ];
-    }
+    $attached = [
+      'library' => ['helfi_platform_config/telia_ace_widget_loadjs'],
+    ];
 
     $build['ibm_chat_app'] = [
       'button' => [
