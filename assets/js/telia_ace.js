f11ec95cf4d18f4965021fb635a23f290c8ea9b9
Juho Lehmonen <juho.lehmonen@siili.com>
2024-06-19 11:25:20 +0300

    UHF-10161: ESLint fixes for telia_ace.js.

--- assets/js/telia_ace.js
+++ assets/js/telia_ace.js
@@ -1,51 +1,32 @@
+// eslint-disable-next-line func-names
 (function (Drupal, drupalSettings) {
-  'use strict';
-
-  Drupal.behaviors.chat_leijuke = {
-    attach: function (context, settings) {
-      const teliaAceData = drupalSettings.telia_ace_data;
-
-      setTimeout(() => {
-        // Only load any leijuke once, in case of ajax triggers.
-        if (teliaAceData.initialized) {
-          console.warn(`Already initialized Telia ACE script!`);
-          return;
-        }
-
-        const teliaAce = new TeliaAceLeijuke(teliaAceData, new EuCookieManager);
-        teliaAce.init();
-        drupalSettings.telia_ace_data.initialized = true;
-      });
-    }
-  }
-
-  class EuCookieManager {
-    cookieCheck() {
-      return Drupal.eu_cookie_compliance.hasAgreedWithCategory('chat');
-    }
-    cookieSet() {
-      if (Drupal.eu_cookie_compliance.hasAgreedWithCategory('chat')) return;
-      Drupal.eu_cookie_compliance.setAcceptedCategories([ ...Drupal.eu_cookie_compliance.getAcceptedCategories(), 'chat' ]);
-    }
-  }
-
   class TeliaAceLeijuke {
-    constructor(teliaAceData, extCookieManager) {
-      this.extCookieManager = extCookieManager;
+    constructor(teliaAceData) {
       this.static = {
-        chat_id: teliaAceData.chat_id,
+        chatId: teliaAceData.chat_id,
         selector: `leijuke_${teliaAceData.chat_id}`,
-        chat_title: teliaAceData.chat_title,
-        script_url: teliaAceData.script_url,
-        script_sri: teliaAceData.script_sri
-      }
+        chatTitle: teliaAceData.chat_title,
+        scriptUrl: teliaAceData.script_url,
+        scriptSri: teliaAceData.script_sri
+      };
       this.state = {
-        cookies: extCookieManager.cookieCheck(),
+        cookies: this.cookieCheck(),
         chatLoading: false,
         chatLoaded: false,
         chatOpened: false,
         busy: false
       };
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    cookieCheck() {
+      return Drupal.eu_cookie_compliance.hasAgreedWithCategory('chat');
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    cookieSet() {
+      if (Drupal.eu_cookie_compliance.hasAgreedWithCategory('chat')) return;
+      Drupal.eu_cookie_compliance.setAcceptedCategories([ ...Drupal.eu_cookie_compliance.getAcceptedCategories(), 'chat' ]);
     }
 
     init() {
@@ -59,27 +40,30 @@
     }
 
     loadChat() {
-      const { script_url, script_sri } = this.static;
+      const { scriptUrl, scriptSri } = this.static;
       const leijuke = this;
-      let chatScript = document.createElement('script');
-      chatScript.src = script_url;
-      chatScript.type = "text/javascript";
+      const chatScript = document.createElement('script');
+      chatScript.src = scriptUrl;
+      chatScript.type = 'text/javascript';
       chatScript.setAttribute('async', '');
 
-      if (script_sri) {
-        chatScript.integrity = script_sri;
+      if (scriptSri) {
+        chatScript.integrity = scriptSri;
         chatScript.crossOrigin = 'anonymous';
       }
 
+      // eslint-disable-next-line func-names
       chatScript.onload = function() {
         leijuke.loaded();
-      }
+      };
+
+      // eslint-disable-next-line func-names
       chatScript.onerror = function() {
-        console.error('Failed to load script ' + script_url);
+        // console.error('Failed to load script ' + scriptUrl);
       };
 
       // Insert chatScript into head
-      let head = document.querySelector('head');
+      const head = document.querySelector('head');
       head.appendChild(chatScript);
       this.state.chatLoading = true;
     }
@@ -98,13 +82,13 @@
       if (!teliaAceLeijukeWrapper) {
         teliaAceLeijukeWrapper = document.createElement('aside');
         teliaAceLeijukeWrapper.id = 'telia-ace-leijuke';
-        document.body.append(teliaAceLeijukeWrapper)
+        document.body.append(teliaAceLeijukeWrapper);
       }
 
-      let teliaAceLeijukeInstance = document.createElement('button');
+      const teliaAceLeijukeInstance = document.createElement('button');
       teliaAceLeijukeInstance.id = this.static.selector;
-      teliaAceLeijukeInstance.classList.add('chat-leijuke')
-      teliaAceLeijukeInstance.classList.add('telia-chat-leijuke')
+      teliaAceLeijukeInstance.classList.add('chat-leijuke');
+      teliaAceLeijukeInstance.classList.add('telia-chat-leijuke');
 
       teliaAceLeijukeWrapper.append(teliaAceLeijukeInstance);
 
@@ -113,7 +97,7 @@
 
     prepButton(button) {
 
-      button.addEventListener('click', (event) => {
+      button.addEventListener('click', () => {
 
         // Debounce button.
         if (this.state.busy) {
@@ -132,31 +116,31 @@
 
         if (!this.state.cookies) {
           // Implicitly allow chat cookies if clicking Leijuke.
-          this.extCookieManager.cookieSet();
+          this.cookieSet();
         }
 
         this.state = {
           ...this.state,
-          cookies: this.extCookieManager.cookieCheck()
+          cookies: this.cookieCheck()
         };
 
         if (this.state.cookies) {
           this.loadChat();
           this.openChat(true);
         } else {
-          console.warn('Missing the required cookies to open chat. Missing cookie not allowed to be set implicitly.')
+          // console.warn('Missing the required cookies to open chat. Missing cookie not allowed to be set implicitly.')
         }
 
       });
     }
 
-    openChat(open_widget) {
+    openChat(openWidget) {
       const leijuke = this;
 
-      let teliaAceWidgetInitialized = setInterval(() => {
-        if(typeof humany !== "undefined" && typeof humany.widgets !== "undefined" && humany.widgets.find(leijuke.static.chat_id)){
-          if (open_widget) {
-            let myWidget = humany.widgets.find(leijuke.static.chat_id);
+      const teliaAceWidgetInitialized = setInterval(() => {
+        if(typeof window.humany !== 'undefined' && typeof window.humany.widgets !== 'undefined' && window.humany.widgets.find(leijuke.static.chatId)){
+          if (openWidget) {
+            const myWidget = window.humany.widgets.find(leijuke.static.chatId);
             myWidget.activate();
             myWidget.invoke('show');
           }
@@ -176,24 +160,41 @@
 
     render() {
       const { chatOpened, chatLoading } = this.state;
-      const label = chatLoading ? Drupal.t('Loading chat...', {}, {context: 'Telia ACE chat'}) : this.static.chat_title;
+      const label = chatLoading ? Drupal.t('Loading chat...', {}, {context: 'Telia ACE chat'}) : this.static.chatTitle;
       const element = document.getElementById(this.static.selector);
       if (!element) {
         return;
       }
-      let innerHTML = `
+      const innerHTML = `
         <span class="hel-icon hel-icon--speechbubble-text"></span>
         <span>${label}</span>
         <span class="hel-icon hel-icon--angle-up"></span>
       `;
 
-      if (element.innerHTML != innerHTML) {
+      if (element.innerHTML !== innerHTML) {
         element.innerHTML = innerHTML;
       }
       element.classList.toggle('loading', chatLoading);
       element.classList.toggle('hidden', chatOpened);
     }
-
   }
 
+
+  Drupal.behaviors.chat_leijuke = {
+    attach: function attach() {
+      const teliaAceData = drupalSettings.telia_ace_data;
+
+      setTimeout(() => {
+        // Only load any leijuke once, in case of ajax triggers.
+        if (teliaAceData.initialized) {
+          // console.warn(`Already initialized Telia ACE script!`);
+          return;
+        }
+
+        const teliaAce = new TeliaAceLeijuke(teliaAceData);
+        teliaAce.init();
+        drupalSettings.telia_ace_data.initialized = true;
+      });
+    }
+  };
 })(Drupal, drupalSettings);
