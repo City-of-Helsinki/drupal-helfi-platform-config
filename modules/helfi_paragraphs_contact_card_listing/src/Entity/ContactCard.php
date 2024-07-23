aabdd5d462d56131b1e93109719a9476079819c0
Tero Elonen <tero.elonen@druid.fi>
2024-07-23 14:08:36 +0300

    UHF-10191: Change the contact card to use responsive image style

--- modules/helfi_paragraphs_contact_card_listing/src/Entity/ContactCard.php
+++ modules/helfi_paragraphs_contact_card_listing/src/Entity/ContactCard.php
@@ -14,35 +14,6 @@ class ContactCard extends Paragraph implements ParagraphInterface {
   use TranslatorTrait;
 
   /**
-   * Get the contact image.
-   *
-   * @return array
-   *   The contact image.
-   */
-  public function getContactImage(): array {
-    if ($this->get('field_contact_image')->isEmpty()) {
-      return [];
-    }
-
-    $image = $this->get('field_contact_image')[0];
-    if (
-      $image &&
-      $this->hasField('field_contact_image_photographer') &&
-      $this->hasField('field_contact_image')
-    ) {
-      $photographer = $this->get('field_contact_image_photographer')->value;
-      $alt = $this->t('@alt @photographer_text: @photographer', [
-        '@alt' => $this->get('field_contact_image')->alt,
-        '@photographer_text' => $this->t('Photographer'),
-        '@photographer' => $photographer,
-      ]);
-
-      $image->alt = $alt;
-    }
-    return $image->view();
-  }
-
-  /**
    * Get the heading level.
    *
    * @return string|null
