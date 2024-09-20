35972edd979d1173ebb3eee8ac299d07cb3e3d66
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2024-09-20 11:02:45 +0300

    UHF-8670: Added tests for Hero bundle class and Hero design alter function.

---
+++ modules/helfi_paragraphs_hero/tests/src/Kernel/HeroDesignAllowedValuesAlterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\Tests\helfi_paragraphs_hero\Kernel;
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests allowed values alter for hero design.
+ *
+ * @group helfi_paragraphs_hero
+ */
+class HeroDesignAllowedValuesAlterTest extends KernelTestBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'helfi_paragraphs_hero',
+    'helfi_paragraphs_hero_test',
+    'field',
+    'entity',
+    'user',
+  ];
+
+  /**
+   * Test the hero design allowed values alter.
+   */
+  public function testHeroDesignAlter() {
+    // Load the required definition and entity mocks.
+    $definition = $this->createMock(FieldStorageDefinitionInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+
+    // Call the allowed designs function.
+    $allowed_designs = helfi_paragraphs_hero_design_allowed_values($definition, $entity);
+
+    // Call the function that triggers the drupal_alter.
+    helfi_paragraphs_hero_test_helfi_hero_design_alter($allowed_designs);
+
+    // Define the expected outcome after the alter hook.
+    $expected_designs = $allowed_designs + ['test-design' => $this->t('Test design')];
+
+    // Assert that the design data has been altered as expected.
+    $this->assertEquals($expected_designs, $allowed_designs);
+  }
+
+}
---
+++ modules/helfi_paragraphs_hero/tests/src/Kernel/HeroDesignAllowedValuesTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\Tests\helfi_paragraphs_hero\Kernel;
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests allowed values for hero design.
+ *
+ * @group helfi_paragraphs_hero
+ */
+class HeroDesignAllowedValuesTest extends KernelTestBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'helfi_paragraphs_hero',
+    'field',
+    'entity',
+    'user',
+  ];
+
+  /**
+   * Test the hero design allowed values function.
+   */
+  public function testAllowedValues() {
+    // Load the required definition and entity mocks.
+    $definition = $this->createMock(FieldStorageDefinitionInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+
+    // Call the allowed designs function.
+    $allowed_designs = helfi_paragraphs_hero_design_allowed_values($definition, $entity);
+
+    // Define the expected allowed designs.
+    $expected_designs = [
+      'without-image-left' => $this->t('Without image, align left'),
+      'with-image-right' => $this->t('Image on the right'),
+      'with-image-left' => $this->t('Image on the left'),
+      'with-image-bottom' => $this->t('Image on the bottom'),
+      'diagonal' => $this->t('Diagonal'),
+    ];
+
+    // Assert that the allowed values match the expected values.
+    $this->assertEquals($expected_designs, $allowed_designs);
+  }
+
+}
