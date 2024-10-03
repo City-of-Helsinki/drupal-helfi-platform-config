915eb0f4628609622ac4e1bd441665b962ad1a47
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2024-10-03 13:29:13 +0300

    UHF-10738: New route and controller for the cookie policy page.

---
+++ modules/hdbt_cookie_banner/src/Controller/HdbtCookieBannerInformationController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hdbt_cookie_banner\Controller;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\hdbt_cookie_banner\Form\HdbtCookieBannerForm;
+
+/**
+ * Defines HdbtCookieBannerInformationController class.
+ */
+final class HdbtCookieBannerInformationController extends ControllerBase {
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The config factory.
+   */
+  public function __construct(private readonly ConfigFactoryInterface $config) {
+  }
+
+  /**
+   * Display the cookie information.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function content() {
+    $config = $this->config->get(HdbtCookieBannerForm::SETTINGS);
+    $content = [];
+
+    $content['#theme'] = 'cookie_policy';
+    $content['#title'] = $config->get('cookie_information.title');
+    $content['#content'] = $config->get('cookie_information.content');
+
+    return $content;
+  }
+
+}
