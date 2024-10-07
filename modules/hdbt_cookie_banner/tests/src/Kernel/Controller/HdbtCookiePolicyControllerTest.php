df3e092dbd011d893629c63bf4b78227c81eb0bf
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2024-10-07 14:26:39 +0300

    UHF-10738: Added tests.

---
+++ modules/hdbt_cookie_banner/tests/src/Kernel/Controller/HdbtCookiePolicyControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\hdbt_cookie_banner\Kernel\Controller;
+
+use Drupal\hdbt_cookie_banner\Controller\HdbtCookiePolicyController;
+use Drupal\Tests\hdbt_cookie_banner\Kernel\KernelTestBase;
+
+/**
+ * Tests the HdbtCookiePolicyController.
+ *
+ * @coversDefaultClass \Drupal\hdbt_cookie_banner\Controller\HdbtCookiePolicyController
+ * @group hdbt_cookie_banner
+ */
+class HdbtCookiePolicyControllerTest extends KernelTestBase {
+
+  /**
+   * Tests the content() method of HdbtCookiePolicyController.
+   */
+  public function testContent() {
+    // Create a mock config object.
+    $config_values = [
+      'cookie_information' => [
+        'title' => 'Test Cookie Policy Title',
+        'content' => 'This is the cookie policy content.',
+      ],
+    ];
+
+    // Set up configuration with mock values.
+    $config_factory = $this->container->get('config.factory');
+    $config_factory->getEditable('hdbt_cookie_banner.settings')
+      ->setData($config_values)
+      ->save();
+
+    // Create an instance of the controller.
+    $controller = new HdbtCookiePolicyController($config_factory);
+
+    // Get the content array.
+    $content = $controller->content();
+
+    // Assert that the content has the correct theme and values.
+    $this->assertEquals('cookie_policy', $content['#theme']);
+    $this->assertEquals('Test Cookie Policy Title', $content['#title']);
+    $this->assertEquals('This is the cookie policy content.', $content['#content']);
+  }
+
+}
