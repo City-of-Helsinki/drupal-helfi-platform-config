cd5e55bb42d3c9c099329ea28b1a5c286f538150
Jere Ljungberg <jere@mysteerio.fi>
2024-10-22 11:34:49 +0300

    UHF-10259: Store event types as const in bundle class

--- modules/helfi_paragraphs_curated_event_list/src/Entity/LinkedEventsEvent.php
+++ modules/helfi_paragraphs_curated_event_list/src/Entity/LinkedEventsEvent.php
@@ -12,6 +12,9 @@ use Drupal\external_entities\Entity\ExternalEntity;
  */
 final class LinkedEventsEvent extends ExternalEntity {
 
+  public const FUTURE_OR_ONGOING = 'future_or_ongoing';
+  public const PAST_EVENTS = 'past_events';
+
   /**
    * Check if event has ended and return result.
    *
@@ -27,6 +30,16 @@ final class LinkedEventsEvent extends ExternalEntity {
 
     $datetime = new DrupalDateTime($end_time);
     return $datetime->format('U') < time();
+  }
+
+  /**
+   * Returns event type.
+   *
+   * @return string
+   *   Event type string.
+   */
+  public function getEventType() : string {
+    return $this->hasEnded() ? self::PAST_EVENTS : self::FUTURE_OR_ONGOING;
   }
 
 }
