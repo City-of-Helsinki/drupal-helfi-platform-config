48a23b40493c4588d7be1c3b1eb450a1918f0518
Anna Lonka <annalonka@Annas-MacBook-Pro-2.local>
2024-11-08 07:48:21 +0200

    UHF-10239: Enables module, adds it to page templates

---
+++ modules/helfi_paragraphs_image_gallery/src/Entity/ParagraphImageGallery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\helfi_paragraphs_image_gallery\Entity;
+
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\ParagraphInterface;
+
+/**
+ * Bundle class for remote_video paragraph.
+ */
+class ParagraphImageGallery extends Paragraph implements ParagraphInterface {
+  
+}
---
+++ modules/helfi_paragraphs_image_gallery/src/Entity/ParagraphImageGalleryItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\helfi_paragraphs_image_gallery\Entity;
+
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\ParagraphInterface;
+
+/**
+ * Bundle class for image_gallery_item paragraph.
+ */
+class ImageGallreyItem extends Paragraph implements ParagraphInterface {
+
+}
