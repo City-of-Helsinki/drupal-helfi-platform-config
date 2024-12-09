32eb0da02e9f58abadbf8f48a47313ff5dde73be
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2024-12-09 22:55:40 +0200

    UHF-10908: Updated the cookie banner functional test.

--- modules/hdbt_cookie_banner/tests/src/FunctionalJavascript/CookieBannerTest.php
+++ modules/hdbt_cookie_banner/tests/src/FunctionalJavascript/CookieBannerTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\hdbt_cookie_banner\FunctionalJavascript;
 
+use Behat\Mink\Driver\Selenium2Driver;
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 
 /**
@@ -42,12 +43,18 @@ class CookieBannerTest extends WebDriverTestBase {
     $json_content = file_get_contents($json_file_path);
     $this->assertNotEmpty($json_content, 'Decoded JSON data is not empty.');
 
+    // Get the public base URL (in a FunctionalJavascript test).
+    // Construct a URL for the hds-cookie-consent.min.js file.
+    $cookie_js_url = "/$module_path/assets/js/hds-cookie-consent.min.js";
+
     // Change configuration value before the test runs.
     $config = $this->config('hdbt_cookie_banner.settings');
     $config
+      ->set('use_custom_settings', TRUE)
       ->set('site_settings', $json_content)
       ->set('use_custom_settings', TRUE)
-      ->set('use_internal_hds_cookie_js', TRUE)
+      ->set('use_internal_hds_cookie_js', FALSE)
+      ->set('hds_cookie_js_override', $cookie_js_url)
       ->save();
 
     \Drupal::service('cache.default')->deleteAll();
@@ -57,31 +64,60 @@ class CookieBannerTest extends WebDriverTestBase {
    * Tests the cookie banner visibility and interaction.
    */
   public function testCookieBanner() {
-
     $this->drupalGet('/test-page');
     $this->assertSession()->pageTextContains('Test Content');
     $this->assertSession()->elementExists('css', '.test-footer');
 
-    // phpcs:disable
-    // Take a screenshot.
-    // $this->createScreenshot(\Drupal::root() .'/sites/default/files/simpletest/test.png');
+    // Get the web driver.
+    $driver = $this->getSession()->getDriver();
 
-    // Get browser logs (console output).
-    // $logs = $this->getSession()->getDriver()->getWebDriverSession()->log('browser');
-    // foreach ($logs as $log) {
-    //   var_dump( "JavaScript Log: " . $log['message']);
-    // }
+    // Check if the driver is an instance of DrupalSelenium2Driver.
+    if ($driver instanceof Selenium2Driver) {
+      // Get all cookies from the browser.
+      $cookies = $driver->getWebDriverSession()->getCookie();
 
-    // Debug the page html.
-    // $page = $this->getSession()->getPage();
-    // $this->htmlOutput($page->getHtml());
-    // phpcs:enable
+      // Extract only the 'name' keys from all the cookies, as we want to check
+      // if a specific cookie "change-me" exists.
+      $cookieNames = array_column($cookies, 'name');
+      $this->assertNotContains(
+        'change-me',
+        $cookieNames,
+        'The cookie "change-me" was found, but it should not exist.',
+      );
+    }
+    else {
+      $this->fail('The driver is not an instance of Selenium2Driver.');
+    }
 
+    // Assert that the cookie banner is visible and click the accept button.
+    $this->assertCookieBannerIsVisible();
+
+    // Get the new cookies from the browser.
+    $new_cookies = $driver->getWebDriverSession()->getCookie();
+
+    // There should be a new cookie called "change-me".
+    $cookieNames = array_column($new_cookies, 'name');
+    $this->assertContains(
+      'change-me',
+      $cookieNames,
+      'The cookie "change-me" was not found after clicking the button.',
+    );
+
+    // Reload the page and assert that the cookie banner is not visible.
+    $this->drupalGet('/test-page');
+    $this->assertSession()->pageTextContains('Test Content');
+    $this->assertCookieBannerNotVisible();
+  }
+
+  /**
+   * Asserts that the cookie banner is visible.
+   */
+  protected function assertCookieBannerIsVisible() {
     // Get the Shadow DOM host and button selectors.
     $shadowHostSelector = '.hds-cc__target';
     $buttonSelector = '.hds-cc__all-cookies-button';
 
-    // JavaScript to locate the button inside the Shadow DOM and click it.
+    // Verify that the cookie banner is visible and click the accept button.
     $js = <<<JS
   const shadowHost = document.querySelector('$shadowHostSelector');
   if (!shadowHost) {
@@ -98,6 +134,24 @@ class CookieBannerTest extends WebDriverTestBase {
   button.click();
 JS;
 
+    // Execute the JavaScript in the browser context.
+    $this->getSession()->executeScript($js);
+  }
+
+  /**
+   * Asserts that the cookie banner is not visible.
+   */
+  protected function assertCookieBannerNotVisible() {
+    // Get the Shadow DOM host and button selectors.
+    $shadowHostSelector = '.hds-cc__target';
+
+    // Verify that the cookie banner is not visible.
+    $js = <<<JS
+  const shadowHost = document.querySelector('$shadowHostSelector');
+  if (shadowHost) {
+    throw new Error('Shadow host still found.');
+  }
+JS;
     // Execute the JavaScript in the browser context.
     $this->getSession()->executeScript($js);
   }
