e3c76c7a94fd9791acef1102a6cfc2ea7910434e
Jere Ljungberg <jere@mysteerio.fi>
2025-01-21 09:43:32 +0200

    UHF-11232: Use HelfiLinkitWidget globally

---
+++ modules/hdbt_admin_tools/src/Plugin/Field/FieldWidget/HelfiLinkitWidget.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hdbt_admin_tools\Plugin\Field\FieldWidget;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\Attribute\FieldWidget;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\linkit\Plugin\Field\FieldWidget\LinkitWidget;
+use Drupal\linkit\Utility\LinkitHelper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Overrides the linkit widget.
+ */
+#[FieldWidget(
+  id: "helfi_linkit",
+  label: new TranslatableMarkup('Helfi: Linkit'),
+  field_types: ['link']
+)]
+
+final class HelfiLinkitWidget extends LinkitWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['third_party_settings'],
+      $container->get('current_user'),
+      $container->get('entity_type.manager'),
+      $container->get('request_stack'),
+    );
+  }
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The widget settings.
+   * @param array $third_party_settings
+   *   The widget third party settings.
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current user.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *   The current request stack.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    array $third_party_settings,
+    AccountProxyInterface $currentUser,
+    EntityTypeManagerInterface $entityTypeManager,
+    protected RequestStack $requestStack,
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $currentUser, $entityTypeManager);
+  }
+
+  /**
+   * Circumvent Linkit to allow linking to internal pages using absolute URLs.
+   *
+   * \Drupal\Linkit\Plugin\Field\FieldWidget\LinkitWidget always runs links
+   * through uriFromUserInput, which coerces absolute URLs to node links if
+   * they belong to the current site.
+   *
+   * @param string $input
+   *   The user input.
+   *
+   * @return string
+   *   The URI.
+   */
+  protected function convertToUri(string $input) {
+    if (
+      UrlHelper::isExternal($input) &&
+      UrlHelper::externalIsLocal($input, $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost())
+    ) {
+      return $input;
+    }
+
+    return LinkitHelper::uriFromUserInput($input);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+    foreach ($values as &$value) {
+      $value['uri'] = $this->convertToUri($value['uri']);
+      $value += ['options' => $value['attributes']];
+    }
+    return $values;
+  }
+
+}
---
+++ modules/hdbt_admin_tools/tests/src/Unit/HelfiLinkitWidgetTest.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\hdbt_admin_tools\Unit;
+
+use Drupal\Core\DependencyInjection\ContainerNotInitializedException;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\hdbt_admin_tools\Plugin\Field\FieldWidget\HelfiLinkitWidget;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Tests the HelfiLinkitWidget.
+ */
+class HelfiLinkitWidgetTest extends UnitTestCase {
+  /**
+   * Instance of the field widget.
+   *
+   * @var \Drupal\hdbt_admin_tools\Plugin\Field\FieldWidget\HelfiLinkitWidget
+   */
+  protected HelfiLinkitWidget $widget;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+
+    $request_stack = $this->prophesize(RequestStack::class);
+    $current_request = $this->prophesize(Request::class);
+    $current_request->getSchemeAndHttpHost()->willReturn('https://helfi-etusivu.docker.so');
+    $current_request->reveal();
+    $request_stack->getCurrentRequest()->willReturn($current_request);
+
+    $this->widget = new HelfiLinkitWidget(
+      [],
+      [],
+      $this->prophesize(FieldDefinitionInterface::class)->reveal(),
+      [],
+      [],
+      $this->prophesize(AccountProxyInterface::class)->reveal(),
+      $this->prophesize(EntityTypeManager::class)->reveal(),
+      $request_stack->reveal(),
+    );
+  }
+
+  /**
+   * Test the massageFieldValues method.
+   */
+  public function testMassageFieldValues(): void {
+    // Urls that should be returned unchanged.
+    $internal_absolute_urls = [
+      [
+        'uri' => 'https://helfi-etusivu.docker.so/fi/node/232',
+        'attributes' => [],
+      ],
+      [
+        'uri' => 'https://helfi-etusivu.docker.so/sv/någon/sida/?fråga=parameter',
+        'attributes' => [],
+      ],
+      [
+        'uri' => 'http://helfi-etusivu.docker.so/en/news/helsinki-city-council-jubilee-decision-free-admission-to-outdoor-swimming-facilities-select-cultural',
+        'attributes' => [],
+      ],
+    ];
+
+    $massagedValues = $this->widget->massageFormValues(
+      $internal_absolute_urls,
+      [],
+      $this->prophesize(FormStateInterface::class)->reveal(),
+    );
+
+    foreach ($massagedValues as $key => $value) {
+      $this->assertEquals(
+        $value['uri'],
+        $internal_absolute_urls[$key]['uri']
+      );
+    }
+
+    // Urls that should be passed forward to \Drupal\linkit\Utility\LinkitHelper
+    // throwing error within test context.
+    $should_throw_error = [
+      [
+        'uri' => 'https://google.com?query=string',
+        'attributes' => [],
+      ],
+      [
+        'uri' => 'helfi-etusivu.docker.so?query=string',
+        'attributes' => [],
+      ],
+      [
+        'uri' => '/sv/någon/sida/',
+        'attributes' => [],
+      ],
+    ];
+
+    foreach ($should_throw_error as $value) {
+      try {
+        $this->widget->massageFormValues(
+          [$value],
+          [],
+          $this->prophesize(FormStateInterface::class)->reveal(),
+        );
+      }
+      catch (\Throwable $t) {
+        $this->assertInstanceOf(ContainerNotInitializedException::class, $t);
+      }
+    }
+  }
+
+}
