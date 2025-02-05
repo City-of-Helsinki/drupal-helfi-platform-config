d8aa5eef45049c15eac20040c36b338427e11ca5
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-02-05 09:10:42 +0200

    UHF-11329: Added HelfiChart bundle class and applied it to the helfi_media_chart.

---
+++ modules/helfi_media_chart/src/Entity/HelfiChart.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_media_chart\Entity;
+
+use Drupal\helfi_media\Entity\MediaEntityBundle;
+use Drupal\media\MediaInterface;
+
+/**
+ * Bundle class for helfi media chart media entity.
+ */
+class HelfiChart extends MediaEntityBundle implements MediaInterface {
+
+  /**
+   * Get service provider url.
+   *
+   * @return string
+   *   Url of the service provider.
+   */
+  public function getServiceUrl(): string {
+    $chart_url = $this->get('field_helfi_chart_url')
+      ?->first()
+      ?->getString();
+    $url_parts = parse_url($chart_url);
+    return $url_parts['scheme'] . "://" . $url_parts['host'];
+  }
+
+  /**
+   * Get the title of map.
+   *
+   * @return string|null
+   *   The title of the map.
+   */
+  public function getMediaTitle(): ?string {
+    $title = (string) $this->get('field_helfi_chart_title')
+      ?->first()
+      ?->getString();
+
+    return empty($title) ? NULL : $title;
+  }
+
+}
