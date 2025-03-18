05f87795eb548bf098fe3b9ce0ba70708f8983c8
Jere Ljungberg <jere@mysteerio.fi>
2025-03-18 10:06:35 +0200

    UHF-11535: Add helfi_matomo module

---
+++ modules/helfi_matomo/src/Form/MatomoSettings.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_matomo\Form;
+
+/**
+ * @file
+ * Contains Drupal\helfi_matomo\Form\MatomoSettings.
+ */
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Matomo settings.
+ */
+class MatomoSettings extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return [
+      'helfi_matomo.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form = parent::buildForm($form, $form_state);
+    $config = $this->config('helfi_matomo.settings');
+
+    $form['matomo_site_id'] = [
+      '#default_value' => $config->get('helfi_matomo.matomo_site_id'),
+      '#description' => $this->t('Matomo site ID that matches the project in Matomo admin. Used to link the analytics of the site to Matomo'),
+      '#title' => $this->t('Matomo site ID'),
+      '#type' => 'textfield',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'helfi_matomo_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $config = $this->config('helfi_matomo.settings');
+    $config->set('helfi_matomo.matomo_site_id', $form_state->getValue('matomo_site_id'));
+    $config->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
