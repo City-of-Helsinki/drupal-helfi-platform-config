f924d70fbdd4e6996d52fab4ad56f36241b194e5
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-04-11 13:29:00 +0300

    UHF-8792: Added contexts to prevent unnecessary translations of the test content.

--- modules/helfi_test_content/src/Form/HelfiTestContentForm.php
+++ modules/helfi_test_content/src/Form/HelfiTestContentForm.php
@@ -23,58 +23,58 @@ class HelfiTestContentForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-
+    $context = ['context' => 'Helfi test content'];
     $form['textfield'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Text field'),
-      '#description' => $this->t('A description for the text field.'),
+      '#title' => $this->t('Text field', options: $context),
+      '#description' => $this->t('A description for the text field.', options: $context),
     ];
 
     $form['textarea'] = [
       '#type' => 'textarea',
-      '#title' => $this->t('Textarea'),
-      '#description' => $this->t('A description for the textarea.'),
+      '#title' => $this->t('Textarea', options: $context),
+      '#description' => $this->t('A description for the textarea.', options: $context),
     ];
 
     $form['select'] = [
       '#type' => 'select',
-      '#title' => $this->t('Select list'),
+      '#title' => $this->t('Select list', options: $context),
       '#options' => [
-        'value_a' => $this->t('Option 1'),
-        'value_b' => $this->t('Option 2'),
+        'value_a' => $this->t('Option 1', options: $context),
+        'value_b' => $this->t('Option 2', options: $context),
       ],
-      '#description' => $this->t('A description for the textarea.'),
+      '#description' => $this->t('A description for the textarea.', options: $context),
     ];
 
     $form['fieldset_radio'] = [
       '#type' => 'fieldset',
-      '#title' => $this->t('Select either one'),
-      '#description' => $this->t('A description for the radio fieldset.'),
+      '#title' => $this->t('Select either one', options: $context),
+      '#description' => $this->t('A description for the radio fieldset.', options: $context),
     ];
     $form['fieldset_radio']['fieldset_radios'] = [
       '#type' => 'radios',
       '#default_value' => 'one',
       '#options' => [
-        'one' => $this->t('Selection 1'),
-        'two' => $this->t('Selection 2'),
+        'one' => $this->t('Selection 1', options: $context),
+        'two' => $this->t('Selection 2', options: $context),
       ],
     ];
 
     $form['fieldset_checkbox'] = [
       '#type' => 'fieldset',
-      '#title' => $this->t('Select either one'),
-      '#description' => $this->t('A description for the checkbox fieldset.'),
+      '#title' => $this->t('Select either one', options: $context),
+      '#description' => $this->t('A description for the checkbox fieldset.', options: $context),
     ];
 
     $form['fieldset_checkbox']['fieldset_checkbox_not_checked'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Checkbox'),
+      '#title' => $this->t('Checkbox', options: $context),
       '#default_value' => FALSE,
     ];
 
     $form['fieldset_checkbox']['fieldset_checkbox_selected'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Selected checkbox'),
+      '#title' => $this->t('Selected checkbox', options: $context),
       '#default_value' => TRUE,
     ];
 
@@ -82,26 +82,26 @@ class HelfiTestContentForm extends FormBase {
       '#type' => 'radios',
       '#default_value' => 'two',
       '#options' => [
-        'one' => $this->t('Selection 1'),
-        'two' => $this->t('Selection 2'),
+        'one' => $this->t('Selection 1', options: $context),
+        'two' => $this->t('Selection 2', options: $context),
       ],
     ];
 
     $form['checkbox'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Checkbox'),
+      '#title' => $this->t('Checkbox', options: $context),
       '#default_value' => FALSE,
     ];
 
     $form['checkbox_selected'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Selected checkbox'),
+      '#title' => $this->t('Selected checkbox', options: $context),
       '#default_value' => TRUE,
     ];
 
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Submit'),
+      '#value' => $this->t('Submit', options: $context),
     ];
 
     $form['cancel'] = [
@@ -112,7 +112,7 @@ class HelfiTestContentForm extends FormBase {
         ],
         'style' => 'margin-left: 16px;',
       ],
-      '#value' => $this->t('Cancel'),
+      '#value' => $this->t('Cancel', options: $context),
     ];
     return $form;
   }
