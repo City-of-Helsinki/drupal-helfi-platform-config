be8cd154219641504ed43be82304174f50272945
Toni Sinisalo <toni.sinisalo@digitalist.com>
2025-04-18 15:27:35 +0300

    UHF-10647: Attempting to install search_api separately to avoid an error when installing helfi_recommendations.

--- modules/helfi_recommendations/src/EventSubscriber/SearchApiSubscriber.php
+++ modules/helfi_recommendations/src/EventSubscriber/SearchApiSubscriber.php
@@ -18,18 +18,10 @@ final class SearchApiSubscriber implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents(): array {
-    $map = [];
-
-    // Make sure the class exists before subscribing to events.
-    // @see: https://www.drupal.org/project/drupal/issues/2825358
-    if (class_exists('\Drupal\search_api\Event\SearchApiEvents', TRUE)) {
-      $map[SearchApiEvents::MAPPING_FIELD_TYPES] = 'mapFieldTypes';
-    }
-    if (class_exists('\Drupal\elasticsearch_connector\Event\FieldMappingEvent', TRUE)) {
-      $map[FieldMappingEvent::class] = 'mapElasticFields';
-    }
-
-    return $map;
+    return [
+      SearchApiEvents::MAPPING_FIELD_TYPES => 'mapFieldTypes',
+      FieldMappingEvent::class => 'mapElasticFields',
+    ];
   }
 
   /**
