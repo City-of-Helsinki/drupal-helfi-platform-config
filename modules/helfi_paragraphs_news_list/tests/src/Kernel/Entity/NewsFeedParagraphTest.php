2c516aebcb8e6b066d712682aca7be88005f577f
tuutti <tuutti@iki.fi>
2025-05-06 11:01:35 +0300

    UHF-11025: Added tests

--- modules/helfi_paragraphs_news_list/tests/src/Kernel/Entity/NewsFeedParagraphTest.php
+++ modules/helfi_paragraphs_news_list/tests/src/Kernel/Entity/NewsFeedParagraphTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\helfi_paragraphs_news_list\Kernel\Entity;
 
-use Drupal\Tests\helfi_paragraphs_news_list\Kernel\KernelTestBase;
-use Drupal\external_entities\ExternalEntityStorage;
 use Drupal\helfi_paragraphs_news_list\Entity\NewsFeedParagraph;
 use Drupal\paragraphs\Entity\Paragraph;
 
@@ -14,53 +12,17 @@ use Drupal\paragraphs\Entity\Paragraph;
  *
  * @group helfi_paragraphs_news_list
  */
-class NewsFeedParagraphTest extends KernelTestBase {
-
-  /**
-   * Gets the storage for given entity type.
-   *
-   * @param string $entityType
-   *   The entity type.
-   *
-   * @return \Drupal\external_entities\ExternalEntityStorage
-   *   The storage.
-   */
-  private function getStorage(string $entityType) : ExternalEntityStorage {
-    $storage = $this->container->get('entity_type.manager')
-      ->getStorage($entityType);
-    assert($storage instanceof ExternalEntityStorage);
-
-    return $storage;
-  }
+class NewsFeedParagraphTest extends EntityKernelTestBase {
 
   /**
    * Tests that paragraph uses proper bundle class.
    */
   public function testBundleClass() : void {
-    $neighbourhood_uuid = 'ff10dbf0-6b00-400b-a8a9-4fae102ea92c';
-    $neighbourhood_id = $neighbourhood_uuid . ':fi';
-    $neighbourhood = $this->getStorage('helfi_news_neighbourhoods')->create([
-      'id' => $neighbourhood_id,
-      'title' => 'Neighbourhood',
-    ]);
-    $tags_uuid = 'ca9a7c2e-acbb-4f03-938b-9cd86fd606ac';
-    $tags_id = $tags_uuid . ':fi';
-    $tags = $this->getStorage('helfi_news_tags')->create([
-      'id' => $tags_id,
-      'title' => 'Tags',
-    ]);
-    $groups_uuid = 'e30fa7be-4d13-4216-8658-103fb9a26c8c';
-    $groups_id = $groups_uuid . ':fi';
-    $groups = $this->getStorage('helfi_news_groups')->create([
-      'id' => $groups_id,
-      'title' => 'Tags',
-    ]);
-
     $paragraph = Paragraph::create([
       'type' => 'news_list',
-      'field_helfi_news_neighbourhoods' => [$neighbourhood],
-      'field_helfi_news_tags' => [$tags],
-      'field_helfi_news_groups' => [$groups],
+      'field_helfi_news_neighbourhoods' => [$this->neighbourhood],
+      'field_helfi_news_tags' => [$this->tag],
+      'field_helfi_news_groups' => [$this->group],
       'field_limit' => 22,
       'field_news_list_title' => 'test title',
       'field_news_list_description' => 'test description',
@@ -68,12 +30,12 @@ class NewsFeedParagraphTest extends KernelTestBase {
     $paragraph->save();
 
     $this->assertInstanceOf(NewsFeedParagraph::class, $paragraph);
-    $this->assertEquals([['target_id' => $neighbourhood_id]], $paragraph->getNeighbourhoods());
-    $this->assertEquals([$neighbourhood_uuid], $paragraph->getNeighbourhoodsUuids());
-    $this->assertEquals([['target_id' => $tags_id]], $paragraph->getTags());
-    $this->assertEquals([$tags_uuid], $paragraph->getTagsUuid());
-    $this->assertEquals([['target_id' => $groups_id]], $paragraph->getGroups());
-    $this->assertEquals([$groups_uuid], $paragraph->getGroupsUuid());
+    $this->assertEquals([['target_id' => 'ff10dbf0-6b00-400b-a8a9-4fae102ea92c:fi']], $paragraph->getNeighbourhoods());
+    $this->assertEquals(['ff10dbf0-6b00-400b-a8a9-4fae102ea92c'], $paragraph->getNeighbourhoodsUuids());
+    $this->assertEquals([['target_id' => 'ca9a7c2e-acbb-4f03-938b-9cd86fd606ac:fi']], $paragraph->getTags());
+    $this->assertEquals(['ca9a7c2e-acbb-4f03-938b-9cd86fd606ac'], $paragraph->getTagsUuid());
+    $this->assertEquals([['target_id' => 'e30fa7be-4d13-4216-8658-103fb9a26c8c:fi']], $paragraph->getGroups());
+    $this->assertEquals(['e30fa7be-4d13-4216-8658-103fb9a26c8c'], $paragraph->getGroupsUuid());
     $this->assertEquals(22, $paragraph->getLimit());
     $this->assertEquals('test title', $paragraph->getTitle());
     $this->assertEquals('test description', $paragraph->getDescription());
---
+++ modules/helfi_paragraphs_news_list/tests/src/Kernel/Entity/NewsListLazyBuilderTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_paragraphs_news_list\Kernel\Entity;
+
+use Drupal\helfi_paragraphs_news_list\Entity\ExternalEntity\News;
+use Drupal\helfi_paragraphs_news_list\Entity\NewsListLazyBuilder;
+use Drupal\helfi_paragraphs_news_list\EventSubscriber\CacheResponseSubscriber;
+use Drupal\Tests\helfi_paragraphs_news_list\Kernel\KernelTestBase;
+use Drupal\paragraphs\Entity\Paragraph;
+use Elastic\Elasticsearch\Client;
+use Prophecy\Argument;
+
+/**
+ * Tests lazy builder.
+ *
+ * @group helfi_paragraphs_news_list
+ */
+class NewsListLazyBuilderTest extends KernelTestBase {
+
+  /**
+   * Tests ::build().
+   */
+  public function testBuild() : void {
+    $client = $this->prophesize(Client::class);
+    $client->search(Argument::any())
+      ->shouldBeCalled()
+      ->willReturn(
+        $this->createElasticsearchResponse([]),
+        $this->createElasticsearchResponse([
+          'hits' => [
+            'hits' => [
+              [
+                '_source' => [
+                  'uuid_langcode' => ['123'],
+                  'uuid' => ['uuid-123'],
+                  'title' => ['test title'],
+                  'field_news_groups' => ['Test groups'],
+                  'field_news_item_tags' => ['Test tag'],
+                  'field_news_neighbourhoods' => ['Test neighbourhood'],
+                  'url' => ['https://localhost'],
+                  'published_at' => [1234567],
+                  'short_title' => ['test shorttitle'],
+                ],
+              ],
+            ],
+          ],
+        ]),
+      );
+    $this->container->set('helfi_paragraphs_news_list.elastic_client', $client->reveal());
+    $sut = $this->container->get(NewsListLazyBuilder::class);
+    assert($sut instanceof NewsListLazyBuilder);
+
+    // Test non-existent paragraph.
+    $this->assertEquals([], $sut->build('1'));
+
+    $paragraph = Paragraph::create([
+      'type' => 'news_list',
+    ]);
+    $paragraph->save();
+    // Test empty response cache tags.
+    $this->assertEquals([
+      '#cache' => [
+        'tags' => ['helfi_news_list_empty_results'],
+        'max-age' => CacheResponseSubscriber::EMPTY_LIST_MAX_AGE,
+      ],
+    ], $sut->build($paragraph->id()));
+
+    $paragraph = Paragraph::create([
+      'type' => 'news_list',
+      'field_helfi_news_neighbourhoods' => [$this->neighbourhood],
+      'field_helfi_news_tags' => [$this->tag],
+      'field_helfi_news_groups' => [$this->group],
+      'field_limit' => 22,
+      'field_news_list_title' => 'test title',
+      'field_news_list_description' => 'test description',
+    ]);
+    $paragraph->save();
+
+    $build = $sut->build($paragraph->id());
+    $this->assertInstanceOf(News::class, $build[0]['#helfi_news']);
+    $this->assertEquals([
+      'helfi_news_view',
+      'helfi_news:123',
+      'paragraph:' . $paragraph->id(),
+    ], $build[0]['#cache']['tags']);
+  }
+
+}
