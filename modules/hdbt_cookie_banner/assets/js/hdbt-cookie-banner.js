df5842922bf4dffc31ef3bc839476144725cf221
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-05-12 14:33:02 +0300

    UHF-11025: Separated the unapproved cookies functionality to its own file and added checks for running the initialization only for the full document.

--- modules/hdbt_cookie_banner/assets/js/hdbt-cookie-banner.js
+++ modules/hdbt_cookie_banner/assets/js/hdbt-cookie-banner.js
@@ -1,12 +1,5 @@
 'use strict';
 
-class UnapprovedItemError extends Error {
-  constructor(message, name) {
-    super(message)
-    this.name = name
-  }
-}
-
 ((Drupal, drupalSettings) => {
 
   // Global cookie consent status object.
@@ -66,62 +59,5 @@ class UnapprovedItemError extends Error {
         }
       };
     }
-  }
-
-  // Attach a behavior to capture unapproved cookies with Sentry.
-  Drupal.behaviors.unapprovedCookies = {
-    attach: function attach() {
-      const apiUrl = drupalSettings.hdbt_cookie_banner.apiUrl;
-      fetch(apiUrl)
-        .then(response => response.json())
-        .then(jsonData => {
-
-          // Function to extract cookie names from each group
-          const extractCookiePatterns = (groups) =>
-            groups?.flatMap(group => group.cookies.map(cookie => cookie.name)) || [];
-
-          // Collect all allowed cookie name patterns
-          const validItems = [
-            ...extractCookiePatterns(jsonData.optionalGroups),
-            ...extractCookiePatterns(jsonData.requiredGroups),
-            ...extractCookiePatterns(jsonData.robotGroups),
-          ];
-
-          window.addEventListener(
-            'hds-cookie-consent-unapproved-item-found',
-            (e) => {
-              if (typeof window.Sentry === 'undefined') {
-                return;
-              }
-
-              const { storageType, keys, acceptedGroups } = e.detail;
-              const sortedKeys = keys.sort();
-
-              // Check which keys do not match any pattern in the valid items list
-              const unapprovedItems = sortedKeys.filter(
-                key => !validItems.some(pattern => key.includes(pattern.replace('*', '')))
-              ).sort();
-
-              // Only log if there are unapproved items that are not found in our list
-              if (unapprovedItems.length > 0) {
-                const name = `Unapproved ${storageType}`;
-                const message = `Found: ${unapprovedItems.join(', ')}`;
-
-                window.Sentry.captureException(new UnapprovedItemError(message, name), {
-                  level: 'warning',
-                  tags: {
-                    approvedCategories: acceptedGroups.join(', '),
-                  },
-                  extra: {
-                    storageType,
-                    missingCookies: unapprovedItems,
-                    approvedCategories: acceptedGroups,
-                  },
-                });
-              }
-            }
-          );
-        })
-    },
   }
 })(Drupal, drupalSettings);
