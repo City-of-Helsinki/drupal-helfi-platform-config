0fe8020fa27e830e9e012f5ed57196c415f251f8
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-06-11 13:09:19 +0300

    UHF-11533: Removed obsolete CKEditor4 plugins.

--- modules/hdbt_admin_tools/assets/js/plugins/quote/lang/fi.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.setLang( 'quote', 'fi', {
-  quoteTitle: 'Lisää lainaus',
-  quoteText: 'Lainaus',
-  handleQuote: 'Muokkaa lainausta',
-  dialogTitle: 'Lainauksen asetukset',
-  dialogQuoteText: 'Lainauksen teksti',
-  dialogQuoteAuthor: 'Lainatun tekstin kirjoittaja',
-  dialogQuoteTextNotEmpty: 'Teksti ei voi olla tyhjä'
-});
