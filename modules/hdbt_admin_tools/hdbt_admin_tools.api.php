86ca3c209a4bed43875952144f17fa8e821ebd04
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-06-11 10:24:40 +0300

    UHF-11533: Removed alterations related to CKEditor4 and link dialogs.

--- modules/hdbt_admin_tools/hdbt_admin_tools.api.php
+++ modules/hdbt_admin_tools/hdbt_admin_tools.api.php
@@ -24,14 +24,3 @@ use Drupal\Core\Form\FormStateInterface;
  */
 function hook_design_selection_images_alter(array &$images, string $field_name) {
 }
-
-/**
- * Alter the CKEditor link dialog form validation.
- *
- * @param array $form
- *   The form.
- * @param Drupal\Core\Form\FormStateInterface $form_state
- *   The form state.
- */
-function hook_helfi_form_editor_link_dialog_alter(array &$form, FormStateInterface &$form_state) {
-}
