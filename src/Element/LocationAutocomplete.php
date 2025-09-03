48b4ca5f28c4a4ef6b7523fa44e313cdb7ee373e
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-09-03 08:33:14 +0300

    UHF-11963: Add LocationAutocomplete.php

---
+++ src/Element/LocationAutocomplete.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_platform_config\Element;
+
+use Drupal\Core\Render\Attribute\FormElement;
+use Drupal\Core\Render\Element\Textfield;
+
+/**
+ * Autocomplete element for location autocomplete.
+ */
+#[FormElement('helfi_location_autocomplete')]
+class LocationAutocomplete extends Textfield {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo(): array {
+    $info = parent::getInfo();
+    $class = static::class;
+    $info['#process'][] = [$class, 'processLocationAutocomplete'];
+    return $info;
+  }
+
+  /**
+   * Preprocess callback.
+   */
+  public static function processLocationAutocomplete(array $element): array {
+    $element['#attached']['library'][] = 'helfi_platform_config/location_autocomplete';
+    $element['#attributes']['data-helfi-etusivu-autocomplete'] = TRUE;
+
+    // Remove "form-autocomplete" class.
+    // This prevents Drupal autocomplete from hijacking the element.
+    $element['#attributes']['class'] = array_filter(
+      $element['#attributes']['class'] ?? [],
+      fn ($class) => $class !== 'form-autocomplete'
+    );
+
+    return $element;
+  }
+
+}
