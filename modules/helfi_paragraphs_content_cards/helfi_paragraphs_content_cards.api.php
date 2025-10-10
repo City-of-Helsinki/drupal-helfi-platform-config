a6552f06a8e9ff76dc66c98bb6121e477faf8455
Tero Elonen <tero.elonen@druid.fi>
2025-10-10 11:41:53 +0300

    UHF-8982: Fixed tests

--- modules/helfi_paragraphs_content_cards/helfi_paragraphs_content_cards.api.php
+++ modules/helfi_paragraphs_content_cards/helfi_paragraphs_content_cards.api.php
@@ -12,11 +12,7 @@ declare(strict_types=1);
  *
  * @param array $designs
  *   The designs for the content cards.
- * @param \Drupal\Core\Field\FieldStorageDefinitionInterface $definition
- *   Content Cards design field.
- * @param \Drupal\Core\Entity\FieldableEntityInterface|null $entity
- *   Current entity.
  */
-function hook_helfi_content_cards_design_alter(array &$designs, \Drupal\Core\Field\FieldStorageDefinitionInterface $definition, \Drupal\Core\Entity\FieldableEntityInterface|null $entity) {
+function hook_helfi_content_cards_design_alter(array &$designs) {
 
 }
