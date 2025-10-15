a4096ab2ded6e36ebc244cde9bff59eb7f010f40
Toni Sinisalo <toni.sinisalo@digitalist.com>
2025-10-15 15:35:28 +0300

    UHF-12377: Updating code comment.

--- modules/helfi_recommendations/src/Plugin/Field/FieldType/SuggestedTopicsReferenceItem.php
+++ modules/helfi_recommendations/src/Plugin/Field/FieldType/SuggestedTopicsReferenceItem.php
@@ -125,11 +125,15 @@ final class SuggestedTopicsReferenceItem extends EntityReferenceItem {
   public function preSave(): void {
     parent::preSave();
 
-    // The 'published'-property in this field is computed and synced with the
-    // 'status'-property of the referenced suggested topics'-entity, so changing
-    // that field value will change the published status of the referenced entity,
-    // so the entity needs to be saved at the end. The above parent::preSave()
-    // will take care of this when the entity is new.
+    // The 'published'-property in this field is computed and synced from/to
+    // the 'status'-property of the referenced 'suggested topics'-entity.
+    // Changing that field value will change the published status of the
+    // referenced entity, so the entity needs to be saved at the end.
+    // The above parent::preSave() will take care of this when the entity is
+    // new.
+    //
+    // @see \Drupal\helfi_recommendations\TypedData\ComputedReferencePublishedStatus::getValue().
+    // @see \Drupal\helfi_recommendations\TypedData\ComputedReferencePublishedStatus::setValue().
     if (!$this->entity->isNew()) {
       $this->entity->save();
     }
