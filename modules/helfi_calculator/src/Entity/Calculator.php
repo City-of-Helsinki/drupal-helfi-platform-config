f219c3f715298dacf006c8564461f7742612874e
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-10-16 14:31:32 +0300

    UHF-8360: Added bundle class for the calculator paragraph.

---
+++ modules/helfi_calculator/src/Entity/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_calculator\Entity;
+
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\ParagraphInterface;
+
+/**
+ * Bundle class for calculator paragraph.
+ */
+class Calculator extends Paragraph implements ParagraphInterface {
+
+}
