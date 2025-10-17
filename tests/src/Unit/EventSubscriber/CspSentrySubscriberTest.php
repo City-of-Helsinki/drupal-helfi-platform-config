9a2dbf670d0d6f1a198f3f20378379a68404f9a0
Toni Sinisalo <toni.sinisalo@digitalist.com>
2025-10-17 17:17:31 +0300

    UHF-12384: Updating CSP for javascript error reporting to Sentry.

---
+++ tests/src/Unit/EventSubscriber/CspSentrySubscriberTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_platform_config\Unit\EventSubscriber;
+
+use Drupal\helfi_platform_config\EventSubscriber\CspSentrySubscriber;
+use Prophecy\Argument;
+
+/**
+ * Unit tests for CspSentrySubscriber.
+ *
+ * @group helfi_platform_config
+ * @coversDefaultClass \Drupal\helfi_platform_config\EventSubscriber\CspSentrySubscriber
+ */
+class CspSentrySubscriberTest extends CspEventSubscriberTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->eventSubscriber = new CspSentrySubscriber(
+      $this->environmentResolver->reveal(),
+      $this->configFactory->reveal(),
+      $this->moduleHandler->reveal(),
+    );
+  }
+
+  /**
+   * Tests policy alteration with Sentry DSN.
+   *
+   * @covers ::policyAlter
+   */
+  public function testPolicyAlterWithSentryDsn(): void {
+    putenv('SENTRY_DSN_PUBLIC=https://randomhash@sentry.test.example.com/123');
+    $this->policy->fallbackAwareAppendIfEnabled('connect-src', ['https://sentry.test.example.com'])->shouldBeCalled();
+    $this->eventSubscriber->policyAlter($this->event->reveal());
+  }
+
+  /**
+   * Tests policy alteration with a different Sentry DSN.
+   *
+   * @covers ::policyAlter
+   */
+  public function testPolicyAlterWithDifferentSentryDsn(): void {
+    // Set a different SENTRY_DSN_PUBLIC environment variable.
+    putenv('SENTRY_DSN_PUBLIC=http://anotherrandomhash@sentry.example.com/321');
+    $this->policy->fallbackAwareAppendIfEnabled('connect-src', ['http://sentry.example.com'])->shouldBeCalled();
+    $this->eventSubscriber->policyAlter($this->event->reveal());
+  }
+
+  /**
+   * Tests policy alteration with no Sentry DSN.
+   *
+   * @covers ::policyAlter
+   */
+  public function testPolicyAlterWithNoSentryDsn(): void {
+    // Unset the SENTRY_DSN_PUBLIC environment variable.
+    putenv('SENTRY_DSN_PUBLIC');
+    $this->policy->fallbackAwareAppendIfEnabled('connect-src', Argument::any())->shouldNotBeCalled();
+    $this->eventSubscriber->policyAlter($this->event->reveal());
+  }
+
+}
