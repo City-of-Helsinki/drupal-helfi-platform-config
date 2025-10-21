69894cebe2508c879ea6d7c1b4f85310b3c6e9b0
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-10-21 10:02:29 +0300

    UHF-8360: Added a bundle class for the calculator and tests for it.

--- modules/helfi_calculator/src/Entity/Calculator.php
+++ modules/helfi_calculator/src/Entity/Calculator.php
@@ -12,4 +12,45 @@ use Drupal\paragraphs\ParagraphInterface;
  */
 class Calculator extends Paragraph implements ParagraphInterface {
 
+  /**
+   * Get the calculator name.
+   *
+   * @return string
+   *   The calculator name.
+   *
+   * @throws \LogicException
+   *   Throws an exception if missing the field_calculator value.
+   */
+  public function getCalculatorName(): string {
+    $value = $this->get('field_calculator')->value ?? '';
+    if ($value === '') {
+      throw new \LogicException('Calculator paragraph is missing field_calculator value.');
+    }
+    return $value;
+  }
+
+  /**
+   * Get calculator instance ID.
+   */
+  public function getCalculatorInstanceId(): string {
+    return 'helfi-calculator-' . $this->uuid();
+  }
+
+  /**
+   * Get library name.
+   */
+  public function getCalculatorLibraryName(): string {
+    return 'helfi_calculator/' . $this->getCalculatorName();
+  }
+
+  /**
+   * Get calculator base libraries.
+   */
+  public function getBaseLibraries(): array {
+    return [
+      'helfi_calculator/helfi_calculator.base',
+      'helfi_calculator/helfi_calculator.loader',
+    ];
+  }
+
 }
