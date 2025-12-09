7c059410a3546dd88fb1da9323e13fe304340a0f
tuutti <tuutti@iki.fi>
2025-12-09 12:08:51 +0200

    UHF-12465: Use real http client instead of mocking it

--- modules/helfi_paragraphs_news_list/tests/src/Kernel/Entity/NewsListLazyBuilderTest.php
+++ modules/helfi_paragraphs_news_list/tests/src/Kernel/Entity/NewsListLazyBuilderTest.php
@@ -7,10 +7,10 @@ namespace Drupal\Tests\helfi_paragraphs_news_list\Kernel\Entity;
 use Drupal\helfi_paragraphs_news_list\Entity\ExternalEntity\News;
 use Drupal\helfi_paragraphs_news_list\Entity\NewsListLazyBuilder;
 use Drupal\helfi_paragraphs_news_list\EventSubscriber\CacheResponseSubscriber;
+use Drupal\Tests\helfi_api_base\Traits\ApiTestTrait;
 use Drupal\Tests\helfi_paragraphs_news_list\Kernel\KernelTestBase;
 use Drupal\paragraphs\Entity\Paragraph;
-use Elastic\Elasticsearch\Client;
-use Prophecy\Argument;
+use Elastic\Elasticsearch\ClientBuilder;
 
 /**
  * Tests lazy builder.
@@ -19,36 +19,41 @@ use Prophecy\Argument;
  */
 class NewsListLazyBuilderTest extends KernelTestBase {
 
+  use ApiTestTrait;
+
   /**
    * Tests ::build().
    */
   public function testBuild() : void {
-    $client = $this->prophesize(Client::class);
-    $client->search(Argument::any())
-      ->shouldBeCalled()
-      ->willReturn(
-        $this->createElasticsearchResponse([]),
-        $this->createElasticsearchResponse([
-          'hits' => [
-            'hits' => [
-              [
-                '_source' => [
-                  'uuid_langcode' => ['123'],
-                  'uuid' => ['uuid-123'],
-                  'title' => ['test title'],
-                  'field_news_groups' => ['Test groups'],
-                  'field_news_item_tags' => ['Test tag'],
-                  'field_news_neighbourhoods' => ['Test neighbourhood'],
-                  'url' => ['https://localhost'],
-                  'published_at' => [1234567],
-                  'short_title' => ['test shorttitle'],
-                ],
-              ],
+    $realResponse = [
+      'hits' => [
+        'hits' => [
+          [
+            '_source' => [
+              'uuid_langcode' => ['123'],
+              'uuid' => ['uuid-123'],
+              'title' => ['test title'],
+              'field_news_groups' => ['Test groups'],
+              'field_news_item_tags' => ['Test tag'],
+              'field_news_neighbourhoods' => ['Test neighbourhood'],
+              'url' => ['https://localhost'],
+              'published_at' => [1234567],
+              'short_title' => ['test shorttitle'],
             ],
           ],
-        ]),
-      );
-    $this->container->set('helfi_platform_config.etusivu_elastic_client', $client->reveal());
+        ],
+      ],
+    ];
+    $mock = $this->createMockHttpClient([
+      $this->createElasticsearchResponse([]),
+      $this->createElasticsearchResponse($realResponse),
+      $this->createElasticsearchResponse($realResponse),
+    ]);
+    $client = ClientBuilder::create()
+      ->setHttpClient($mock)
+      ->build();
+
+    $this->container->set('helfi_platform_config.etusivu_elastic_client', $client);
     $sut = $this->container->get(NewsListLazyBuilder::class);
     assert($sut instanceof NewsListLazyBuilder);
 
