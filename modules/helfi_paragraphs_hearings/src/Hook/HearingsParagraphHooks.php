bf71a824f7c54073cde9291efa5df0ea05e1aa51
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-12-12 09:40:35 +0200

    UHF-12398 (#1150)
    * UHF-12398: Remove unused helfi_paragraphs_hearings.install
    
    * UHF-12398: Add cache logic for hearings paragraph
    
    * UHF-12398: Move to OOP hooks
    
    * UHF-12398: Add tests for hearings
    
    * UHF-12398: Use cache value from external entity.

---
+++ modules/helfi_paragraphs_hearings/src/Hook/HearingsParagraphHooks.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_paragraphs_hearings\Hook;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\entity\BundleFieldDefinition;
+use Drupal\helfi_platform_config\DTO\ParagraphTypeCollection;
+use Drupal\paragraphs\ParagraphInterface;
+
+/**
+ * Hooks for helfi_paragraphs_hearings module.
+ */
+class HearingsParagraphHooks {
+
+  use AutowireTrait;
+
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+  ) {
+  }
+
+  /**
+   * Implements hook_ENTITY_TYPE_view().
+   */
+  #[Hook('paragraph_view')]
+  public function view(
+    array &$build,
+    ParagraphInterface $entity,
+    EntityViewDisplayInterface $display,
+  ): void {
+    if ($entity->bundle() !== 'hearings') {
+      return;
+    }
+
+    if ($display->getComponent('list')) {
+      $storage = $this->entityTypeManager
+        ->getStorage('helfi_hearings');
+
+      $entities = $storage->loadMultiple();
+
+      $cache = new CacheableMetadata();
+
+      if (!$entities) {
+        // Retries request every minute if no hearings are found.
+        $cache->setCacheMaxAge(60);
+      }
+
+      foreach ($entities as $item) {
+        // See 'persistent_cache_max_age' for the external entity type.
+        $cache->addCacheableDependency($item);
+
+        $build['list'][] = $this->entityTypeManager
+          ->getViewBuilder('helfi_hearings')
+          ->view($item);
+      }
+
+      $cache->applyTo($build);
+    }
+  }
+
+  /**
+   * Implements hook_entity_extra_field_info().
+   */
+  #[Hook('entity_extra_field_info')]
+  public static function entityExtraFieldInfo() : array {
+    $extra = [];
+    $extra['paragraph']['hearings']['display']['list'] = [
+      'label' => new TranslatableMarkup('List of hearings'),
+      'description' => new TranslatableMarkup('The value for this field is defined in %hook hook.', [
+        '%hook' => 'helfi_paragraphs_hearings_paragraph_view()',
+      ]),
+      'weight' => 100,
+      'visible' => TRUE,
+    ];
+
+    return $extra;
+  }
+
+  /**
+   * Implements hook_helfi_paragraph_types().
+   */
+  #[Hook('helfi_paragraph_types')]
+  public static function helfiParagraphTypes() : array {
+    $types = [
+      'field_content' => [
+        'hearings' => 14,
+      ],
+    ];
+
+    $enabled = [];
+    foreach ($types as $field => $paragraphTypes) {
+      foreach ($paragraphTypes as $paragraphType => $weight) {
+        $enabled[] = new ParagraphTypeCollection('node', 'landing_page', $field, $paragraphType, $weight);
+      }
+    }
+    return $enabled;
+  }
+
+  /**
+   * Implements hook_entity_bundle_field_info_alter().
+   */
+  #[Hook('entity_bundle_field_info_alter')]
+  public static function entityBundleFieldInfoAlter(
+    &$fields,
+    EntityTypeInterface $entity_type,
+    $bundle,
+  ) : void {
+    if ($entity_type->id() === 'helfi_hearings') {
+      $fields['main_image'] = BundleFieldDefinition::create('link')
+        ->setName('main_image')
+        ->setLabel(new TranslatableMarkup('Main image'))
+        ->setTargetEntityTypeId($entity_type->id())
+        ->setTargetBundle($bundle)
+        ->setSettings([
+          'max_length' => 1024,
+        ])
+        ->setDisplayConfigurable('view', TRUE);
+
+      // Additional entity info fields.
+      $entity_info_fields = [
+        'close_at' => new TranslatableMarkup('Close at'),
+        'created_at' => new TranslatableMarkup('Created at'),
+        'open_at' => new TranslatableMarkup('Open at'),
+        'slug' => new TranslatableMarkup('Slug'),
+        'comments' => new TranslatableMarkup('Comments'),
+        'organization' => new TranslatableMarkup('Organization'),
+        'abstract' => new TranslatableMarkup('Abstract'),
+        'main_image_title' => new TranslatableMarkup('Main image title'),
+        'url' => new TranslatableMarkup('Url'),
+        'count' => new TranslatableMarkup('Count'),
+        'langcode' => new TranslatableMarkup('Langcode'),
+        'existing_translations' => new TranslatableMarkup('Existing translations'),
+      ];
+
+      foreach ($entity_info_fields as $field_name => $field_label) {
+        $fields[$field_name] = BundleFieldDefinition::create('string')
+          ->setName($field_name)
+          ->setLabel($field_label)
+          ->setDisplayConfigurable('view', TRUE)
+          ->setDisplayConfigurable('form', TRUE);
+      }
+    }
+  }
+
+}
---
+++ modules/helfi_paragraphs_hearings/tests/src/Kernel/HooksTest.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_paragraphs_hearings\Kernel;
+
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\external_entities\Entity\ExternalEntityType;
+use Drupal\helfi_paragraphs_hearings\Hook\HearingsParagraphHooks;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\Tests\helfi_api_base\Traits\ApiTestTrait;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+/**
+ * Tests hearing paragraph.
+ */
+class HooksTest extends KernelTestBase {
+
+  use ApiTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'paragraphs',
+    'external_entities',
+    'helfi_platform_config',
+    'config_rewrite',
+    'helfi_api_base',
+    'field',
+    'file',
+    'imagecache_external',
+    'responsive_image',
+    'text',
+    'helfi_paragraphs_hearings',
+    'system',
+    'link',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Triggers rebuilding routes.
+    // https://www.drupal.org/project/external_entities/issues/3549828.
+    $this->container
+      ->get(RouteProviderInterface::class)
+      ->getAllRoutes();
+
+    $this->installEntitySchema('paragraph');
+    $this->installConfig(['helfi_paragraphs_hearings']);
+  }
+
+  /**
+   * Test hearing paragraph.
+   */
+  public function testParagraph() {
+    $client = $this->createMockHttpClient([
+      // Success.
+      new Response(body: file_get_contents(dirname(__DIR__, 2) . '/fixtures/hearings.json')),
+      // Empty response.
+      new Response(body: json_encode([])),
+      // Server refuses to brew coffee because it is, permanently, a teapot.
+      new ClientException('I\'m a teapot', new Request('GET', ''), new Response(status: 418)),
+    ]);
+    $this->container->set('http_client', $client);
+
+    $paragraph = Paragraph::create([
+      'type' => 'hearings',
+    ]);
+
+    $build = [];
+
+    $display = $this->prophesize(EntityViewDisplayInterface::class);
+    $display->getComponent('list')->willReturn(['¯\_(ツ)_/¯']);
+
+    $sut = HearingsParagraphHooks::create($this->container);
+
+    $hearings = ExternalEntityType::load('helfi_hearings');
+
+    // Success.
+    $sut->view($build, $paragraph, $display->reveal());
+    $this->assertEquals($hearings->getPersistentCacheMaxAge(), $build['#cache']['max-age']);
+    $this->assertNotEmpty($build['list'] ?? []);
+
+    // Empty response.
+    $sut->view($build, $paragraph, $display->reveal());
+    $this->assertEquals(60, $build['#cache']['max-age']);
+
+    // Server failure.
+    $sut->view($build, $paragraph, $display->reveal());
+    $this->assertEquals(60, $build['#cache']['max-age']);
+  }
+
+}
