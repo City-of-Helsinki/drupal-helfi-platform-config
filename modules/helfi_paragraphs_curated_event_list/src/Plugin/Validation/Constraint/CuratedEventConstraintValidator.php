9e2bcd64c223ed0923855d10bab4a3b32c5c4424
Tero Elonen <tero.elonen@druid.fi>
2025-12-23 10:43:32 +0200

    UHF-12495: Converted the fix to a more simple one

--- modules/helfi_paragraphs_curated_event_list/src/Plugin/Validation/Constraint/CuratedEventConstraintValidator.php
+++ modules/helfi_paragraphs_curated_event_list/src/Plugin/Validation/Constraint/CuratedEventConstraintValidator.php
@@ -24,19 +24,11 @@ class CuratedEventConstraintValidator extends ConstraintValidator {
       return;
     }
 
-    foreach ($events as $delta => $event) {
-      try {
-        if ($event->hasEnded()) {
-          $this->context->buildViolation($constraint->containsEndedEvents, ['%title' => $event->label()])
-            ->atPath((string) $delta . '.target_id')
-            ->addViolation();
-        }
-      } catch (\Exception $e) {
-        // Log the error but don't block the form submission.
-        \Drupal::logger('helfi_paragraphs_curated_event_list')->error(
-          'Error validating event @id: @message',
-          ['@id' => $event->id(), '@message' => $e->getMessage()]
-        );
+    foreach ($events as $key => $event) {
+      if ($event->hasEnded()) {
+        $this->context->buildViolation($constraint->containsEndedEvents, ['%title' => $event->label()])
+          ->atPath((string) $key)
+          ->addViolation();
       }
     }
   }
