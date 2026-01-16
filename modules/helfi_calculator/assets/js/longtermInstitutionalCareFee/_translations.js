a6ab02a53b88198a9c3c00695b8ff6db1cd6cb46
Anna Lonka <anna.lonka@druid.fi>
2026-01-16 11:00:31 +0200

    UHF-11877: New caclulator form long term institutional care fee

---
+++ modules/helfi_calculator/assets/js/longtermInstitutionalCareFee/_translations.js
@@ -0,0 +1,254 @@
+const translations = {
+  social_welfare_act_heading: {
+    fi: 'Sosiaalihuoltolain mukaisuus',
+    sv: 'Tillämpning av socialvårdslagen',
+    en: 'Compliance with the Social Welfare Act',
+  },
+  social_welfare_act_paragraph: {
+    fi: 'Sosiaalihuoltolain mukaisuus vaikuttaa palvelusta perittävään enimmäismaksuun. Voit tarkistaa asian palvelupäätöksestä.',
+    sv: 'Tillämpning av socialvårdslagen påverkar den maximala avgift som kan debiteras för en tjänst. Du kan se detta på servicebeslutet.',
+    en: 'Compliance with the Social Welfare Act affects the maximum fee charged for the service. You can check this in your service decision.',
+  },
+  label_social_welfare_act: {
+    fi: 'Kyseessä on sosiaalihuoltolain mukainen palvelu',
+    sv: 'Det gäller en service som avses i socialvårdslagen',
+    en: 'This service is provided under the Social Welfare Act',
+  },
+  net_income_heading: {
+    fi: 'Nettotulot',
+    sv: 'Nettoinkomster',
+    en: 'Net income',
+  },
+  earned_income: {
+    fi: 'Ansiotulot',
+    sv: 'Förvärvsinkomster',
+    en: 'Earned income',
+  },
+  earned_income_explanation: {
+    fi: 'Asiakkaan yhteenlasketut palkkatulot, ammatinharjoittamisesta tai yritystoiminnasta saadut tulot tai omaishoidon tuet ennen verojen vähentämistä kuukaudessa.',
+    sv: 'Klientens sammanlagda månatliga löneinkomster, inkomster från yrkesutövning eller näringsverksamhet eller stöd för närståendevård före skatt.',
+    en: "The client's total monthly wages, income from self-employment or business activities, or informal care support, before tax.",
+  },
+  client_benefits: {
+    fi: 'Etuudet',
+    sv: 'Förmåner',
+    en: 'Benefits',
+  },
+  client_benefits_explanation: {
+    fi: 'Asiakkaan yhteenlaskettu työeläke, kansaneläke, perhe-eläke, takuueläke, ulkomaan eläke, ansiosidonnaiset työttömyyspäivärahat ja työmarkkinatuki, sairauspäivärahat, veroista vapaat tulot, opintoraha tai aikuiskoulutustuki, eläkettä saavan hoitotuki, lasten kotihoidon tuki, elatusapu tai -tuki, vammaistuki ja päivärahat (esim. sairaus-, työttömyys- tai vanhempainrahat) ennen verojen vähentämistä kuukaudessa.',
+    sv: 'Klientens sammanlagda arbetspension, folkpension, familjepension, garantipension, utländska pensioner, inkomstrelaterade arbetslöshetsdagpenning och arbetsmarknadsstöd, sjukdagpenning, skattefria inkomster, studiestöd, vårdbidrag för pensionstagare, hemvårdsstöd för barn, underhållsbidrag eller -stöd, handikappbidrag och dagpenningar (för sjukdom eller föräldraledighet, per månad före skatt.',
+    en: 'The client’s total monthly earnings-related pension, national pension, survivors’ pension, guarantee pension, foreign pension, earnings-related unemployment benefits and labour market subsidy, sickness allowance, tax-exempt income, student financial aid or adult education allowance, care allowance for pensioners, child home care allowance, maintenance support or maintenance allowance, disability allowance, and daily allowances (e.g. sickness, unemployment or parental allowances) before tax.',
+  },
+  capital_income: {
+    fi: 'Pääomatulot',
+    sv: 'Kapitalinkomster',
+    en: 'Capital income',
+  },
+  capital_income_explanation: {
+    fi: 'Asiakkaan yhteenlasketut osinko-, vuokra- tai korkotulot, elinkorot ja muut pääomatulot ennen verojen vähentämistä kuukaudessa. Pääomatulon määrän voi tarkistaa edellisvuoden verotuspäätöksestä.',
+    sv: 'Klientens sammanlagda dividendinkomster, hyresinkomster eller ränteinkomster, livränta och övriga kapitalinkomster per månad, före avdrag. Kapitalinkomsterna kan ses på beskattningsbeslutet från det föregående skatteåret.',
+    en: 'The client’s total monthly dividends, rental income, interest income, life annuities and other forms of capital income, before tax. The amount of capital income can be checked in the previous year’s tax decision.',
+  },
+  annual_forest_income: {
+    fi: 'Metsän vuotuinen tuotto',
+    sv: 'Årlig avkastning på skog',
+    en: 'Annual forest income',
+  },
+  annual_forest_income_explanation: {
+    fi: 'Asiakkaan omistamien metsien yhteenlaskettu vuotuinen tuotto ennen verojen vähentämistä vuodessa. Metsän keskimääräisen tuoton voi tarkastaa verottajan sivuilta aluekohtaisesti.',
+    sv: 'Den genomsnittliga årliga avkastningen på skog som klienten äger, före skatter. På Skatteförvaltningens webbplats kan man kolla den genomsnittliga avkastningen på skog enligt område.',
+    en: 'The client’s total annual forest income before tax. The average regional forest yield can be checked on the Tax Administration’s website.',
+  },
+  deductions_heading: {
+    fi: 'Vähennykset',
+    sv: 'Avdrag',
+    en: 'Deductions',
+  },
+  guardianship_fees: {
+    fi: 'Edunvalvontamaksut',
+    sv: 'Intressebevakningsavgift',
+    en: 'Guardianship fees',
+  },
+  guardianship_fees_explanation: {
+    fi: 'Asiakkaan yhteenlasketut edunvalvojan palkkion perusmaksu ja edunvalvontavaltuutetun palkkiot kuukaudessa.',
+    sv: 'Klientens sammanlagda avgift för intressebevakarens arvode och intressebevakningsombudets arvode per månad.',
+    en: 'The client’s total monthly basic fee charged by the guardian and fees charged by an authorised representative.',
+  },
+  client_foreclosure: {
+    fi: 'Ulosmittaus',
+    sv: 'Utmätning',
+    en: 'Foreclosure',
+  },
+  client_foreclosure_explanation: {
+    fi: 'Asiakkaan ulosmittauksen määrä kuukaudessa.',
+    sv: 'Klientens belopp som utmäts per månad.',
+    en: 'The client’s monthly amount of foreclosure.',
+  },
+  compensation_or_life_annuity: {
+    fi: 'Hyvitys tai syytinki',
+    sv: 'Gottgörelse eller sytning',
+    en: 'Compensation or life annuity',
+  },
+  compensation_or_life_annuity_explanation: {
+    fi: 'Asiakkaan osuus avopuolisoiden yhteistalouden purkamisesta annetussa laissa tarkoitettu hyvitys tai syytinki kuukaudessa.',
+    sv: 'Klientens andel av månatlig gottgörelse eller sytning som avses i lagen om upplösning av sambors gemensamma hushåll.',
+    en: 'Compensation or life annuity under the Act on the Dissolution of the Household of Cohabiting Partners, paid monthly by the client.',
+  },
+  maintenance_payments: {
+    fi: 'Elatusapu',
+    sv: 'Underhållsbidrag',
+    en: 'Maintenance payments',
+  },
+  maintenance_payments_explanation: {
+    fi: 'Asiakkaan maksaman elatusavun yhteenlaskettu määrä kuukaudessa.',
+    sv: 'Totalbeloppet underhållsbidrag som klienten betalar månatligen.',
+    en: 'The client’s total monthly child maintenance payments.',
+  },
+  label_has_spouse: {
+    fi: 'Onko sinulla puoliso',
+    sv: 'Har du en make eller maka',
+    en: 'Do you have a spouse',
+  },
+  label_yes: {
+    fi: 'Kyllä',
+    sv: 'Ja',
+    en: 'Yes',
+  },
+  label_no: {
+    fi: 'Ei',
+    sv: 'Nej',
+    en: 'No',
+  },
+  spouse_earned_income_explanation: {
+    fi: 'Yhteenlasketut palkkatulot, ammatinharjoittamisesta tai yritystoiminnasta saadut tulot tai omaishoidon tuet ennen verojen vähentämistä kuukaudessa.',
+    sv: 'Sammanlagda månatliga löneinkomster, inkomster från yrkesutövning eller näringsverksamhet eller stöd för närståendevård före skatt.',
+    en: 'Total monthly wages, income from self-employment or business activities, or informal care support, before tax.',
+  },
+  spouse_client_benefits_explanation: {
+    fi: 'Yhteenlaskettu työeläke, kansaneläke, perhe-eläke, takuueläke, ulkomaan eläke, ansiosidonnaiset työttömyyspäivärahat ja työmarkkinatuki, sairauspäivärahat, veroista vapaat tulot, opintoraha tai aikuiskoulutustuki, eläkettä saavan hoitotuki, lasten kotihoidon tuki, elatusapu tai -tuki, vammaistuki ja päivärahat (esim. sairaus-, työttömyys- tai vanhempainrahat) ennen verojen vähentämistä kuukaudessa.',
+    sv: 'Sammanlagd arbetspension, folkpension, familjepension, garantipension, utländska pensioner, inkomstrelaterad arbetslöshetsdagpenning och arbetsmarknadsstöd, sjukdagpenning, skattefria inkomster, studiestöd, vårdbidrag för pensionstagare, hemvårdsstöd för barn, underhållsbidrag eller -stöd, handikappbidrag och dagpenningar (för sjukdom eller föräldraledighet, per månad före skatt.',
+    en: 'Total monthly amounts of the spouse’s earnings-related pension, national pension, survivors’ pension, guarantee pension, foreign pension, earnings-related unemployment benefits and labour market subsidy, sickness allowance, tax-exempt income, student financial aid or adult education allowance, care allowance for pensioners, child home care allowance, maintenance payments or support, disability allowance, and daily allowances (e.g., sickness, unemployment, or parental allowances) before tax.',
+  },
+  spouse_capital_income_explanation: {
+    fi: 'Yhteenlasketut osinko-, vuokra- tai korkotulot, elinkorot ja muut pääomatulot ennen verojen vähentämistä kuukaudessa. Pääomatulon määrän voi tarkistaa edellisvuoden verotuspäätöksestä.',
+    sv: 'Sammanlagda dividendinkomster, hyresinkomster eller ränteinkomster, livränta och övriga kapitalinkomster per månad, före avdrag. Kapitalinkomsterna kan ses på beskattningsbeslutet från det föregående skatteåret.',
+    en: 'Total monthly amounts of the above capital income, before tax. The amount can be checked in the previous year’s tax decision.',
+  },
+  spouse_annual_forest_income_explanation: {
+    fi: 'Metsäomistuksien yhteenlaskettu vuotuinen tuotto ennen verojen vähentämistä vuodessa. Metsän keskimääräisen tuoton voi tarkastaa verottajan sivuilta aluekohtaisesti.',
+    sv: 'Den genomsnittliga årliga avkastningen på skogsegendom, före skatter. På Skatteförvaltningens webbplats kan man kolla den genomsnittliga avkastningen på skog enligt område.',
+    en: 'Total annual forest income before tax. The average regional forest yield can be checked on the Tax Administration’s website.',
+  },
+  spouse_guardianship_fees_explanation: {
+    fi: 'Yhteenlasketut edunvalvojan palkkion perusmaksu ja edunvalvontavaltuutetun palkkiot kuukaudessa.',
+    sv: 'Avgift för intressebevakarens arvode och intressebevakningsombudets arvode per månad.',
+    en: 'Total monthly basic fee charged by the guardian and fees charged by an authorised representative.',
+  },
+  spouse_client_foreclosure_explanation: {
+    fi: 'Ulosmittauksen määrä kuukaudessa.	',
+    sv: 'Belopp som utmäts per månad.',
+    en: 'Monthly amount of foreclosure.',
+  },
+  spouse_compensation_or_life_annuity_explanation: {
+    fi: 'Avopuolisoiden yhteistalouden purkamisesta annetussa laissa tarkoitettu hyvitys tai syytinki kuukaudessa.',
+    sv: 'Månatlig gottgörelse eller sytning som avses i lagen om upplösning av sambors gemensamma hushåll.',
+    en: 'Monthly compensation or life annuity under the same Act.',
+  },
+  spouse_maintenance_payments_explanation: {
+    fi: 'Suoritettavan elatusavun yhteenlaskettu määrä kuukaudessa.',
+    sv: 'Totalbeloppet underhållsbidrag som betalas månatligen.',
+    en: 'Total monthly child maintenance payments paid.',
+  },
+  subtotal_total_income_client: {
+    fi: 'Asiakkaan nettotulot',
+    sv: 'Klientens nettoinkomster',
+    en: 'Client’s net income',
+  },
+  subtotal_total_deductions_client: {
+    fi: 'Asiakkaan vähennykset',
+    sv: 'Klientens avdrag',
+    en: "Client's deductions",
+  },
+  subtotal_total_income_spouse: {
+    fi: 'Puolison nettotulot',
+    sv: 'Makens nettoinkomster',
+    en: 'Spouse’s net income',
+  },
+  subtotal_total_deductions_spouse: {
+    fi: 'Puolison vähennykset',
+    sv: 'Makens avdrag',
+    en: "Spouse's deductions",
+  },
+  spouse_net_income_heading: {
+    fi: 'Puolison nettotulot',
+    sv: 'Makens nettoinkomster',
+    en: "Spouse's net income",
+  },
+  spouse_deductions_heading: {
+    fi: 'Puolison vähennykset',
+    sv: 'Makens avdrag',
+    en: "Spouse's deductions",
+  },
+  spouse_net_income_paragraph: {
+    fi: 'Mikäli asiakkaan tulot ovat pienemmät tai yhtä suuret kuin mahdollisella puolisolla, puolison tuloja ei oteta huomioon asiakasmaksua määriteltäessä.',
+    sv: 'Om klientens inkomster är mindre än eller lika stora som makens, beaktas makens inkomster inte då klientavgiften fastställs.',
+    en: "If the client's income is lower than or equal to that of their spouse, the spouse's income is not considered when determining the fee.",
+  },
+  receipt_estimate_of_payment: {
+    fi: 'Arvio koostuu seuraavista tiedoista',
+    sv: 'Uppskattad klientavgift för långvarig sluten vård',
+    en: 'Estimated long-term institutional care fee',
+  },
+  receipt_estimated_payment_prefix: {
+    fi: 'Arvoitu asiakasmaksu on yhteensä',
+    sv: 'Den uppskattade totala klientavgiften är',
+    en: 'The estimated client fee is',
+  },
+  receipt_estimated_payment_suffix: {
+    fi: 'euroa kuukaudessa',
+    sv: 'euro i månaden',
+    en: 'euros per month',
+  },
+  receipt_estimated_payment_explanation: {
+    fi: 'Tämä arvio on suuntaa antava. Asiakasmaksujen tarkka määrä lasketaan laitoshoidon asiakasmaksupäätökseen. Tämä arvio ei ole viranomaisen tekemä virallinen asiakasmaksupäätös.',
+    sv: 'Denna uppskattning är riktgivande. Den exakta klientavgiften räknas ut då man fattar beslut om sluten vård. Denna uppskattning är inte myndighetens officiella klientavgiftsbeslut.',
+    en: 'This estimate is indicative. The exact fee will be determined in the official decision on the long-term institutional care fee. This estimate is not an official authority decision.',
+  },
+  receipt_estimate_of_payment_breakdown_title: {
+    fi: 'Arvio koostuu seuraavista tiedoista:',
+    sv: 'The estimate is based on the following information:',
+    en: 'Uppskattningen grundar sig på följande uppgifter:',
+  },
+  subtotal_minimum_disposable_amount: {
+    fi: 'Asiakkaalle jäävä vähimmäiskäyttövara',
+    sv: 'Client’s remaining minimum disposable amount',
+    en: 'Minimibelopp för klientens disponibla medel',
+  },
+  subtotal_minimum_disposable_amount_with_spouse: {
+    fi: 'Puolison kanssa yhteensä',
+    sv: 'Combined with spouse, where applicable',
+    en: 'Tillsammans med maken',
+  },
+  additional_detail_spouse_higher_income: {
+    fi: 'Asiakkaan tulot ovat pienemmät tai yhtä suuret kuin puolisolla, jolloin asiakasmaksu on 85 % asiakkaan nettokuukausituloista, joihin on tehty lainmukaiset vähennykset.',
+    sv: 'Klientens inkomster är mindre än eller lika stora som makans varmed klientavgiften är 85 % av klientens sammanlagda nettoinkomster per månad, med de avdrag som föreskrivs i lag.',
+    en: 'The client’s income is lower than or equal to the spouse’s, in which case the fee is 85% of the client’s net monthly income after statutory deductions.',
+  },
+  additional_detail_lower_income: {
+    fi: 'Asiakkaan tulot ovat suuremmat kuin puolisolla, jolloin asiakasmaksu on 42,5 % asiakkaan ja puolison yhteenlasketuista nettokuukausituloista, joihin on tehty lainmukaiset vähennykset.',
+    sv: 'Alternativt: Klientens inkomster är större än makans varmed klientavgiften är 42,5 % av klientens och makans sammanlagda nettoinkomster per månad, med de avdrag som föreskrivs i lag.',
+    en: 'The client’s income is higher than the spouse’s, in which case the fee is 42.5% of the spouses’ combined net monthly income after statutory deductions.',
+  },
+  additional_detail_forest_income: {
+    fi: 'Metsätulo huomioidaan 90-prosenttisesti.',
+    sv: 'Skogsinkomster beaktas till 90 procent.',
+    en: 'Forest income is taken into account at 90%.',
+  },
+  additional_detail_social_welfare_act: {
+    fi: 'Asiakasmaksu on sosiaalihuoltolain enimmäismaksun mukainen.',
+    sv: 'Klientavgiften i enlighet med den maxavgift som avses i socialvårdslagen.',
+    en: 'The fee corresponds to the maximum fee under the Social Welfare Act.',
+  },
+};
+
+export default translations;
---
+++ modules/helfi_calculator/assets/js/longtermInstitutionalCareFee/longtermInstitutionalCareFee.js
@@ -0,0 +1,303 @@
+import form from './_form';
+import translations from './_translations';
+
+class LongtermInstitutionalCareFee {
+  constructor(id, settings) {
+    this.id = id;
+
+    const parsedSettings = JSON.parse(settings);
+
+    // Expecting settings to follow this JSON format:
+    /*
+    const parsedSettings = {
+      "payment_percentage_high": 0.85,
+      "payment_percentage_low": 0.425,
+      "minimum_funds": 122.00,
+      "basic_amount": 539.55,
+      "minimum_funds_spouse": 715.00,
+      "maximum_payment_social_welfare_act": 7656.00,
+      "maximum_payment_other": 12045.00
+    };
+    // */
+    // Form content
+    const getFormData = () => form.getFormData(this.id, this.t, parsedSettings);
+
+    const update = () => {
+      const fields = [{ field: 'has_spouse', group: 'spouse_income_group' }];
+
+      fields.forEach(({ field, group }) => {
+        if (this.calculator.getFieldValue(field) === 'true') {
+          this.calculator.showGroup(group);
+        } else {
+          this.calculator.hideGroup(group);
+        }
+      });
+    };
+
+    const validate = () => {
+      const errorMessages = [];
+
+      // Validate required fields
+      errorMessages.push(...this.calculator.validateBasics('social_welfare_act'));
+      errorMessages.push(...this.calculator.validateBasics('earned_income'));
+      errorMessages.push(...this.calculator.validateBasics('client_benefits'));
+      errorMessages.push(...this.calculator.validateBasics('capital_income'));
+      errorMessages.push(...this.calculator.validateBasics('annual_forest_income'));
+      errorMessages.push(...this.calculator.validateBasics('guardianship_fees'));
+      errorMessages.push(...this.calculator.validateBasics('client_foreclosure'));
+      errorMessages.push(...this.calculator.validateBasics('compensation_or_life_annuity'));
+      errorMessages.push(...this.calculator.validateBasics('maintenance_payments'));
+      errorMessages.push(...this.calculator.validateBasics('has_spouse'));
+
+      if (errorMessages.length) {
+        return {
+          error: {
+            title: this.t('missing_input'),
+            message: errorMessages,
+          },
+        };
+      }
+
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      // Get field values for calculating.
+      const socialWelfareAct = this.calculator.getFieldValue('social_welfare_act');
+
+      // Client income
+      const earnedIncome = Number(this.calculator.getFieldValue('earned_income'));
+      const clientBenefits = Number(this.calculator.getFieldValue('client_benefits'));
+      const capitalIncome = Number(this.calculator.getFieldValue('capital_income'));
+      const annualForestIncome = Number(this.calculator.getFieldValue('annual_forest_income'));
+
+      // Client deductions
+      const guardianshipFees = Number(this.calculator.getFieldValue('guardianship_fees'));
+      const clientForeclosure = Number(this.calculator.getFieldValue('client_foreclosure'));
+      const compensationOrLifeAnnuity = Number(this.calculator.getFieldValue('compensation_or_life_annuity'));
+      const maintenancePayments = Number(this.calculator.getFieldValue('maintenance_payments'));
+
+      const hasSpouse = this.calculator.getFieldValue('has_spouse');
+
+      // Spouse income
+      const spouseEarnedIncome = Number(this.calculator.getFieldValue('spouse_earned_income'));
+      const spouseClientBenefits = Number(this.calculator.getFieldValue('spouse_client_benefits'));
+      const spouseCapitalIncome = Number(this.calculator.getFieldValue('spouse_capital_income'));
+      const spouseAnnualForestIncome = Number(this.calculator.getFieldValue('spouse_annual_forest_income'));
+
+      // Spouse deductions
+      const spouseGuardianshipFees = Number(this.calculator.getFieldValue('spouse_guardianship_fees'));
+      const spouseClientForeclosure = Number(this.calculator.getFieldValue('spouse_client_foreclosure'));
+      const spouseCompensationOrLifeAnnuity = Number(
+        this.calculator.getFieldValue('spouse_compensation_or_life_annuity'),
+      );
+      const spouseMaintenancePayments = Number(this.calculator.getFieldValue('spouse_maintenance_payments'));
+
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      // Calculate results:
+      // 1. Get total income, deductions and net income for client and spouse
+      // 2. Calculate total payment
+
+      //Calculate forest income
+      const annualForestIncomeActual = (annualForestIncome / 12) * 0.9;
+
+      //1. Total income for client
+      const totalIncomeClient = earnedIncome + clientBenefits + capitalIncome + annualForestIncomeActual;
+      const totalDeductionsClient =
+        guardianshipFees + clientForeclosure + compensationOrLifeAnnuity + maintenancePayments;
+      const clientNetIncome = totalIncomeClient - totalDeductionsClient;
+
+      //1. Total income for spouse
+      const spouseAnnualForestIncomeActual = hasSpouse === 'true' ? (spouseAnnualForestIncome / 12) * 0.9 : 0;
+      const totalIncomeSpouse =
+        hasSpouse === 'true'
+          ? spouseEarnedIncome + spouseClientBenefits + spouseCapitalIncome + spouseAnnualForestIncomeActual
+          : 0;
+      const totalDeductionsSpouse =
+        hasSpouse === 'true'
+          ? spouseGuardianshipFees +
+            spouseClientForeclosure +
+            spouseCompensationOrLifeAnnuity +
+            spouseMaintenancePayments
+          : 0;
+      const spouseNetIncome = totalIncomeSpouse - totalDeductionsSpouse;
+
+      let paymentPercentage = 0;
+      let maximumPayment = 0;
+
+      if (totalIncomeClient <= totalIncomeSpouse) {
+        paymentPercentage = parsedSettings.payment_percentage_high;
+      } else {
+        paymentPercentage = parsedSettings.payment_percentage_low;
+      }
+
+      if (socialWelfareAct === 'true') {
+        maximumPayment = parsedSettings.maximum_payment_social_welfare_act;
+      } else {
+        maximumPayment = parsedSettings.maximum_payment_other;
+      }
+
+      let totalPayment = 0;
+      let disposableAmount = 0;
+      let disposableAmountCombined = 0;
+
+      if (paymentPercentage === parsedSettings.payment_percentage_high) {
+        totalPayment = clientNetIncome * paymentPercentage;
+        disposableAmount = clientNetIncome - totalPayment;
+        disposableAmountCombined = clientNetIncome + spouseNetIncome - totalPayment;
+        if (disposableAmount < parsedSettings.minimum_funds) {
+          totalPayment = clientNetIncome - parsedSettings.minimum_funds;
+        }
+      } else {
+        const combinedIncome = clientNetIncome + spouseNetIncome;
+        totalPayment = combinedIncome * paymentPercentage;
+        disposableAmount = clientNetIncome - totalPayment;
+        disposableAmountCombined = combinedIncome - totalPayment;
+        if (disposableAmount < parsedSettings.minimum_funds_spouse) {
+          totalPayment = combinedIncome - parsedSettings.minimum_funds_spouse;
+        }
+      }
+
+      // Clamp payment between 0 and max payment, round to even eurocents
+      totalPayment = this.calculator.clamp(0, Math.round(totalPayment * 100) / 100, maximumPayment);
+
+      const subtotals = [];
+
+      subtotals.push({
+        title: this.t('subtotal_total_income_client'),
+        has_details: false,
+        sum: this.calculator.formatFinnishEuroCents(totalIncomeClient),
+      });
+
+      subtotals.push({
+        title: this.t('subtotal_total_deductions_client'),
+        has_details: false,
+        sum: this.calculator.formatFinnishEuroCents(totalDeductionsClient),
+      });
+
+      if (hasSpouse === 'true') {
+        subtotals.push({
+          title: this.t('subtotal_total_income_spouse'),
+          has_details: false,
+          sum: this.calculator.formatFinnishEuroCents(totalIncomeSpouse),
+        });
+
+        subtotals.push({
+          title: this.t('subtotal_total_deductions_spouse'),
+          has_details: false,
+          sum: this.calculator.formatFinnishEuroCents(totalDeductionsSpouse),
+        });
+      }
+
+      subtotals.push({
+        title: this.t('subtotal_minimum_disposable_amount'),
+        has_details: false,
+        sum: this.calculator.formatFinnishEuroCents(disposableAmount),
+      });
+
+      if (hasSpouse === 'true') {
+        subtotals.push({
+          title: this.t('subtotal_minimum_disposable_amount_with_spouse'),
+          has_details: false,
+          sum: this.calculator.formatFinnishEuroCents(disposableAmountCombined),
+        });
+      }
+
+      const additionalDetails = [];
+
+      if (paymentPercentage === parsedSettings.payment_percentage_high) {
+        additionalDetails.push({
+          title: null,
+          text: this.t('additional_detail_spouse_higher_income'),
+        });
+      } else {
+        additionalDetails.push({
+          title: null,
+          text: this.t('additional_detail_lower_income'),
+        });
+      }
+
+      additionalDetails.push({
+        title: null,
+        text: this.t('additional_detail_forest_income', {
+          maximumPayment: this.calculator.formatFinnishEuroCents(maximumPayment),
+        }),
+      });
+
+      additionalDetails.push({
+        title: null,
+        text: this.t('additional_detail_social_welfare_act', {
+          maximumPayment: this.calculator.formatFinnishEuroCents(maximumPayment),
+        }),
+      });
+
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+      // Create receipt
+      const receiptData = {
+        id: this.id,
+        title: this.t('receipt_estimate_of_payment'),
+        total_prefix: this.t('receipt_estimated_payment_prefix'),
+        total_value: this.calculator.formatFinnishEuroCents(totalPayment),
+        total_suffix: this.t('receipt_estimated_payment_suffix'),
+        total_explanation: this.t('receipt_estimated_payment_explanation'),
+        hr: true,
+        breakdown: [
+          {
+            title: this.t('receipt_estimate_of_payment_breakdown_title'),
+            subtotals: subtotals,
+            additional_details: additionalDetails,
+          },
+        ],
+      };
+
+      const receipt = this.calculator.getPartialRender('{{>receipt}}', receiptData);
+
+      return {
+        receipt,
+        ariaLive: this.t('receipt_aria_live', { totalPayment }),
+      };
+    };
+
+    const eventHandlers = {
+      submit: (event) => {
+        event.preventDefault();
+        this.calculator.clearResult();
+        const result = validate();
+        this.calculator.renderResult(result);
+      },
+      keydown: () => {
+        update();
+      },
+      change: () => {
+        update();
+      },
+      reset: () => {
+        window.setTimeout(update, 1);
+        this.calculator.clearResult();
+        this.calculator.showAriaLiveText(this.t('reset_aria_live'));
+      },
+    };
+
+    // Initialize calculator
+    this.calculator = window.helfiCalculator({
+      name: 'LongtermInstitutionalCareFee',
+      translations,
+    });
+
+    // Translation shortcut
+    this.t = (key, value) => this.calculator.translate(key, value);
+
+    // Parse settings
+    this.settings = this.calculator.parseSettings(settings);
+
+    // Init
+    this.calculator.init({
+      id,
+      formData: getFormData(),
+      eventHandlers,
+    });
+  }
+}
+
+// Expose to global scope
+window.helfiCalculator = window.helfiCalculator || {};
+window.helfiCalculator.long_term_institutional_care_fee = (id, settings) =>
+  new LongtermInstitutionalCareFee(id, settings);
