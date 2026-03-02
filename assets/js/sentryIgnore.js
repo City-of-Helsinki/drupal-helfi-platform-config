fe50e4b60740a0c51ae65e97e8d85c7d62c382af
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2026-03-02 13:32:15 +0200

    Check if the event is a cookie-consent storage SecurityError.

--- assets/js/sentryIgnore.js
+++ assets/js/sentryIgnore.js
@@ -86,6 +86,41 @@
   ];
 
   /**
+   * Checks if the event is the cookie-consent storage SecurityError.
+   *
+   * @todo This should be fixed in HDS cookie consent.
+   * The storage calls should be guarded with try/catch.
+   * The error is thrown when the library tries to read keys from a storage
+   * backend (localStorage, sessionStorage, indexedDB, cacheStorage) in an
+   * environment where the browser blocks that operation (incognito).
+   *
+   * @param {Object} event
+   *   The Sentry event.
+   *
+   * @return {boolean}
+   *   TRUE if the event should be dropped.
+   */
+  const isCookieConsentInsecureOperation = (event) => {
+    const exceptions = event?.exception?.values || [];
+
+    return exceptions.some((exception) => {
+      if (exception?.type !== 'SecurityError') {
+        return false;
+      }
+
+      if (typeof exception?.value !== 'string' || !exception.value.includes('The operation is insecure')) {
+        return false;
+      }
+
+      const frames = exception?.stacktrace?.frames || [];
+      return frames.some((frame) => {
+        const filename = frame?.filename || '';
+        return filename.includes('hds-cookie-consent.min.js') || filename.includes('/hdbt_cookie_banner/');
+      });
+    });
+  };
+
+  /**
    * Checks if the event matches to listed errors.
    *
    * @param {Object} event
@@ -116,7 +151,7 @@
    */
   drupalSettings.raven.options.beforeSend = (event, hint) => {
     // Do not send errors that match the configured errorMatchers to Sentry.
-    if (isLoadFailedError(event)) {
+    if (isLoadFailedError(event) || isCookieConsentInsecureOperation(event)) {
       return null;
     }
 
