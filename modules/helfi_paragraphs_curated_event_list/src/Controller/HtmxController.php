7f372cc89ff6cbfdbb94e9c63570f53e27bfa978
tuutti <tuutti@iki.fi>
2026-03-17 09:23:36 +0200

    UHF-12617: Initial working version

--- modules/helfi_paragraphs_curated_event_list/src/Controller/HtmxController.php
+++ modules/helfi_paragraphs_curated_event_list/src/Controller/HtmxController.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_paragraphs_curated_event_list\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\helfi_paragraphs_curated_event_list\Entity\LazyViewBuilder;
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\helfi_paragraphs_curated_event_list\Entity\LinkedEventsEvent;
 use Drupal\paragraphs\Entity\Paragraph;
 
 /**
@@ -14,15 +19,84 @@ use Drupal\paragraphs\Entity\Paragraph;
  * @see \Drupal\helfi_paragraphs_curated_event_list\Entity\LazyViewBuilder
  * @see \helfi_paragraphs_curated_event_list_paragraph_view()
  */
-final class HtmxController extends ControllerBase {
+final readonly class HtmxController implements ContainerInjectionInterface {
+
+  use AutowireTrait;
 
   public function __construct(
-    private LazyViewBuilder $lazyViewBuilder,
+    private EntityTypeManagerInterface $entityTypeManager,
+    private RendererInterface $renderer,
+    private TimeInterface $time,
   ) {
   }
 
+  /**
+   * A HTMX callback for Curated event list.
+   *
+   * @param \Drupal\paragraphs\Entity\Paragraph $paragraph
+   *   The paragraph.
+   *
+   * @return array
+   *   A render array of results.
+   */
   public function content(Paragraph $paragraph): array {
-    return $this->lazyViewBuilder->build($paragraph)
+    $currentTime = $this->time->getCurrentTime();
+
+    $selections = $paragraph->get('field_events')->referencedEntities();
+
+    $ids = array_map(function (LinkedEventsEvent $event) {
+      return $event->id();
+    }, $selections);
+
+    $build = [
+      '#cache' => [
+        'max-age' => 3600,
+      ],
+    ];
+    $this->renderer->addCacheableDependency($build, $paragraph);
+    $this->renderer->addCacheableDependency($build, $paragraph->getParentEntity());
+
+    $storage = $this->entityTypeManager->getStorage('linkedevents_event');
+    /** @var \Drupal\helfi_paragraphs_curated_event_list\Entity\LinkedEventsEvent[] $entities */
+    $entities = $storage->loadMultiple($ids);
+
+    if (!$entities) {
+      $build['message'] = [
+        '#markup' => new TranslatableMarkup('Recommended events were not found', options: [
+          'context' => 'Curated events list empty message',
+        ]),
+      ];
+      return $build;
+    }
+    // Expire in one month by default.
+    $maxAge = 2.628e+6;
+
+    foreach ($entities as $entity) {
+      $entity->addCacheableDependency($paragraph);
+
+      if ($endTime = $entity->getEndTime()?->getTimestamp()) {
+        // Skip expired items.
+        if ($endTime < $currentTime) {
+          continue;
+        }
+        $newExpireTime = ($endTime - $currentTime) + 5;
+
+        // Max-age should match the first expiring item so the block
+        // is invalidated as soon as the event expires.
+        if ($newExpireTime < $maxAge) {
+          $maxAge = $newExpireTime;
+        }
+      }
+
+      $build[] = $this->entityTypeManager->getViewBuilder('linkedevents_event')
+        ->view($entity);
+    }
+    // Show maximum of three items.
+    $build = array_slice($build, 0, 3);
+
+    $build['#cache']['max-age'] = $maxAge;
+
+    return $build;
   }
 
 }
