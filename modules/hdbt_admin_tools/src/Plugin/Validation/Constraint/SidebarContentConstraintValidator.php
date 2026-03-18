2b65cbb621f95ce26d92938a083ed71186d61efb
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2026-03-18 14:31:51 +0200

    UHF-12699: Use containerInjectionInterface.

--- modules/hdbt_admin_tools/src/Plugin/Validation/Constraint/SidebarContentConstraintValidator.php
+++ modules/hdbt_admin_tools/src/Plugin/Validation/Constraint/SidebarContentConstraintValidator.php
@@ -4,18 +4,34 @@ declare(strict_types=1);
 
 namespace Drupal\hdbt_admin_tools\Plugin\Validation\Constraint;
 
-use Symfony\Component\Validator\Constraint;
-use Symfony\Component\Validator\ConstraintValidator;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
 
 /**
  * Validates the Sidebar Content constraint.
  *
  * @todo UHF-13030 Remove this when the field is removed.
  */
-class SidebarContentConstraintValidator extends ConstraintValidator {
+final class SidebarContentConstraintValidator extends ConstraintValidator implements ContainerInjectionInterface {
+
   use StringTranslationTrait;
+
+  public function __construct(
+    private readonly MessengerInterface $messenger,
+  ) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): static {
+    return new static(
+      $container->get(MessengerInterface::class),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -23,11 +39,9 @@ class SidebarContentConstraintValidator extends ConstraintValidator {
   public function validate(mixed $value, Constraint $constraint): void {
     assert($constraint instanceof SidebarContentConstraint);
 
-    $messenger = \Drupal::service(MessengerInterface::class);
-
     if ($value->count() > 0) {
       $this->context->addViolation($constraint->sidebarContentExists);
-      $messenger->addError($this->t('The sidebar content area will be removed. Please move the content to the upper or lower content area or remove it.'));
+      $this->messenger->addError($this->t('The sidebar content area will be removed. Please move the content to the upper or lower content area or remove it.'));
     }
   }
 
