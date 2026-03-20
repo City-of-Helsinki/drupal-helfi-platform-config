fccac1cec4b6108401008b0f8051c49aeaf94431
Risto Nykänen <risto.nykanen@druid.fi>
2026-03-20 08:46:08 +0200

    UHF-13017: add the rest csp changes

--- src/EventSubscriber/CspTeliaAceWidgetSubscriber.php
+++ src/EventSubscriber/CspTeliaAceWidgetSubscriber.php
@@ -7,7 +7,7 @@ namespace Drupal\helfi_platform_config\EventSubscriber;
 /**
  * Event subscriber for CSP policy alteration.
  *
- * CSP directives for 'telia_ace_widget'-block.
+ * CSP directives for 'telia_ace_widget'-block and 'telia_ace_authenticated_widger'-block.
  *
  * @package Drupal\helfi_platform_config\EventSubscriber
  */
@@ -18,18 +18,22 @@ class CspTeliaAceWidgetSubscriber extends CspSubscriberBase {
     'https://wds.ace.teliacompany.com',
     'https://chat.ace.teliacompany.net',
     'https://api.ace.teliacompany.net',
+    'https://widgets.ace.teliacompany.net',
   ];
   const FONT_SRC = [
     'https://hel.humany.net',
     'https://ace-knowledge-cdn.teliacompany.net',
     'https://makasiini.hel.ninja',
+    'https://widgets.ace.teliacompany.net',
   ];
   const FRAME_SRC = [
     'https://wds.ace.teliacompany.com',
+    'https://widgets.ace.teliacompany.net',
   ];
   const IMG_SRC = [
     'https://hel.humany.net',
     'https://wds.ace.teliacompany.com',
+    'https://widgets.ace.teliacompany.net',
   ];
   const SCRIPT_SRC = [
     'https://wds.ace.teliacompany.com',
@@ -38,6 +42,7 @@ class CspTeliaAceWidgetSubscriber extends CspSubscriberBase {
   const STYLE_SRC = [
     'https://hel.humany.net',
     'https://wds.ace.teliacompany.com',
+    'https://widgets.ace.teliacompany.net',
   ];
 
 }
