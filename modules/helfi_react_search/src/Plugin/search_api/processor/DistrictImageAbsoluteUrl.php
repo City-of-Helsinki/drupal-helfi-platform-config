cc541748834ad21b7f79f0d4e8f6651b7ed17922
tuutti <tuutti@iki.fi>
2026-03-27 08:46:49 +0200

    UHF-12862: Changed similar image fields to use same base class

--- modules/helfi_react_search/src/Plugin/search_api/processor/DistrictImageAbsoluteUrl.php
+++ modules/helfi_react_search/src/Plugin/search_api/processor/DistrictImageAbsoluteUrl.php
@@ -4,32 +4,33 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_react_search\Plugin\search_api\processor;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\helfi_platform_config\SearchAPI\Processor\MainImageUrlProcessorBase;
 use Drupal\helfi_platform_config\SearchAPI\Processor\MainImageProcessorProperties;
 use Drupal\node\NodeInterface;
+use Drupal\search_api\Attribute\SearchApiProcessor;
 
 /**
- * Get start and end date for daterange field.
- *
- * @SearchApiProcessor(
- *    id = "district_image_absolute_url",
- *    label = @Translation("Image absolute URL"),
- *    description = @Translation("Generate absolute URL for image"),
- *    stages = {
- *      "add_properties" = 0,
- *    },
- *    locked = true,
- *    hidden = true,
- * )
+ * Indexes District image uri in correct image style.
  */
-class DistrictImageAbsoluteUrl extends MainImageUrlProcessorBase {
+#[SearchApiProcessor(
+  id: 'district_image_absolute_url',
+  label: new TranslatableMarkup('Image absolute URL'),
+  description: new TranslatableMarkup('Generate absolute URL for image'),
+  stages: [
+    'add_properties' => 0,
+  ],
+  locked: TRUE,
+  hidden: TRUE,
+)]
+final class DistrictImageAbsoluteUrl extends MainImageUrlProcessorBase {
 
   /**
    * {@inheritdoc}
    */
   protected function getFieldProperties(): MainImageProcessorProperties {
     return new MainImageProcessorProperties(
-      searchApiField: 'district_image_absolute_url',
+      imageStyleField: 'district_image_absolute_url',
       entityField: 'field_district_image',
     );
   }
--- src/SearchAPI/Processor/MainImageProcessorProperties.php
+++ src/SearchAPI/Processor/MainImageProcessorProperties.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_platform_config\SearchAPI\Processor;
 
+/**
+ * A DTO to represent 'main image' Search API processor properties.
+ */
 final readonly class MainImageProcessorProperties {
 
   public function __construct(
-    public string $searchApiField,
+    public string $imageStyleField,
     public string $entityField,
   ) {
   }
 
 }
-
