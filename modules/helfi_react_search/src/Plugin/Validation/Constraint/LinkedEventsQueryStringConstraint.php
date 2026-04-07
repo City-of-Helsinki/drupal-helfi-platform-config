3ad227de5994cb9d93aadf9149744254b14feae9
Toni Sinisalo <toni.sinisalo@digitalistgroup.com>
2026-04-07 15:50:25 +0300

    UHF-12803: Updating constraint plugin to use php attributes and named arguments.

--- modules/helfi_react_search/src/Plugin/Validation/Constraint/LinkedEventsQueryStringConstraint.php
+++ modules/helfi_react_search/src/Plugin/Validation/Constraint/LinkedEventsQueryStringConstraint.php
@@ -4,33 +4,32 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_react_search\Plugin\Validation\Constraint;
 
-use Symfony\Component\Validator\Constraint;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Validation\Attribute\Constraint;
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
+use Symfony\Component\Validator\Constraint as SymfonyConstraint;
 
 /**
  * Checks that the submitted value is a valid LinkedEvents API query string.
- *
- * @Constraint(
- *   id = "LinkedEventsQueryString",
- *   label = @Translation("LinkedEvents API query string", context = "Validation"),
- *   type = "string"
- * )
  */
-class LinkedEventsQueryStringConstraint extends Constraint {
+#[Constraint(
+  id: 'LinkedEventsQueryString',
+  label: new TranslatableMarkup('LinkedEvents API query string', options: ['context' => 'Validation']),
+  type: ['string']
+)]
+class LinkedEventsQueryStringConstraint extends SymfonyConstraint {
 
-  /**
-   * The disallowed query parameters.
-   */
-  public array $disallowedQueryParameters = [
-    'all_ongoing',
-    'all_ongoing_AND',
-    'all_ongoing_OR',
-  ];
-
-  /**
-   * The error message.
-   *
-   * Will be shown if the value is not a valid LinkedEvents API query string.
-   */
-  public string $notValid = '%value is not a valid LinkedEvents API query string. Please remove the disallowed query parameters: %disallowedQueryParameters. Parameter <code>all_ongoing</code> is no longer needed and parameters <code>all_ongoing_AND</code> and <code>all_ongoing_OR</code> should be replaced with <code>full_text</code>.';
+  #[HasNamedArguments]
+  public function __construct(
+    public readonly array $disallowedQueryParameters = [
+      'all_ongoing',
+      'all_ongoing_AND',
+      'all_ongoing_OR',
+    ],
+    public readonly string $notValid = '%value is not a valid LinkedEvents API query string. Please remove the disallowed query parameters: %disallowedQueryParameters. Parameter <code>all_ongoing</code> is no longer needed and parameters <code>all_ongoing_AND</code> and <code>all_ongoing_OR</code> should be replaced with <code>full_text</code>.',
+    mixed ...$args,
+  ) {
+    parent::__construct(...$args);
+  }
 
 }
