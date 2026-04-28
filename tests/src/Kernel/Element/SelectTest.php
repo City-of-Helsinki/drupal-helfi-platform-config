fd766b10e797111f63e56cd7243f7da006609b6a
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-04-28 09:38:28 +0300

    UHF-12964: Helfi select element (#1269)
    * Install tom-select from NPM
    
    * Add tom-select based select element
    
    * Support multiselect and autocomplete
    
    * UHF-12964: Configure tom-select to not to open on focus, adjust spacebar and enter key functionality
    
    ---------
    
    Co-authored-by: Tero Elonen <tero.elonen@druid.fi>

---
+++ tests/src/Kernel/Element/SelectTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_platform_config\Kernel\Element;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Tests\helfi_platform_config\Kernel\KernelTestBase;
+
+/**
+ * Tests select element.
+ */
+class SelectTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'serialization',
+    'system',
+  ];
+
+  /**
+   * Tests location autocomplete element rendering.
+   */
+  public function testSelect(): void {
+    $form = $this->buildForm([
+      'field' => [
+        '#type' => 'helfi_select',
+        '#empty_option' => '- All -',
+        '#autocomplete_route_name' => 'system.404',
+      ],
+    ]);
+
+    $markup = $this->render($form);
+
+    // Tests that the element renders correctly.
+    $this->assertStringContainsString('helfi-select', $markup);
+    $this->assertStringContainsString('data-autocomplete-path="/system/404"', $markup);
+  }
+
+  /**
+   * Builds form.
+   *
+   * @param array<string, mixed> $form
+   *   Form render array.
+   */
+  private function buildForm(array $form): mixed {
+    $formState = new FormState();
+
+    $formBuilder = $this->container
+      ->get(FormBuilderInterface::class);
+
+    return $formBuilder->buildForm(new class($form) extends FormBase {
+
+      /**
+       * Constructs the anonymous form.
+       *
+       * @param array<string, mixed> $renderArray
+       *   The render array to merge into the form.
+       */
+      public function __construct(private readonly array $renderArray) {
+      }
+
+      /**
+       * {@inheritdoc}
+       */
+      public function getFormId(): string {
+        return 'test_form';
+      }
+
+      /**
+       * {@inheritdoc}
+       *
+       * @phpstan-param array<string, mixed> $form
+       *
+       * @return array<string, mixed>
+       *   The built form.
+       */
+      public function buildForm(array $form, FormStateInterface $form_state): array {
+        $form += $this->renderArray;
+        return $form;
+      }
+
+      /**
+       * {@inheritdoc}
+       *
+       * @phpstan-param array<string, mixed> $form
+       */
+      public function submitForm(array &$form, FormStateInterface $form_state): void {
+      }
+
+    }, $formState);
+  }
+
+}
