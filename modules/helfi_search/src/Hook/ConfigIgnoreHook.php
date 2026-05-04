5ebdd0d51c0e1e0586bf67cb49dc7a294ca67ed6
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-05-04 16:29:11 +0300

    UHF-13119 Combined settings form (#1275)
    * UHF-13119: Move settings form from etusivu to shared module

--- modules/helfi_search/src/Hook/ConfigIgnoreHook.php
+++ modules/helfi_search/src/Hook/ConfigIgnoreHook.php
@@ -26,6 +26,8 @@ final readonly class ConfigIgnoreHook {
       'helfi_search.settings:deboost_bundles',
       'helfi_search.settings:deboost_factor',
       'helfi_search.settings:min_score',
+      'helfi_search.settings:external_links',
+      'helfi_search.settings:ai_register_url',
     ];
     foreach ($ignored as $entry) {
       if (!in_array($entry, $settings, TRUE)) {
