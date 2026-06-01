400b57a4f875341dba40d53e0eb9491d5052859b
Tero Elonen <tero.elonen@druid.fi>
2026-06-01 18:02:00 +0300

    UHF-13297: Readd the Translation outdated feature to the branch

---
+++ modules/helfi_users/src/Plugin/QueueWorker/TranslationOutdatedWorker.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_users\Plugin\QueueWorker;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\Attribute\QueueWorker;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\helfi_users\TranslationOutdatedManager;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+#[QueueWorker(
+  id: 'translation_outdated_check',
+  title: new TranslatableMarkup('Translation outdated check'),
+  cron: [
+    'time' => 30
+  ]
+)]
+class TranslationOutdatedWorker extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  private EntityTypeManagerInterface $entityTypeManager;
+  private TranslationOutdatedManager $translationOutdatedManager;
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    $instance = new self($configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get(EntityTypeManagerInterface::class);
+    $instance->translationOutdatedManager = $container->get(TranslationOutdatedManager::class);
+    return $instance;
+  }
+
+  public function processItem(mixed $data): void {
+    $node = $this->entityTypeManager->getStorage('node')->load($data['nid']);
+    if (!$node instanceof NodeInterface) {
+      return;
+    }
+    $this->translationOutdatedManager->updateOutdatedFlags($node);
+  }
+
+}
