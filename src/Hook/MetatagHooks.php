67e318a56ab1e31c35b30bab631effaa94296bed
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-06-23 08:56:07 +0300

    Move hook to platform config

--- src/Hook/MetatagHooks.php
+++ src/Hook/MetatagHooks.php
@@ -2,26 +2,21 @@
 
 declare(strict_types=1);
 
-namespace Drupal\hdbt_admin_tools\Hook;
+namespace Drupal\helfi_platform_config\Hook;
 
-use Drupal\Core\DependencyInjection\AutowireTrait;
 use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\Core\Render\Element;
 use Drupal\Core\Routing\AdminContext;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
- * Hook implementations for metatag form alterations.
+ * Hook implementations for form alterations.
  */
-final class MetatagHooks {
-
-  use AutowireTrait;
-  use StringTranslationTrait;
+final readonly class MetatagHooks {
 
   public function __construct(
-    private readonly AdminContext $adminContext,
+    private AdminContext $adminContext,
   ) {
   }
 
@@ -31,6 +26,8 @@ final class MetatagHooks {
    * The advanced metatag group exposes a large number of tags that editors
    * should not need to touch. Hide everything except the robots "noindex"
    * setting.
+   *
+   * @phpstan-param array<string, mixed> $form
    */
   #[Hook('form_alter')]
   public function formAlter(array &$form, FormStateInterface $form_state, string $form_id): void {
