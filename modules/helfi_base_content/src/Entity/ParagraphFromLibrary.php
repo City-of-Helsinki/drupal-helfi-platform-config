dc288e363f953a1da6f4aff3b025a6b770e656d3
Anna Lonka <anna.lonka@druid.fi>
2026-07-10 10:40:54 +0300

    UHF-12730: Added published status to view

--- modules/helfi_base_content/src/Entity/ParagraphFromLibrary.php
+++ modules/helfi_base_content/src/Entity/ParagraphFromLibrary.php
@@ -17,7 +17,7 @@ class ParagraphFromLibrary extends Paragraph implements ParagraphInterface {
    * Checks whether the referenced library item is published.
    *
    * @return bool
-   *   TRUE if the referenced library item exists and is published.
+   *   TRUE if the referenced library item exists and is not published.
    */
   public function isNotPublished(): bool {
     $libraryItem = $this->get('field_reusable_paragraph')->entity;
