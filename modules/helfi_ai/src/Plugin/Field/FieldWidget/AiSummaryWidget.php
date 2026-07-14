ba9f9f643ad734975c1660097c176bfad26041b2
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2026-07-14 12:46:51 +0300

    UHF-13337: Use configFactory.

--- modules/helfi_ai/src/Plugin/Field/FieldWidget/AiSummaryWidget.php
+++ modules/helfi_ai/src/Plugin/Field/FieldWidget/AiSummaryWidget.php
@@ -35,6 +35,39 @@ final class AiSummaryWidget extends WidgetBase implements ContainerFactoryPlugin
   private const string TEXT_FORMAT = 'minimal';
 
   /**
+   * The constructor.
+   *
+   * @phpstan-param array<mixed> $settings
+   * @phpstan-param array<mixed> $third_party_settings
+   */
+  public function __construct(
+    string $plugin_id,
+    mixed $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    array $third_party_settings,
+    private readonly ConfigFactoryInterface $configFactory,
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @phpstan-param array<string, mixed> $configuration
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['third_party_settings'],
+      $container->get('config.factory'),
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function isApplicable(FieldDefinitionInterface $field_definition): bool {
