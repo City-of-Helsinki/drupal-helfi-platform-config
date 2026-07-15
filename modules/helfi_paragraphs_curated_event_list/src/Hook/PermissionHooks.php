1cfb2634019106fbc891f9413bdf468856984a32
tuutti <tuutti@iki.fi>
2026-07-15 07:46:26 +0300

    UHF-13371: Remove ended events from curated event lists on entity save

---
+++ modules/helfi_paragraphs_curated_event_list/src/Hook/PermissionHooks.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_paragraphs_curated_event_list\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Permission hooks.
+ */
+final class PermissionHooks {
+
+  /**
+   * Implements hook_platform_config_grant_permissions().
+   */
+  #[Hook(hook: 'platform_config_grant_permissions')]
+  public function permissions(): array {
+    $permissions = [
+      // These permissions are required for the Curated Events paragraph widget.
+      // Without them, the entity reference field displays a "Restricted access"
+      // message.
+      'update linkedevents_event external entity',
+      'view linkedevents_event external entity',
+      'view linkedevents_event external entity collection',
+    ];
+    return [
+      'admin' => $permissions,
+      'content_producer' => $permissions,
+      'editor' => $permissions,
+    ];
+  }
+
+}
