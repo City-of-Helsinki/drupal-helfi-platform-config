af74aaeb57d6edfefcf2c6b0890b61a9412074fd
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-08-20 07:36:16 +0300

    UHF-13342: Grant admins access to search settings

---
+++ modules/helfi_search/src/Hook/PlatformConfigHooks.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_search\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Hook implementations.
+ */
+final class PlatformConfigHooks {
+
+  /**
+   * Implements hook_platform_config_grant_permissions().
+   *
+   * @return array<string, string[]>
+   *   Permissions to grant, keyed by role ID.
+   */
+  #[Hook('platform_config_grant_permissions')]
+  public function grantPermissions(): array {
+    return [
+      'admin' => [
+        'administer helfi_search'
+      ],
+    ];
+  }
+
+}
