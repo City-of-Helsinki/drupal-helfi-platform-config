b34e2edbef9e5b308e8666de5002b782f0917aa4
Toni Sinisalo <toni.sinisalo@digitalistgroup.com>
2026-09-07 14:17:05 +0300

    UHF-13388: Adding new event list paragraph field to control visibility of target group filters.

--- modules/helfi_react_search/src/Enum/Filters.php
+++ modules/helfi_react_search/src/Enum/Filters.php
@@ -14,13 +14,13 @@ enum Filters: string {
   case RemoteEvents = 'field_remote_events';
   case Language = 'field_language';
   case SearchTerm = 'field_search_term';
-  case TargetGroups = 'field_target_groups';
+  case TargetGroup = 'field_target_group';
 
   // Return the Drupal setting name for the filter. Defaults to the field name,
   // but allows overrides where needed.
   public function drupalSettingName(): string {
     return match ($this) {
-      self::TargetGroups => 'useTargetGroupFilter',
+      self::TargetGroup => 'useTargetGroupFilter',
       default => $this->value,
     };
   }
