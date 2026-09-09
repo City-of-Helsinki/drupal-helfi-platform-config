9b157283f266a5e22b232e8432c42f83b29c9304
Toni Sinisalo <toni.sinisalo@digitalistgroup.com>
2026-09-09 10:39:30 +0300

    UHF-13388: Fixing code quality issues.

--- modules/helfi_react_search/src/Enum/Filters.php
+++ modules/helfi_react_search/src/Enum/Filters.php
@@ -16,12 +16,16 @@ enum Filters: string {
   case SearchTerm = 'field_search_term';
   case TargetGroup = 'field_target_group';
 
-  // Return the Drupal setting name for the filter. Defaults to the field name,
-  // but allows overrides where needed.
+  /**
+   * Return the Drupal setting name for the filter.
+   *
+   * Defaults to the field name, allowing overrides where needed.
+   */
   public function drupalSettingName(): string {
     return match ($this) {
       self::TargetGroup => 'useTargetGroupFilter',
       default => $this->value,
     };
   }
+
 }
