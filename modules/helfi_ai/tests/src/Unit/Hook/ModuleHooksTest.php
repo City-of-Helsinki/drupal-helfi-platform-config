281a734c20293bc90ecb5bf20746ed12f50a37cf
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2026-09-17 11:15:50 +0300

    UHF-13335: Updated tests.

---
+++ modules/helfi_ai/tests/src/Unit/Hook/ModuleHooksTest.php
@@ -0,0 +1,240 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_ai\Unit\Hook;
+
+use Drupal\Core\Config\Entity\ConfigEntityStorageInterface;
+use Drupal\Core\Entity\Display\EntityFormDisplayInterface;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\helfi_ai\Hook\ModuleHooks;
+use Drupal\Tests\UnitTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use Prophecy\Prophecy\ObjectProphecy;
+
+/**
+ * Tests the ai_summary field installation for tpr_service.
+ */
+#[Group('helfi_ai')]
+#[CoversClass(ModuleHooks::class)]
+class ModuleHooksTest extends UnitTestCase {
+
+  /**
+   * The entity definition update manager.
+   *
+   * @var \Prophecy\Prophecy\ObjectProphecy
+   */
+  private ObjectProphecy $entityDefinitionUpdateManager;
+
+  /**
+   * The entity field manager.
+   *
+   * @var \Prophecy\Prophecy\ObjectProphecy
+   */
+  private ObjectProphecy $entityFieldManager;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Prophecy\Prophecy\ObjectProphecy
+   */
+  private ObjectProphecy $entityTypeManager;
+
+  /**
+   * The tpr_service form display.
+   *
+   * @var \Prophecy\Prophecy\ObjectProphecy
+   */
+  private ObjectProphecy $formDisplay;
+
+  /**
+   * The tpr_service view display.
+   *
+   * @var \Prophecy\Prophecy\ObjectProphecy
+   */
+  private ObjectProphecy $viewDisplay;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->entityDefinitionUpdateManager = $this->prophesize(EntityDefinitionUpdateManagerInterface::class);
+    $this->entityFieldManager = $this->prophesize(EntityFieldManagerInterface::class);
+    $this->entityTypeManager = $this->prophesize(EntityTypeManagerInterface::class);
+    $this->formDisplay = $this->prophesize(EntityFormDisplayInterface::class);
+    $this->viewDisplay = $this->prophesize(EntityViewDisplayInterface::class);
+  }
+
+  /**
+   * Sets up a site that has the tpr_service entity type and the field.
+   *
+   * @return \Drupal\Core\Field\FieldStorageDefinitionInterface
+   *   The ai_summary field definition.
+   */
+  private function setUpTprService(): FieldStorageDefinitionInterface {
+    $definition = $this->prophesize(FieldStorageDefinitionInterface::class)->reveal();
+
+    $this->entityTypeManager->hasDefinition('tpr_service')->willReturn(TRUE);
+    $this->entityFieldManager->getFieldStorageDefinitions('tpr_service')
+      ->willReturn(['ai_summary' => $definition]);
+
+    $displays = [
+      'entity_form_display' => $this->formDisplay,
+      'entity_view_display' => $this->viewDisplay,
+    ];
+
+    foreach ($displays as $entityTypeId => $display) {
+      $storage = $this->prophesize(ConfigEntityStorageInterface::class);
+      $storage->load('tpr_service.tpr_service.default')->willReturn($display->reveal());
+      $this->entityTypeManager->getStorage($entityTypeId)->willReturn($storage->reveal());
+    }
+
+    return $definition;
+  }
+
+  /**
+   * Builds the hooks object under test with mocked dependencies.
+   *
+   * @return \Drupal\helfi_ai\Hook\ModuleHooks
+   *   The hooks object.
+   */
+  private function createHooks(): ModuleHooks {
+    return new ModuleHooks(
+      $this->entityDefinitionUpdateManager->reveal(),
+      $this->entityFieldManager->reveal(),
+      $this->entityTypeManager->reveal(),
+    );
+  }
+
+  /**
+   * Tests that a missing field is installed for helfi_ai.
+   */
+  public function testInstallsMissingFieldStorage(): void {
+    $definition = $this->setUpTprService();
+    $this->entityDefinitionUpdateManager->getFieldStorageDefinition('ai_summary', 'tpr_service')
+      ->willReturn(NULL);
+
+    $this->entityDefinitionUpdateManager
+      ->installFieldStorageDefinition('ai_summary', 'tpr_service', 'helfi_ai', $definition)
+      ->shouldBeCalled();
+    $this->entityDefinitionUpdateManager->updateFieldStorageDefinition($definition)
+      ->shouldNotBeCalled();
+
+    $this->createHooks()->installAiSummaryField();
+  }
+
+  /**
+   * Tests that the field installed by helfi_tpr_config is taken over.
+   */
+  public function testTakesOverFieldStorageInstalledByTprConfig(): void {
+    $definition = $this->setUpTprService();
+
+    $installed = $this->prophesize(FieldStorageDefinitionInterface::class);
+    $installed->getProvider()->willReturn('helfi_tpr_config');
+    $this->entityDefinitionUpdateManager->getFieldStorageDefinition('ai_summary', 'tpr_service')
+      ->willReturn($installed->reveal());
+
+    $this->entityDefinitionUpdateManager->updateFieldStorageDefinition($definition)
+      ->shouldBeCalled();
+    $this->entityDefinitionUpdateManager
+      ->installFieldStorageDefinition('ai_summary', 'tpr_service', 'helfi_ai', $definition)
+      ->shouldNotBeCalled();
+
+    $this->createHooks()->installAiSummaryField();
+  }
+
+  /**
+   * Tests that a field already owned by helfi_ai is left alone.
+   */
+  public function testKeepsFieldStorageInstalledByHelfiAi(): void {
+    $definition = $this->setUpTprService();
+
+    $installed = $this->prophesize(FieldStorageDefinitionInterface::class);
+    $installed->getProvider()->willReturn('helfi_ai');
+    $this->entityDefinitionUpdateManager->getFieldStorageDefinition('ai_summary', 'tpr_service')
+      ->willReturn($installed->reveal());
+
+    $this->entityDefinitionUpdateManager->updateFieldStorageDefinition($definition)
+      ->shouldNotBeCalled();
+    $this->entityDefinitionUpdateManager
+      ->installFieldStorageDefinition('ai_summary', 'tpr_service', 'helfi_ai', $definition)
+      ->shouldNotBeCalled();
+
+    $this->createHooks()->installAiSummaryField();
+  }
+
+  /**
+   * Tests that both displays are saved for the presave hooks.
+   */
+  public function testSavesFormAndViewDisplays(): void {
+    $this->setUpTprService();
+    $this->entityDefinitionUpdateManager->getFieldStorageDefinition('ai_summary', 'tpr_service')
+      ->willReturn(NULL);
+
+    $this->formDisplay->save()->shouldBeCalledOnce();
+    $this->viewDisplay->save()->shouldBeCalledOnce();
+
+    $this->createHooks()->installAiSummaryField();
+  }
+
+  /**
+   * Tests that nothing is installed without the tpr_service entity type.
+   */
+  public function testSkipsSiteWithoutTprService(): void {
+    $this->entityTypeManager->hasDefinition('tpr_service')->willReturn(FALSE);
+    $this->entityFieldManager->getFieldStorageDefinitions('tpr_service')->shouldNotBeCalled();
+
+    $this->createHooks()->installAiSummaryField();
+  }
+
+  /**
+   * Tests that nothing is installed when the field is not defined.
+   */
+  public function testSkipsUndefinedField(): void {
+    $this->entityTypeManager->hasDefinition('tpr_service')->willReturn(TRUE);
+    $this->entityFieldManager->getFieldStorageDefinitions('tpr_service')->willReturn([]);
+    $this->entityDefinitionUpdateManager->getFieldStorageDefinition('ai_summary', 'tpr_service')
+      ->shouldNotBeCalled();
+
+    $this->createHooks()->installAiSummaryField();
+  }
+
+  /**
+   * Tests that installing either module installs the field.
+   */
+  public function testInstallsFieldForRelatedModules(): void {
+    $this->entityTypeManager->hasDefinition('tpr_service')
+      ->willReturn(FALSE)
+      ->shouldBeCalledTimes(3);
+
+    foreach ([['helfi_ai'], ['helfi_tpr_config'], ['node', 'helfi_ai']] as $modules) {
+      $this->createHooks()->modulesInstalled($modules, FALSE);
+    }
+  }
+
+  /**
+   * Tests that unrelated module installations are ignored.
+   */
+  public function testSkipsUnrelatedModules(): void {
+    $this->entityTypeManager->hasDefinition('tpr_service')->shouldNotBeCalled();
+
+    $this->createHooks()->modulesInstalled(['node', 'helfi_tpr'], FALSE);
+  }
+
+  /**
+   * Tests that configuration import is ignored.
+   */
+  public function testSkipsSyncingInstall(): void {
+    $this->entityTypeManager->hasDefinition('tpr_service')->shouldNotBeCalled();
+
+    $this->createHooks()->modulesInstalled(['helfi_ai'], TRUE);
+  }
+
+}
--- modules/helfi_ai/tests/src/Unit/HookTest.php
+++ modules/helfi_ai/tests/src/Unit/HookTest.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\helfi_ai\Unit;
 
+use Drupal\Core\Entity\Display\EntityFormDisplayInterface;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Field\FieldTypePluginManagerInterface;
 use Drupal\helfi_ai\Hook\EntityHooks;
 use Drupal\Tests\UnitTestCase;
 use PHPUnit\Framework\Attributes\Group;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -28,22 +32,9 @@ class HookTest extends UnitTestCase {
   }
 
   /**
-   * Test that there is no ai_summary for non node entity types.
+   * Sets up a container that can build field definitions.
    */
-  public function testReturnsEmptyArrayForNonNodeEntityType(): void {
-    $entityType = $this->prophesize(EntityTypeInterface::class);
-    $entityType->id()->willReturn('user');
-
-    $hooks = new EntityHooks();
-
-    $result = $hooks->entityBaseFieldInfo($entityType->reveal());
-    $this->assertSame([], $result);
-  }
-
-  /**
-   * Test that the hook adds the ai_summary base field to node entities.
-   */
-  public function testReturnsFieldDefinitionForNodeEntityType(): void {
+  private function setUpFieldTypeContainer(): void {
     $fieldTypeManager = $this->createMock(FieldTypePluginManagerInterface::class);
     $fieldTypeManager->method('getDefaultStorageSettings')->willReturn([]);
     $fieldTypeManager->method('getDefaultFieldSettings')->willReturn([]);
@@ -61,6 +52,47 @@ class HookTest extends UnitTestCase {
       }
     );
     \Drupal::setContainer($container);
+  }
+
+  /**
+   * Builds a display that has no ai_summary field yet.
+   *
+   * @param string $id
+   *   The display id.
+   * @param string $class
+   *   The display interface to prophesize.
+   *
+   * @return \Prophecy\Prophecy\ObjectProphecy
+   *   The display.
+   */
+  private function prophesizeDisplay(string $id, string $class): ObjectProphecy {
+    $display = $this->prophesize($class);
+    $display->id()->willReturn($id);
+    $display->isSyncing()->willReturn(FALSE);
+    $display->getComponent('ai_summary')->willReturn(NULL);
+    $display->get('hidden')->willReturn([]);
+
+    return $display;
+  }
+
+  /**
+   * Test that there is no ai_summary for unsupported entity types.
+   */
+  public function testReturnsEmptyArrayForUnsupportedEntityType(): void {
+    $entityType = $this->prophesize(EntityTypeInterface::class);
+    $entityType->id()->willReturn('user');
+
+    $hooks = new EntityHooks();
+
+    $result = $hooks->entityBaseFieldInfo($entityType->reveal());
+    $this->assertSame([], $result);
+  }
+
+  /**
+   * Test that the hook adds the ai_summary base field to node entities.
+   */
+  public function testReturnsFieldDefinitionForNodeEntityType(): void {
+    $this->setUpFieldTypeContainer();
 
     $entityType = $this->prophesize(EntityTypeInterface::class);
     $entityType->id()->willReturn('node');
@@ -73,6 +105,100 @@ class HookTest extends UnitTestCase {
     $this->assertInstanceOf(BaseFieldDefinition::class, $result['ai_summary']);
     $this->assertTrue($result['ai_summary']->isRevisionable());
     $this->assertTrue($result['ai_summary']->isTranslatable());
+  }
+
+  /**
+   * Test that the hook adds the ai_summary base field to tpr_service entities.
+   */
+  public function testReturnsFieldDefinitionForTprServiceEntityType(): void {
+    $this->setUpFieldTypeContainer();
+
+    $entityType = $this->prophesize(EntityTypeInterface::class);
+    $entityType->id()->willReturn('tpr_service');
+
+    $hooks = new EntityHooks();
+
+    $result = $hooks->entityBaseFieldInfo($entityType->reveal());
+
+    $this->assertArrayHasKey('ai_summary', $result);
+    $this->assertInstanceOf(BaseFieldDefinition::class, $result['ai_summary']);
+  }
+
+  /**
+   * Test that the widget is added to the tpr_service form display.
+   */
+  public function testAddsWidgetToTprServiceFormDisplay(): void {
+    $display = $this->prophesizeDisplay('tpr_service.tpr_service.default', EntityFormDisplayInterface::class);
+    $display->setComponent('ai_summary', [
+      'type' => 'ai_summary',
+      'weight' => 7,
+      'region' => 'content',
+    ])->shouldBeCalled();
+
+    (new EntityHooks())->entityFormDisplayPresave($display->reveal());
+  }
+
+  /**
+   * Test that the formatter is added to the tpr_service view display.
+   */
+  public function testAddsFormatterToTprServiceViewDisplay(): void {
+    $display = $this->prophesizeDisplay('tpr_service.tpr_service.default', EntityViewDisplayInterface::class);
+    $display->setComponent('ai_summary', [
+      'type' => 'text_default',
+      'label' => 'hidden',
+      'weight' => 4,
+      'region' => 'content',
+    ])->shouldBeCalled();
+
+    (new EntityHooks())->entityViewDisplayPresave($display->reveal());
+  }
+
+  /**
+   * Test that other displays are left alone.
+   */
+  public function testIgnoresOtherDisplays(): void {
+    $formDisplay = $this->prophesizeDisplay('node.page.default', EntityFormDisplayInterface::class);
+    $formDisplay->setComponent(Argument::cetera())->shouldNotBeCalled();
+
+    $viewDisplay = $this->prophesizeDisplay('tpr_service.tpr_service.teaser', EntityViewDisplayInterface::class);
+    $viewDisplay->setComponent(Argument::cetera())->shouldNotBeCalled();
+
+    $hooks = new EntityHooks();
+    $hooks->entityFormDisplayPresave($formDisplay->reveal());
+    $hooks->entityViewDisplayPresave($viewDisplay->reveal());
+  }
+
+  /**
+   * Test that a display being imported is left alone.
+   */
+  public function testIgnoresSyncingDisplay(): void {
+    $display = $this->prophesizeDisplay('tpr_service.tpr_service.default', EntityFormDisplayInterface::class);
+    $display->isSyncing()->willReturn(TRUE);
+    $display->setComponent(Argument::cetera())->shouldNotBeCalled();
+
+    (new EntityHooks())->entityFormDisplayPresave($display->reveal());
+  }
+
+  /**
+   * Test that an existing component is not overridden.
+   */
+  public function testKeepsExistingComponent(): void {
+    $display = $this->prophesizeDisplay('tpr_service.tpr_service.default', EntityFormDisplayInterface::class);
+    $display->getComponent('ai_summary')->willReturn(['type' => 'string_textarea']);
+    $display->setComponent(Argument::cetera())->shouldNotBeCalled();
+
+    (new EntityHooks())->entityFormDisplayPresave($display->reveal());
+  }
+
+  /**
+   * Test that a field hidden by an editor stays hidden.
+   */
+  public function testKeepsFieldHiddenByEditor(): void {
+    $display = $this->prophesizeDisplay('tpr_service.tpr_service.default', EntityFormDisplayInterface::class);
+    $display->get('hidden')->willReturn(['ai_summary' => TRUE]);
+    $display->setComponent(Argument::cetera())->shouldNotBeCalled();
+
+    (new EntityHooks())->entityFormDisplayPresave($display->reveal());
   }
 
 }
