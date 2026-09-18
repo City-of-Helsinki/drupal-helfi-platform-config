b3d7a893f177e8485d12d540859ffae9c5d9228d
Anna Lonka <anna.lonka@druid.fi>
2026-09-18 13:22:37 +0300

    UHF-12588: Updated map related texts for content creators

--- modules/helfi_media_map/src/Form/HelfiMediaMapAddForm.php
+++ modules/helfi_media_map/src/Form/HelfiMediaMapAddForm.php
@@ -25,10 +25,7 @@ class HelfiMediaMapAddForm extends AddFormBase {
     $container['helfi_media_map_url'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Map embed URL'),
-      '#description' => $this->t('Enter the map embed URL from @kartta or @palvelukartta.', [
-        '@kartta' => Link::fromTextAndUrl('https://kartta.hel.fi/', Url::fromUri('https://kartta.hel.fi/', ['attributes' => ['target' => '_blank']]))->toString(),
-        '@palvelukartta' => Link::fromTextAndUrl('https://palvelukartta.hel.fi/fi/', Url::fromUri('https://palvelukartta.hel.fi/fi/', ['attributes' => ['target' => '_blank']]))->toString(),
-      ]),
+      '#description' => $this->t('You can embed maps from Palvelukartta or Karttapalvelu.', []),
       '#maxlength' => 2048,
     ];
 
